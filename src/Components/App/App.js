320029d0a8c61b7dbfc7a9a459f4facc2df05f8f
Mike Joyce <mjoyce91@gmail.com>
2017-06-26 14:17:18 -0400

    Render filters from TalentMAP API

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,15 +4,14 @@ import { Switch, Route, HashRouter } from 'react-router-dom';
 import Home from '../Home/Home';
 import Results from '../Results/Results';
 import Details from '../Details/Details';
-import items from '../Home/items.json';
 
-const api = 'http://localhost:3005';
+const api = 'http://localhost:8000/api/v1';
 
 const App = () => (
   <HashRouter>
     <main>
       <Switch>
-        <Route exact path="/" component={() => <Home items={items} />} />
+        <Route exact path="/" component={() => <Home api={api} />} />
         <Route path="/results" component={props => <Results {...props} api={api} />} />
         <Route path="/details/:id" component={props => <Details {...props} api={api} />} />
       </Switch>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import queryString from 'query-string';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import Wrapper from '../Wrapper/Wrapper';
 
 class Home extends Component {
@@ -8,15 +9,63 @@ class Home extends Component {
     super(props);
     this.state = {
       selection: { skill: [], language: [], grade: [], q: '' },
+      items: [
+        {
+          title: 'Skill code',
+          endpoint: 'position/skills',
+          selectionRef: 'skill',
+          text: 'Choose skill codes',
+          choices: [
+          ],
+        },
+        {
+          title: 'Language',
+          endpoint: 'language',
+          selectionRef: 'language',
+          text: 'Choose languages',
+          choices: [
+          ],
+        },
+        {
+          title: 'Grade',
+          endpoint: 'position/grades',
+          selectionRef: 'grade',
+          text: 'Choose grades',
+          choices: [
+          ],
+        },
+      ],
       proficiency: {},
       qString: null,
     };
   }
 
   componentWillMount() {
-    let { items } = this.state;
-    items = this.props.items;
-    this.setState({ items });
+  }
+
+  componentDidMount() {
+    this.getFilters(); // eslint-disable-line react/prop-types
+  }
+
+  getFilters() {
+    const api = this.props.api;
+    this.state.items.forEach((item, i) => {
+      const endpoint = item.endpoint;
+      axios.get(`${api}/${endpoint}`)
+        .then((res) => {
+          const filters = res.data;
+          console.log(res.data);
+          this.state.items[i].choices = filters;
+          console.log(this.state.items);
+          const items = this.state.items;
+          items[i].choices = filters;
+
+          // update state
+          this.setState({
+            items,
+          });
+        });
+    });
   }
 
   changeProficiency(prof, val) {
@@ -120,53 +169,94 @@ class Home extends Component {
               {items.map((item, i) => {
                 const id = item.id || `item${i}`;
                 const checks = item.choices.map(choice => (
-                  <div key={choice.code} className="usa-width-one-fourth">
-                    <input
-                      id={`${i}-${choice.code}`}
-                      type="checkbox"
-                      title={`${i}-${choice.description}`}
-                      name="historical-figures-1"
-                      value={choice.code}
-                      onChange={e => this.changeCheck(i, e)}
-                      checked={selection[items[i].selection_ref]
-                                .indexOf(choice.code) !== -1}
-                    />
-                    <label htmlFor={`${i}-${choice.description}`}>
-                      {choice.description}
-                    </label>
-                    { items[i].selection_ref === 'language' ?
-                      <div>
-                      Written
-                      <div className="button_wrapper">
-                        {[1, 2, 3, 4, 5].map(a => (
-                          <button
-                            key={`${choice.description}-written-${a}`}
-                            id={`${choice.description}-written-${a}`}
-                            className={this.state.proficiency[`${choice.description}-written`] === a.toString() ? 'usa-button-primary-alt usa-button-active' : 'usa-button-primary-alt'}
-                            onClick={() => this.changeProficiency(`${choice.description}-written`, a.toString(), choice.code)}
-                          >
-                            {a}
-                          </button>
-                        ),
-                        )}
+                  <div>
+                    { items[i].selectionRef === 'skill' ?
+                      <div key={choice.code} className="usa-width-one-fourth">
+                        <input
+                          id={`${i}-${choice.code}`}
+                          type="checkbox"
+                          title={`${i}-${choice.description}`}
+                          name="historical-figures-1"
+                          value={choice.code}
+                          onChange={e => this.changeCheck(i, e)}
+                          checked={selection[items[i].selectionRef]
+                                    .indexOf(choice.code) !== -1}
+                        />
+                        <label htmlFor={`${i}-${choice.description}`}>
+                          {choice.description}
+                        </label>
                       </div>
-                      Spoken
-                      <div className="button_wrapper">
-                        {[1, 2, 3, 4, 5].map(a => (
-                          <button
-                            key={`${choice.description}-spoken-${a}`}
-                            id={`${choice.description}-spoken-${a}`}
-                            className={this.state.proficiency[`${choice.description}-spoken`] === a.toString() ? 'usa-button-primary-alt usa-button-active' : 'usa-button-primary-alt'}
-                            onClick={() => this.changeProficiency(`${choice.description}-spoken`, a.toString(), choice.code)}
-                          >
-                            {a}
-                          </button>
-                        ),
-                        )}
+                    : null
+                    }
+                    { items[i].selectionRef === 'language' ?
+                      <div key={choice.code} className="usa-width-one-fourth">
+                        <input
+                          id={`${i}-${choice.code}`}
+                          type="checkbox"
+                          title={`${i}-${choice.short_description}`}
+                          name="historical-figures-1"
+                          value={choice.code}
+                          onChange={e => this.changeCheck(i, e)}
+                          checked={selection[items[i].selectionRef]
+                                    .indexOf(choice.code) !== -1}
+                        />
+                        <label htmlFor={`${i}-${choice.short_description}`}>
+                          {choice.short_description}
+                        </label>
+                        { items[i].selectionRef === 'language' ?
+                          <div>
+                          Written
+                          <div className="button_wrapper">
+                            {[1, 2, 3, 4, 5].map(a => (
+                              <button
+                                key={`${choice.short_description}-written-${a}`}
+                                id={`${choice.short_description}-written-${a}`}
+                                className={this.state.proficiency[`${choice.short_description}-written`] === a.toString() ? 'usa-button-primary-alt usa-button-active' : 'usa-button-primary-alt'}
+                                onClick={() => this.changeProficiency(`${choice.short_description}-written`, a.toString(), choice.code)}
+                              >
+                                {a}
+                              </button>
+                            ),
+                            )}
+                          </div>
+                          Spoken
+                          <div className="button_wrapper">
+                            {[1, 2, 3, 4, 5].map(a => (
+                              <button
+                                key={`${choice.short_description}-spoken-${a}`}
+                                id={`${choice.short_description}-spoken-${a}`}
+                                className={this.state.proficiency[`${choice.short_description}-spoken`] === a.toString() ? 'usa-button-primary-alt usa-button-active' : 'usa-button-primary-alt'}
+                                onClick={() => this.changeProficiency(`${choice.short_description}-spoken`, a.toString(), choice.code)}
+                              >
+                                {a}
+                              </button>
+                            ),
+                            )}
+                          </div>
+                            <br />
+                          </div>
+                        : null }
                       </div>
-                        <br />
+                    : null
+                    }
+                    { items[i].selectionRef === 'grade' ?
+                      <div key={choice.code} className="usa-width-one-fourth">
+                        <input
+                          id={`${i}-${choice.code}`}
+                          type="checkbox"
+                          title={`${i}-${choice.code}`}
+                          name="historical-figures-1"
+                          value={choice.code}
+                          onChange={e => this.changeCheck(i, e)}
+                          checked={selection[items[i].selectionRef]
+                                    .indexOf(choice.code) !== -1}
+                        />
+                        <label htmlFor={`${i}-${choice.code}`}>
+                          {choice.code}
+                        </label>
                       </div>
-                    : null }
+                    : null
+                    }
                   </div>
                 ));
                 return (
@@ -198,23 +288,10 @@ class Home extends Component {
 }
 
 Home.propTypes = {
-  items: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string,
-      selection_ref: PropTypes.string,
-      text: PropTypes.string,
-      choices: PropTypes.arrayOf(
-        PropTypes.shape({
-          code: PropTypes.number,
-          description: PropTypes.string,
-        }),
-      ),
-    }),
-  ),
+  api: PropTypes.string.isRequired,
 };
 
 Home.defaultProps = {
-  items: [],
 };
 
 export default Home;
