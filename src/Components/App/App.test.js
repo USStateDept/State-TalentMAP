cb39c72aebe574e0a8832cf7cb825ab5f86d7c39
Nat Burgwyn <burgwyn@gmail.com>
2017-07-03 16:04:05 -0400

    Moving routing from App to Main

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<MemoryRouter><App api={'http://localhost:8000/api/v1'} /></MemoryRouter>, div);
   global.document.getElementById = id => id === 'root' && div;
 });
