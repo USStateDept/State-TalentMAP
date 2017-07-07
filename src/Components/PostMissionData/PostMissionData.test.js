3ccb5a16469c0e9f701d1598858bd98b2b082965
Mike Joyce <mjoyce91@gmail.com>
2017-07-07 09:01:08 -0400

    Clean up test for PostMissionData component

--- src/Components/PostMissionData/PostMissionData.test.js
+++ src/Components/PostMissionData/PostMissionData.test.js
@@ -22,19 +22,6 @@ describe('PostMissionDataComponent', () => {
     languages: [{ id: 1, language: 'French (FR)', written_proficiency: '2', spoken_proficiency: '2', representation: 'French (FR) 2/2' }],
   };
 
-  const postObjectFalses = {
-    id: 200,
-    tour_of_duty: '1Y2RR',
-    code: 'AF1000000',
-    description: 'HERAT, AFGHANISTAN',
-    cost_of_living_adjustment: 0,
-    differential_rate: 35,
-    danger_pay: 35,
-    rest_relaxation_point: 'London',
-    has_consumable_allowance: false,
-    has_service_needs_differential: false,
-  };
-
   beforeEach(() => {
     postMissionData = TestUtils.renderIntoDocument(<MemoryRouter>
       <PostMissionData post={postObject} />
@@ -52,8 +39,13 @@ describe('PostMissionDataComponent', () => {
   });
 
   it('is can receive props with false values', () => {
-    wrapper = shallow(<PostMissionData post={postObjectFalses} />);
-    expect(wrapper.instance().props.post.id).toBe(200);
+    Object.assign(postObject, {
+      languages: [],
+      has_consumable_allowance: null,
+      has_service_needs_differential: false,
+    });
+    wrapper = shallow(<PostMissionData post={postObject} />);
+    expect(wrapper.instance().props.post.languages.length).toBe(0);
     expect(wrapper.instance().props.post.has_service_needs_differential).toBe(false);
   });
 });
