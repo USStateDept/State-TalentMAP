041ed03efd921acb35d69d08845a529c79a482b0
Mike Joyce <mjoyce91@gmail.com>
2017-07-07 11:24:05 -0400

    Use memory router for Results test

--- src/Containers/Results/Results.js
+++ src/Containers/Results/Results.js
@@ -12,14 +12,15 @@ class Results extends Component {
   }
 
   componentWillMount() {
-    if (!this.props.results) {
-      this.getPosts(window.location.search);
-    }
+    console.log('1', window.location.search);
+    this.getPosts(window.location.search);
   }
 
   getPosts(q) {
-    const query = q;
+    const query = q || '';
     const api = this.props.api;
+    console.log('2');
+    console.log(`${api}/position/${query}`);
     ajax(`${api}/position/${query}`)
       .then((res) => {
         const results = res.data;
@@ -47,44 +48,10 @@ class Results extends Component {
 
 Results.propTypes = {
   api: PropTypes.string.isRequired,
-  results: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      grade: PropTypes.string,
-      skill: PropTypes.string,
-      bureau: PropTypes.string,
-      organization: PropTypes.string,
-      position_number: PropTypes.string.isRequired,
-      is_overseas: PropTypes.boolean,
-      create_date: PropTypes.string,
-      update_date: PropTypes.string,
-      post: PropTypes.shape({
-        id: PropTypes.number,
-        tour_of_duty: PropTypes.string,
-        code: PropTypes.string,
-        description: PropTypes.string,
-        cost_of_living_adjustment: PropTypes.number,
-        differential_rate: PropTypes.number,
-        danger_pay: PropTypes.number,
-        rest_relaxation_point: PropTypes.string,
-        has_consumable_allowance: PropTypes.boolean,
-        has_service_needs_differential: PropTypes.boolean,
-      }),
-      languages: PropTypes.arrayOf(
-        PropTypes.shape({
-          id: PropTypes.number,
-          language: PropTypes.string,
-          written_proficiency: PropTypes.string,
-          spoken_proficiency: PropTypes.string,
-          representation: PropTypes.string,
-        }),
-      ),
-    }),
-  ),
 };
 
-Results.defaultProps = {
-  results: [],
+Results.contextTypes = {
+  router: PropTypes.object,
 };
 
 export default Results;
