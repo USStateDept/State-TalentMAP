15fcfe842536de4c2971c6af73e4d65f78b0f2d8
Mike Joyce <mjoyce91@gmail.com>
2017-07-18 14:09:39 -0400

    Revert layout of position details to one div

--- src/Components/PositionDetails/PositionDetails.jsx
+++ src/Components/PositionDetails/PositionDetails.jsx
@@ -15,43 +15,37 @@ const PositionDetails = ({ details, api }) => {
     <div className="usa-grid-full">
       <div style={{ backgroundColor: '#F2F2F2', marginTop: '10px', marginBottom: '10px', padding: '15px 30px' }}>
         <h3> Position Number: {details.position_number} </h3>
-        <div className="usa-grid-full">
-          <div className="usa-width-one-half">
-            <p>
-              Grade: {details.grade}
-              <br />
-              Skill: {details.skill}
-              <br />
-              Bureau: {details.bureau}
-              <br />
-              Organization: {details.organization}
-              <br />
-              Overseas: {details.is_overseas ? 'Yes' : 'No'}
-              <br />
-              Language: <span>{languageList}</span>
-              <br />
-              Danger Pay: {details.post ? details.post.danger_pay : AlertMessages.NO_DANGER_PAY}
-              <br />
-              Region: 5 {/* TODO replace hard-coded value with API value */}
-              <br />
-              Post: {details.post ? <Link to={`/post/${details.post.id}`}>{details.post.description}</Link> : AlertMessages.NO_POST }
-              <br />
-              Post Differential: {
-                details.post ?
-                details.post.differential_rate
-                : AlertMessages.NO_POST_DIFFERENTIAL
-              }
-              <br />
-              Created: {details.create_date}
-              <br />
-              Updated: {details.update_date}
-            </p>
-            <FavoritesButton refKey={details.position_number} type="fav" />
-          </div>
-          <div className="usa-width-one-half">
-            <Share api={api} identifier={details.id} />
-          </div>
-        </div>
+        <p>
+            Grade: {details.grade}
+          <br />
+            Skill: {details.skill}
+          <br />
+            Bureau: {details.bureau}
+          <br />
+            Organization: {details.organization}
+          <br />
+            Overseas: {details.is_overseas ? 'Yes' : 'No'}
+          <br />
+            Language: <span>{languageList}</span>
+          <br />
+            Danger Pay: {details.post ? details.post.danger_pay : AlertMessages.NO_DANGER_PAY}
+          <br />
+            Region: 5 {/* TODO replace hard-coded value with API value */}
+          <br />
+            Post: {details.post ? <Link to={`/post/${details.post.id}`}>{details.post.description}</Link> : AlertMessages.NO_POST }
+          <br />
+            Post Differential: {
+              details.post ?
+              details.post.differential_rate
+              : AlertMessages.NO_POST_DIFFERENTIAL
+            }
+          <br />
+            Created: {details.create_date}
+          <br />
+            Updated: {details.update_date}
+        </p>
+        <FavoritesButton refKey={details.position_number} type="fav" />
+        <Share api={api} identifier={details.id} />
       </div>
     </div>
   );
