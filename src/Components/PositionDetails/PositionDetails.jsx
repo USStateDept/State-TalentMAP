0cca5a95a6a7a090b0631ca5872a33b734791ed0
Mike Joyce <mjoyce91@gmail.com>
2017-08-01 15:49:42 -0400

    Initial stab at position details style

--- src/Components/PositionDetails/PositionDetails.jsx
+++ src/Components/PositionDetails/PositionDetails.jsx
@@ -1,62 +1,59 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import FavoritesButton from '../FavoritesButton/FavoritesButton';
+// import FavoritesButton from '../FavoritesButton/FavoritesButton';
 import * as AlertMessages from '../../Constants/AlertMessages';
 import { POSITION_DETAILS } from '../../Constants/PropTypes';
-import Share from '../Share/Share';
+// import Share from '../Share/Share';
 import LanguageList from '../LanguageList/LanguageList';
 import Loading from '../Loading/Loading';
+import PositionTitle from '../PositionTitle/PositionTitle';
 
-const PositionDetails = ({ details, api, isLoading, hasErrored }) => {
-  const detailsBody = details && !isLoading && !hasErrored ? (
-    <div className="usa-grid">
-      <div style={{ backgroundColor: '#F2F2F2', marginTop: '10px', marginBottom: '10px', padding: '15px 30px' }}>
-        <h3> Position Number: {details.position_number} </h3>
-        <p>
-          Grade: {details.grade}
-          <br />
-          Skill: {details.skill}
-          <br />
-          Bureau: {details.bureau}
-          <br />
-          Organization: {details.organization}
-          <br />
-          Overseas: {details.is_overseas ? 'Yes' : 'No'}
-          <br />
-          Language: <LanguageList languages={details.languages} />
-          <br />
-          Danger Pay: {details.post ? details.post.danger_pay : AlertMessages.NO_DANGER_PAY}
-          <br />
-          Region: 5 {/* TODO replace hard-coded value with API value */}
-          <br />
-          Post: {details.post ? <Link to={`/post/${details.post.id}`}>{details.post.description}</Link> : AlertMessages.NO_POST }
-          <br />
-          Post Differential: {
-            details.post ?
-            details.post.differential_rate
-            : AlertMessages.NO_POST_DIFFERENTIAL
-          }
-          <br />
-          Created: {details.create_date}
-          <br />
-          Updated: {details.update_date}
-        </p>
-        <FavoritesButton refKey={details.position_number} type="fav" />
-        <Share api={api} identifier={details.id} />
-      </div>
-    </div>
-  ) : <Loading isLoading={isLoading} hasErrored={hasErrored} />;
-  return (
-    <div className="usa-grid-full">
-      {detailsBody}
-    </div>
+const PositionDetails = ({ details, isLoading, hasErrored }) => (
+  <div>
+    {(details && !isLoading && !hasErrored) &&
+      <div>
+        <PositionTitle details={details} />
+        <div className="usa-grid-full">
+          <div className="usa-width-one-whole" style={{ marginTop: '10px', marginBottom: '10px', padding: '15px 30px' }}>
+            <div className="usa-width-one-whole position-details-description" style={{ marginLeft: '9%' }}>
+              <strong>POSITION DESCRIPTION</strong>
+              <br />
+              <div className="usa-width-one-whole">
+                <div className="usa-width-one-fourth position-details-description-section">
+                  <div className="title">GRADE</div>
+                  <div className="description">{details.grade}</div>
+                </div>
+                <div className="usa-width-one-fourth position-details-description-section">
+                  <div className="title">LANGUAGE</div>
+                  <div className="description"><LanguageList languages={details.languages} /></div>
+                </div>
+                <div className="usa-width-one-fourth position-details-description-section">
+                  <div className="title">SKILLS</div>
+                  <div className="description">Information Management</div>
+                </div>
+              </div>
+              <div className="usa-width-one-whole">
+                <div className="usa-width-one-fourth position-details-description-section">
+                  <div className="title">DANGER PAY</div>
+                  <div className="description">
+                    {details.post ? details.post.danger_pay : AlertMessages.NO_DANGER_PAY}
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* <FavoritesButton refKey={details.position_number} type="fav" />
+            <Share api={api} identifier={details.id} /> */}
+          </div>
+        </div>
+      </div>}
+    {isLoading && <Loading isLoading={isLoading} hasErrored={hasErrored} />}
+  </div>
   );
-};
 
 PositionDetails.propTypes = {
   details: POSITION_DETAILS,
-  api: PropTypes.string.isRequired,
+  // api: PropTypes.string.isRequired,
   isLoading: PropTypes.bool,
   hasErrored: PropTypes.bool,
 };
---
+++ src/Components/PositionTitle/PositionTitle.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { POSITION_DETAILS } from '../../Constants/PropTypes';
+// import * as AlertMessages from '../../Constants/AlertMessages';
+
+const PositionTitle = ({ details }) => (
+  <div className="position-details-header" style={{ overflow: 'hidden', backgroundColor: '#F2F2F2' }}>
+    <div className="usa-grid" style={{ overflow: 'hidden' }}>
+      <div className="usa-width-one-half">
+        <div className="position-details-header-back">
+          Back to Search Results
+        </div>
+        <div className="position-details-header-title">
+          <strong>Position Number: {details.position_number}</strong>
+        </div>
+        <p className="position-details-header-body">
+          <strong>Description: </strong>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry standard dummy text
+            ever since the 1500s, when an unknown printer
+            took a galley of type and scrambled it to make...
+        </p>
+        <div className="usa-width-one-half position-details-header-body">
+          <strong>Post website:</strong> www.post.gov
+        </div>
+        <div className="usa-width-one-half position-details-header-body">
+          <strong>Point of Contact:</strong> 222-222-2222
+        </div>
+      </div>
+    </div>
+    <img
+      className="position-details-header-image"
+      alt="department of state seal"
+      src="/assets/img/dos-seal.png"
+    />
+  </div>
+);
+
+PositionTitle.propTypes = {
+  details: POSITION_DETAILS,
+};
+
+PositionTitle.defaultProps = {
+  details: null,
+};
+
+export default PositionTitle;
