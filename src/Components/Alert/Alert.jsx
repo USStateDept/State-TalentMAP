88074d80d2cdf9a5510a5f0bd35bdad5a69df239
Nat Burgwyn <burgwyn@gmail.com>
2017-08-02 21:31:36 -0400

    Implementing shortid as a ket strategy

--- src/Components/Alert/Alert.jsx
+++ src/Components/Alert/Alert.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const shortid = require('shortid');
+
 const Alert = ({ type, title, messages }) => (
   // 'type' is injected into the class name
   // type 'error' requires an ARIA role
   <div className={`usa-alert usa-alert-${type}`} role={(type === 'error') ? 'alert' : null}>
     <div className="usa-alert-body">
       <h3 className="usa-alert-heading">{title}</h3>
-      {messages.map((message, index) =>
-        (<p className="usa-alert-text" key={index}>{/* eslint-disable react/no-array-index-key */}
+      {messages.map(message =>
+        (<p className="usa-alert-text" key={shortid.generate()}>
           {message.body}
         </p>),
       )}
