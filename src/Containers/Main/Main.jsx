9389eb4eb42eae3829a88c7ae9ac0e9fb62d6910
Nat Burgwyn <burgwyn@gmail.com>
2017-08-03 22:00:03 -0400

    Moving routes into a new container

--- src/Containers/Main/Main.jsx
+++ src/Containers/Main/Main.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { createStore, applyMiddleware } from 'redux';
@@ -13,12 +12,7 @@ import createHistory from 'history/createBrowserHistory';
 
 import rootReducer from '../../reducers';
 
-import Home from '../../Containers/Home/Home';
-import Results from '../../Containers/Results/Results';
-import Position from '../../Containers/Position/Position';
-import Post from '../../Containers/Post/Post';
-import Login from '../../login';
-import Compare from '../../Containers/Compare/Compare';
+import Routes from '../../Containers/Routes/Routes';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 
@@ -54,33 +48,7 @@ const Main = props => (
       <div>
         <Header {...props} isAuthorized={isAuthorized()} />
         <main id="main-content">
-          <Switch {...props}>
-            <Route
-              path="/login"
-              component={() => (<Login {...props} isAuthorized={isAuthorized} />)}
-            />
-            <Route
-              exact
-              path="/"
-              component={() => (<Home {...props} isAuthorized={isAuthorized} />)}
-            />
-            <Route
-              path="/results"
-              component={() => (<Results {...props} isAuthorized={isAuthorized} />)}
-            />
-            <Route
-              path="/details/:id"
-              component={() => (<Position {...props} isAuthorized={isAuthorized} />)}
-            />
-            <Route
-              path="/post/:id"
-              component={() => (<Post {...props} isAuthorized={isAuthorized} />)}
-            />
-            <Route
-              path="/compare/:ids"
-              component={() => (<Compare {...props} isAuthorized={isAuthorized} />)}
-            />
-          </Switch>
+          <Routes {...props} isAuthorized={isAuthorized} />
         </main>
         <Footer />
       </div>
