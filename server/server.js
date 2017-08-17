990e59b40ae5e22486ddbcde1d6057b7781c737e
Nat Burgwyn <burgwyn@gmail.com>
2017-08-17 15:52:58 -0400

    Adding API_URL env variable

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 
+// define the url consuming the API, provide local dev value as default
+const API_URL = process.env.API_URL || 'http://localhost:8000/api/v1';
+
 const port = process.env.PORT || 3000;
 const app = express();
 
