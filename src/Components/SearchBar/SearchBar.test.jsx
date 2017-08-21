76896df645825d96164a6a295b10172a4817f894
Mike Joyce <mjoyce91@gmail.com>
2017-08-21 14:20:20 -0400

    Run tests against all SearchBar sizes

--- src/Components/SearchBar/SearchBar.test.jsx
+++ src/Components/SearchBar/SearchBar.test.jsx
@@ -3,69 +3,71 @@ import React from 'react';
 import toJSON from 'enzyme-to-json';
 import SearchBar from './SearchBar';
 
-describe('SearchBarComponent', () => {
-  let wrapper = null;
+['small', 'medium', 'big'].forEach((size) => {
+  describe(`SearchBarComponent ${size}`, () => {
+    let wrapper = null;
 
-  it('can receive props', () => {
-    wrapper = shallow(
-      <SearchBar
-        id="search"
-        type="big"
-        submitDisabled
-        submitText="Submit"
-        alertText="Search disabled"
-        onChangeText={() => {}}
-        onSubmitSearch={() => {}}
-      />,
-    );
-    expect(wrapper.instance().props.id).toBe('search');
-  });
+    it('can receive props', () => {
+      wrapper = shallow(
+        <SearchBar
+          id="search"
+          type={size}
+          submitDisabled
+          submitText="Submit"
+          alertText="Search disabled"
+          onChangeText={() => {}}
+          onSubmitSearch={() => {}}
+        />,
+      );
+      expect(wrapper.instance().props.id).toBe('search');
+    });
 
-  it('handles different props', () => {
-    wrapper = shallow(
-      <SearchBar
-        id="search-2"
-        label="Label"
-        type="small"
-        submitDisabled={false}
-        submitText="Submit 2"
-        alertText="Search is disabled"
-        onChangeText={() => {}}
-        onSubmitSearch={() => {}}
-      />,
-    );
-    expect(wrapper.instance().props.id).toBe('search-2');
-  });
+    it('handles different props', () => {
+      wrapper = shallow(
+        <SearchBar
+          id="search-2"
+          label="Label"
+          type={size}
+          submitDisabled={false}
+          submitText="Submit 2"
+          alertText="Search is disabled"
+          onChangeText={() => {}}
+          onSubmitSearch={() => {}}
+        />,
+      );
+      expect(wrapper.instance().props.id).toBe('search-2');
+    });
 
-  it('can change text', () => {
-    wrapper = shallow(
-      <SearchBar
-        id="search-2"
-        type="small"
-        submitDisabled={false}
-        submitText="Submit 2"
-        alertText="Search is disabled"
-        onChangeText={() => {}}
-        onSubmitSearch={() => {}}
-      />,
-    );
-    wrapper.instance().changeText({ target: { value: 'test' } });
-    expect(wrapper.instance().state.searchText.value).toBe('test');
-  });
+    it('can change text', () => {
+      wrapper = shallow(
+        <SearchBar
+          id="search-2"
+          type={size}
+          submitDisabled={false}
+          submitText="Submit 2"
+          alertText="Search is disabled"
+          onChangeText={() => {}}
+          onSubmitSearch={() => {}}
+        />,
+      );
+      wrapper.instance().changeText({ target: { value: 'test' } });
+      expect(wrapper.instance().state.searchText.value).toBe('test');
+    });
 
-  it('matches snapshot', () => {
-    wrapper = shallow(
-      <SearchBar
-        id="search-2"
-        label="Label"
-        type="small"
-        submitDisabled={false}
-        submitText="Submit 2"
-        alertText="Search is disabled"
-        onChangeText={() => {}}
-        onSubmitSearch={() => {}}
-      />,
-    );
-    expect(toJSON(wrapper)).toMatchSnapshot();
+    it('matches snapshot', () => {
+      wrapper = shallow(
+        <SearchBar
+          id="search-2"
+          label="Label"
+          type={size}
+          submitDisabled={false}
+          submitText="Submit 2"
+          alertText="Search is disabled"
+          onChangeText={() => {}}
+          onSubmitSearch={() => {}}
+        />,
+      );
+      expect(toJSON(wrapper)).toMatchSnapshot();
+    });
   });
 });
