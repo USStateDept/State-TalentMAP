604bfe7eefb42ea87055a6ef4718e5da2b92ca6e
Mike Joyce <mjoyce91@gmail.com>
2017-08-22 17:15:47 -0400

    Add tests for form submission and text change

--- src/Components/SearchBar/SearchBar.test.jsx
+++ src/Components/SearchBar/SearchBar.test.jsx
@@ -1,6 +1,7 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import toJSON from 'enzyme-to-json';
+import sinon from 'sinon';
 import SearchBar from './SearchBar';
 
 ['small', 'medium', 'big'].forEach((size) => {
@@ -50,8 +51,27 @@ import SearchBar from './SearchBar';
           onSubmitSearch={() => {}}
         />,
       );
-      wrapper.instance().changeText({ target: { value: 'test' } });
+      // wrapper.instance().changeText({ target: { value: 'test' } });
+      wrapper.find('input').simulate('change', { target: { value: 'test' } });
       expect(wrapper.instance().state.searchText.value).toBe('test');
+    });
+
+    it('can submit the form', () => {
+      const spy = sinon.spy();
+      wrapper = shallow(
+        <SearchBar
+          id="search-2"
+          type={size}
+          submitDisabled={false}
+          submitText="Submit 2"
+          alertText="Search is disabled"
+          onChangeText={() => {}}
+          onSubmitSearch={spy}
+        />,
+      );
+      wrapper.find('input').simulate('change', { target: { value: 'test' } });
+      wrapper.find('form').simulate('submit');
+      expect(spy.calledOnce).toBe(true);
     });
 
     it('matches snapshot', () => {
