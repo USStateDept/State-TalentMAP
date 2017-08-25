3002748152a3dd6e1cc8ca0fb6ae0cbbf36e4db4
Mike Joyce <mjoyce91@gmail.com>
2017-08-25 13:30:33 -0400

    Use shortid

--- src/Components/ResultsCardDataItem/ResultsCardDataItem.jsx
+++ src/Components/ResultsCardDataItem/ResultsCardDataItem.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ResultsCardDataPoint from '../ResultsCardDataPoint/ResultsCardDataPoint';
 
+const shortid = require('shortid');
+
 const ResultsCardDataItem = ({ title, items }) => (
   <div>
     <div className="section-title">{title}</div>
     <div className="section-data-points">
       {items.map(item =>
         (<ResultsCardDataPoint
-          key={item.description}
+          key={shortid.generate()}
           description={item.description}
           text={item.text}
         />),
