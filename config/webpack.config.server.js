4b1381953637c481f144bcdd19f19e59088e6bfe
Nat Burgwyn <burgwyn@gmail.com>
2017-08-25 09:34:26 -0400

    Adding to webpack config

--- config/webpack.config.server.js
+++ config/webpack.config.server.js
@@ -3,6 +3,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     target: 'node',
@@ -10,21 +12,101 @@ module.exports = {
         __dirname: false,
     },
     externals: [nodeExternals()],
-    entry: {
-        server: './server.js',
-    },
+    entry: [
+        require.resolve('./polyfills'),
+        require.resolve('react-error-overlay'),
+        './src/server.js',
+    ],
     output: {
-        path: path.join(__dirname, './src'),
+        path: path.join(__dirname, '../build'),
         filename: 'server-app.js',
+    },
+    resolve: {
+        modules: ['node_modules',],
+        extensions: ['.js', '.json', '.jsx'],
+        plugins: [
+            new ModuleScopePlugin('./src'),
+        ],
     },
     module: {
         rules: [
             {
-                test: path.join(__dirname, './src'),
+                exclude: [
+                  /\.html$/,
+                  /\.(js|jsx)$/,
+                  /\.css$/,
+                  /\.json$/,
+                  /\.bmp$/,
+                  /\.gif$/,
+                  /\.jpe?g$/,
+                  /\.png$/,
+                  /\.scss$/,
+                  /\.svg$/,
+                ],
+                loader: require.resolve('file-loader'),
+                options: {
+                  name: 'static/media/[name].[hash:8].[ext]',
+                },
+            },
+            {
+                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                loader: require.resolve('url-loader'),
+                options: {
+                  limit: 20000,
+                  name: 'static/media/[name].[hash:8].[ext]',
+                },
+            },
+            {
+                test: /\.svg$/,
+                use: [{
+                    loader: 'babel-loader'
+                  }, {
+                    loader: 'react-svg-loader'
+                  }]
+            },
+            {
+                test: /\.(js|jsx)$/,
                 use: {
                     loader: 'babel-loader'
                 },
-            }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  require.resolve('style-loader'),
+                  {
+                    loader: require.resolve('css-loader'),
+                    options: {
+                      importLoaders: 1,
+                    },
+                  },
+                  {
+                    loader: require.resolve('postcss-loader'),
+                    options: {
+                      ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
+                      plugins: () => [
+                        require('postcss-flexbugs-fixes'),
+                        autoprefixer({
+                          browsers: [
+                            '>1%',
+                            'last 4 versions',
+                            'Firefox ESR',
+                            'not ie < 9', // React doesn't support IE8 anyway
+                          ],
+                          flexbox: 'no-2009',
+                        }),
+                      ],
+                    },
+                  },
+                ],
+              },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use: ['css-loader', 'sass-loader'],
+                }),
+            },
         ]
     }
 };
