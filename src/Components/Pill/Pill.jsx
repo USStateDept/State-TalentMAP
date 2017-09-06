ed2520fe18853122298abf513f5a9cd982e22e3c
Mike Joyce <mjoyce91@gmail.com>
2017-09-06 09:24:52 -0400

    Add title to pill filters, indicating that clicking the pill will remove it from filters

--- src/Components/Pill/Pill.jsx
+++ src/Components/Pill/Pill.jsx
@@ -9,7 +9,11 @@ const Pill = ({ description, codeRef, selectionRef, onPillClick }) => {
     padding: '.5em',
   };
   return (
-    <button style={style} onClick={() => onPillClick(selectionRef, codeRef, true)}>
+    <button
+      style={style}
+      title={`Remove ${description} filter`}
+      onClick={() => onPillClick(selectionRef, codeRef, true)}
+    >
       {description} <FontAwesome name="times" />
     </button>
   );
