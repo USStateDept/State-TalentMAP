9e123f106d51aa678536134f6557c299027f8cba
Nat Burgwyn <burgwyn@gmail.com>
2017-09-11 14:02:01 -0400

    Adding polyfill

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import './sass/styles.scss';
 import App from './Components/App/App';
 
 require('../node_modules/uswds/dist/js/uswds.min.js');
+require('babel-polyfill');
 
 ReactDOM.render((
   <App />
