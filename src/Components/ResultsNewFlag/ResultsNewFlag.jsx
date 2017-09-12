01f76be6ca1c09b18e80d200ac1031125f897d1d
Mike Joyce <mjoyce91@gmail.com>
2017-09-12 10:21:25 -0400

    Externalize styles for the condensed cards’ New flag

--- src/Components/ResultsNewFlag/ResultsNewFlag.jsx
+++ src/Components/ResultsNewFlag/ResultsNewFlag.jsx
@@ -1,21 +1,9 @@
 import React from 'react';
 
-const ResultsNewFlag = () => {
-  const style = {
-    align: 'auto',
-    backgroundColor: 'white',
-    borderRadius: '100px',
-    color: 'black',
-    fontSize: '13px',
-    padding: '.1em',
-    textAlign: 'center',
-    width: '50px',
-  };
-  return (
-    <div style={style}>
-      New
-    </div>
-  );
-};
+const ResultsNewFlag = () => (
+  <div className="new-flag">
+    New
+  </div>
+);
 
 export default ResultsNewFlag;
