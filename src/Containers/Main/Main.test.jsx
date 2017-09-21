8f117ed75f371046d80a8e54b6a8096898b5ebb2
Mike Joyce <mjoyce91@gmail.com>
2017-09-21 19:13:01 -0400

    Handle profile routes

--- src/Containers/Main/Main.test.jsx
+++ src/Containers/Main/Main.test.jsx
@@ -58,4 +58,22 @@ describe('Main', () => {
     </MemoryRouter>);
     expect(main).toBeDefined();
   });
+  it('handles a profile route', () => {
+    const main = TestUtils.renderIntoDocument(<MemoryRouter initialEntries={['/profile']}>
+      <Main />
+    </MemoryRouter>);
+    expect(main).toBeDefined();
+  });
+  it('handles a profile/favorites route', () => {
+    const main = TestUtils.renderIntoDocument(<MemoryRouter initialEntries={['/profile/favorites']}>
+      <Main />
+    </MemoryRouter>);
+    expect(main).toBeDefined();
+  });
+  it('handles a profile/searches route', () => {
+    const main = TestUtils.renderIntoDocument(<MemoryRouter initialEntries={['/profile/searches']}>
+      <Main />
+    </MemoryRouter>);
+    expect(main).toBeDefined();
+  });
 });
