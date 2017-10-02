e05bbe51846a35e569657f7e4c433bb7ff951733
James Seppi <james.seppi@gsa.gov>
2017-10-02 16:16:01 -0500

    [TEST COMMIT] to see if CodeClimate works

--- src/Components/App/App.test.jsx
+++ src/Components/App/App.test.jsx
@@ -6,5 +6,4 @@ import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<MemoryRouter><App /></MemoryRouter>, div);
-  global.document.getElementById = id => id === 'root' && div;
 });
