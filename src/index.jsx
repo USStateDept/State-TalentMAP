76692966b3200c5ae823fbf2fa95f459e62c4509
Mike Joyce <mjoyce91@gmail.com>
2017-10-23 08:44:04 -0400

    Move polyfill to top of index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
+// important: babel-polyfill needs to be first to avoid any errors in IE11
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './sass/styles.scss';
 import App from './Components/App/App';
 
-require('../node_modules/uswds/dist/js/uswds.min.js');
-require('babel-polyfill');
+require('../node_modules/es6-shim/es6-shim.js');
 
 ReactDOM.render((
   <App />
