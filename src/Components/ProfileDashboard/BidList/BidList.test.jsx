eb96513ca76a6eda8594e17190875a1717302b1c
Mike Joyce <mjoyce91@gmail.com>
2017-11-13 08:53:49 -0500

    Pass a bids array to the BidList component test

--- src/Components/ProfileDashboard/BidList/BidList.test.jsx
+++ src/Components/ProfileDashboard/BidList/BidList.test.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
 import BidList from './BidList';
+import bidListObject from '../../../__mocks__/bidListObject';
 
 describe('BidListComponent', () => {
   it('is defined', () => {
-    const wrapper = shallow(<BidList />);
+    const wrapper = shallow(<BidList bids={bidListObject.results} />);
     expect(wrapper).toBeDefined();
   });
 
   it('matches snapshot', () => {
-    const wrapper = shallow(<BidList />);
+    const wrapper = shallow(<BidList bids={bidListObject.results} />);
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
 });
