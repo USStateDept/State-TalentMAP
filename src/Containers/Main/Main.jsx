d225e7d62aa851dea0e7a845eba0031a44f11dd9
Nat Burgwyn <burgwyn@gmail.com>
2017-12-08 14:42:34 -0500

    Setting basename

--- src/Containers/Main/Main.jsx
+++ src/Containers/Main/Main.jsx
@@ -24,7 +24,7 @@ import IndexSagas from '../../index-sagas';
 // Setup the middleware to watch between the Reducers and the Actions
 const sagaMiddleware = createSagaMiddleware();
 
-const history = createHistory();
+const history = createHistory({basename: process.env.PUBLIC_URL});
 
 const middleware = routerMiddleware(history);
 
