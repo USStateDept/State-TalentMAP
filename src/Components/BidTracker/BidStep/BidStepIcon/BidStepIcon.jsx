030c62336671dcde329c970b0982961619f91380
Nat Burgwyn <burgwyn@gmail.com>
2017-12-18 14:38:16 -0500

    Tweaking class assignment

--- src/Components/BidTracker/BidStep/BidStepIcon/BidStepIcon.jsx
+++ src/Components/BidTracker/BidStep/BidStepIcon/BidStepIcon.jsx
@@ -2,16 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
 
+const assignClasses = (isComplete, needsAction, isCurrent) => {
+  const classes = ['number-icon'];
+
+  if (!needsAction) {
+    classes.push('number-icon-incomplete');
+  }
+
+  if (!needsAction && isCurrent) {
+    classes.push('number-icon-is-current-no-action');
+  }
+
+  if (needsAction && isCurrent) {
+    classes.push('number-icon-needs-action');
+  }
+
+  return classes.join(' ');
+};
+
 const BidStepIcon = ({ isComplete, needsAction, isCurrent, number }) => (
   <span className={isComplete ? 'icon-complete' : 'icon-incomplete'}>
     { !isComplete
         ?
           <span
-            className={`number-icon
-              ${!needsAction ? 'number-icon-incomplete' : ''}
-              ${!needsAction && isCurrent ? 'number-icon-is-current-no-action' : ''}
-              ${needsAction && isCurrent ? 'number-icon-needs-action' : ''}
-            `}
+            className={assignClasses(isComplete, needsAction, isCurrent)}
           >
             {number}
           </span> :
