aecfd80f272b40e6c58874a98bbcfaef237ca74e
Mike Joyce <mjoyce91@gmail.com>
2017-12-22 11:23:32 -0500

    Add test, add static dev wrapper to PCS Travel link

--- src/Components/BidTracker/OverlayAlert/ApprovedAlert/ApprovedAlert.jsx
+++ src/Components/BidTracker/OverlayAlert/ApprovedAlert/ApprovedAlert.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import StaticDevContent from '../../../StaticDevContent';
 
+// TODO - need to add PCS Travel link
 const ApprovedAlert = ({ userName }) => (
   <div className="bid-tracker-alert-container bid-tracker-alert-container--approved">
     <div className="top-text">
       {`${userName}, you've been `}<strong>approved</strong> for this position!
       </div>
     <div>
-      <button className="tm-button-transparent">Prepare for Next Steps</button>
+      <StaticDevContent>
+        <button className="tm-button-transparent">Prepare for Next Steps</button>
+      </StaticDevContent>
     </div>
     <div className="sub-text">
       This link will take you to PCS Travel
