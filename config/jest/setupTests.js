915195383065df6bbfaa729b1eba81fdb4ffa6ed
Mike Joyce <mjoyce91@gmail.com>
2017-12-29 16:13:59 -0500

    an -> any

--- config/jest/setupTests.js
+++ config/jest/setupTests.js
@@ -1,5 +1,5 @@
 // Override console.warn() for invalid or failed propTypes by throwing an error
-// when either is met, allowing us to be alerted of an proptype issues.
+// when either is met, allowing us to be alerted of any proptype issues.
 // Based on answer from https://stackoverflow.com/a/29654112/4584189
 const error = console.error;
 const errorWrapper = (message, ...args) => {
