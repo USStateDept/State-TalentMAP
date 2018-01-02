229394148ffcf4be048176a2183faedb96dba022
Mike Joyce <mjoyce91@gmail.com>
2018-01-02 11:20:00 -0500

    Move breakpoints.js to Constants folder, add comment to variables.scss about matching breakpoint.js

--- src/Components/MediaQuery/MediaQuery.jsx
+++ src/Components/MediaQuery/MediaQuery.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MediaQuery from 'react-responsive';
-import breakpoints from './breakpoints';
+import breakpoints from '../../Constants/breakpoints';
 
 const MediaQueryWrapper = ({ breakpoint, widthType, children }) => {
   const mediaProps = {};
