8ceb84f44752da36a3f51cb20667a5e7f5854d87
Mike Joyce <mjoyce91@gmail.com>
2018-01-02 11:25:22 -0500

    Wrap responsive headers with MediaQuery on declaration instead of inside component

--- src/Components/Header/DesktopNav/DesktopNav.jsx
+++ src/Components/Header/DesktopNav/DesktopNav.jsx
@@ -6,75 +6,72 @@ import Inbox from '../Inbox';
 import Notifications from '../Notifications';
 import NavLink from '../NavLink';
 import AccountDropdown from '../../AccountDropdown/AccountDropdown';
-import MediaQuery from '../../MediaQuery';
 
 const DesktopNav = ({ isLoggedIn, shouldShowSearchBar,
 userProfile, logout, toggleSearchVisibility }) => (
-  <MediaQuery widthType="min" breakpoint="screenMdMin">
-    <div className="navigation-container">
-      <div className="nav-link-container header-nav-desktop desktop-nav-only">
-        {
-          isLoggedIn &&
-          <div className={`header-nav-link-container ${shouldShowSearchBar ? 'is-highlighted' : 'is-not-highlighted'}`}>
-            <div className="header-nav-link">
-              <div className="header-nav-link-text search-text">
-                {/* At the time of writing, CodeClimate's version of eslint-a11y-plugin
-                    did not take role="button" into account with the following error: */ }
-                <span // eslint-disable-line jsx-a11y/no-static-element-interactions
-                  tabIndex="0"
-                  role="button"
-                  onClick={toggleSearchVisibility}
-                >
-                  <FontAwesome name="search" /> Search
-                </span>
-              </div>
-            </div>
-          </div>
-        }
-        <NavLink link="/" title="Home" navLinkClass="home-text" />
-        <div className="header-nav-link-container">
+  <div className="navigation-container">
+    <div className="nav-link-container header-nav-desktop desktop-nav-only">
+      {
+        isLoggedIn &&
+        <div className={`header-nav-link-container ${shouldShowSearchBar ? 'is-highlighted' : 'is-not-highlighted'}`}>
           <div className="header-nav-link">
-            <div className="header-nav-link-text about-text">
-              <a href="https://github.com/18F/State-TalentMAP">About</a>
+            <div className="header-nav-link-text search-text">
+              {/* At the time of writing, CodeClimate's version of eslint-a11y-plugin
+                  did not take role="button" into account with the following error: */ }
+              <span // eslint-disable-line jsx-a11y/no-static-element-interactions
+                tabIndex="0"
+                role="button"
+                onClick={toggleSearchVisibility}
+              >
+                <FontAwesome name="search" /> Search
+              </span>
             </div>
           </div>
         </div>
-        <div className="header-nav-link-container">
-          <div className="header-nav-link">
-            <div className="header-nav-link-text feedback-text">
-              <a href="https://github.com/18F/State-TalentMAP/issues">Feedback</a>
-            </div>
+      }
+      <NavLink link="/" title="Home" navLinkClass="home-text" />
+      <div className="header-nav-link-container">
+        <div className="header-nav-link">
+          <div className="header-nav-link-text about-text">
+            <a href="https://github.com/18F/State-TalentMAP">About</a>
           </div>
         </div>
       </div>
-      <div className="header-nav-desktop desktop-nav-only account-notification-container">
-        <div className="header-nav-link-container account-container">
-          <div className="header-nav-link">
-            {
-              isLoggedIn &&
-              <AccountDropdown
-                userProfile={userProfile}
-                logoutRequest={logout}
-              />
-            }
-          </div>
-        </div>
-      </div>
-      <div className="header-nav-desktop desktop-nav-only account-notification-container">
-        <div className="header-nav-link-container notifications-container">
-          <div className="header-nav-link">
-            {
-              isLoggedIn &&
-                <span>
-                  <Inbox />
-                  <Notifications />
-                </span>
-            }
+      <div className="header-nav-link-container">
+        <div className="header-nav-link">
+          <div className="header-nav-link-text feedback-text">
+            <a href="https://github.com/18F/State-TalentMAP/issues">Feedback</a>
           </div>
         </div>
       </div>
     </div>
-  </MediaQuery>
+    <div className="header-nav-desktop desktop-nav-only account-notification-container">
+      <div className="header-nav-link-container account-container">
+        <div className="header-nav-link">
+          {
+            isLoggedIn &&
+            <AccountDropdown
+              userProfile={userProfile}
+              logoutRequest={logout}
+            />
+          }
+        </div>
+      </div>
+    </div>
+    <div className="header-nav-desktop desktop-nav-only account-notification-container">
+      <div className="header-nav-link-container notifications-container">
+        <div className="header-nav-link">
+          {
+            isLoggedIn &&
+              <span>
+                <Inbox />
+                <Notifications />
+              </span>
+          }
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 DesktopNav.propTypes = {
