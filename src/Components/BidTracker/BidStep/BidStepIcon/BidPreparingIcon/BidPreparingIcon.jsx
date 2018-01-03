414abecafe943df49401db8057bdbcf7980e0964
Mike Joyce <mjoyce91@gmail.com>
2018-01-03 14:35:05 -0500

    Fix class mix up with different tooltips

--- src/Components/BidTracker/BidStep/BidStepIcon/BidPreparingIcon/BidPreparingIcon.jsx
+++ src/Components/BidTracker/BidStep/BidStepIcon/BidPreparingIcon/BidPreparingIcon.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import FontAwesome from 'react-fontawesome';
 
 const BidPreparingIcon = () => (
-  <div className="preparing-tooltip-container" style={{ position: 'absolute', left: '50%' }}>
-    <div className="step-tooltip preparing-tooltip" style={{ position: 'relative', left: '-55%', height: '57px' }}>
+  <div className="preparing-tooltip-container">
+    <div className="step-tooltip preparing-tooltip">
       <div className="bid-tracker-preparing-icon-container">
         <FontAwesome name="clipboard" />
       </div>
