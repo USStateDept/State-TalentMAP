79c3cd0d594a97068f8945da1541972fec0d3c20
Mike Joyce <mjoyce91@gmail.com>
2018-01-04 18:14:08 -0500

    Remove Form wrapper from glossary search

--- src/Components/Glossary/GlossarySearch/GlossarySearch.jsx
+++ src/Components/Glossary/GlossarySearch/GlossarySearch.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Form from '../../Form';
 import FieldSet from '../../FieldSet/FieldSet';
 import TextInput from '../../TextInput';
 
 const GlossarySearch = ({ changeText, searchTextValue }) => (
-  <Form className="usa-grid-full glossary-form">
-    <FieldSet
-      className="glossary-fieldset"
-      legend="Enter a keyword to search"
-      legendSrOnly
-    >
-      <TextInput
-        id="glossary-search"
-        label="Enter a keyword to search"
-        changeText={changeText}
-        value={searchTextValue}
-        labelSrOnly
-        placeholder="Search for terms"
-      />
-    </FieldSet>
-  </Form>
+  <FieldSet
+    className="glossary-fieldset"
+    legend="Enter a keyword to search"
+    legendSrOnly
+  >
+    <TextInput
+      id="glossary-search"
+      label="Enter a keyword to search"
+      changeText={changeText}
+      value={searchTextValue}
+      labelSrOnly
+      placeholder="Search for terms"
+    />
+  </FieldSet>
 );
 
 GlossarySearch.propTypes = {
