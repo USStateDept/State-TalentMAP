1b208de0b7591b37b7dbcc2b195f010ee9663d33
Mike Joyce <mjoyce91@gmail.com>
2018-01-11 10:22:19 -0500

    numnber -> number

--- src/Constants/PropTypes.js
+++ src/Constants/PropTypes.js
@@ -162,7 +162,7 @@ export const FAVORITE_POSITION = PropTypes.shape({
 export const FAVORITE_POSITIONS_ARRAY = PropTypes.arrayOf(FAVORITE_POSITION);
 
 export const USER_SKILL_CODE = PropTypes.shape({
-  id: PropTypes.numnber,
+  id: PropTypes.number,
   cone: PropTypes.string,
   code: PropTypes.string,
   description: PropTypes.string,
