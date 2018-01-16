9d0d71ec9460a5b3b8c75a9349dc2c66d2d87f1d
Mike Joyce <mjoyce91@gmail.com>
2018-01-16 10:53:16 -0500

    Get around setState warnings by using componentWillMount and componentWillReceiveProps

--- src/Containers/AuthorizedWrapper/AuthorizedWrapper.jsx
+++ src/Containers/AuthorizedWrapper/AuthorizedWrapper.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const AuthorizedWrapper = ({ isAuthorized, children }) => {
-  const hasAuthorization = isAuthorized();
-  return (
-    <div className="authorized-wrapper">
-      { hasAuthorization && children }
-    </div>
-  );
-};
+class AuthorizedWrapper extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthorized: { value: false },
+    };
+  }
+  componentWillMount() {
+    this.setAuthorization();
+  }
+  componentWillReceiveProps() {
+    this.setAuthorization();
+  }
+  setAuthorization() {
+    const isAuthorized = { value: this.props.isAuthorized() };
+    this.setState({ isAuthorized });
+  }
+  render() {
+    const { isAuthorized } = this.state;
+    return (
+      <div className="authorized-wrapper">
+        { isAuthorized.value && this.props.children }
+      </div>
+    );
+  }
+}
 
 AuthorizedWrapper.propTypes = {
   isAuthorized: PropTypes.func.isRequired,
