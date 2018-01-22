4f3fe47b0131216bc078da696cd03391b7d9aae0
Mike Joyce <mjoyce91@gmail.com>
2018-01-22 10:02:22 -0500

    Remove comments

--- src/Components/ResultsCondensedCardTop/ResultsCondensedCardTop.jsx
+++ src/Components/ResultsCondensedCardTop/ResultsCondensedCardTop.jsx
@@ -1,34 +1,23 @@
 import React from 'react';
 import { POSITION_DETAILS } from '../../Constants/PropTypes';
 
-const ResultsCondensedCardTop = ({ position, /* toggleFavorite, favorites,
-  userProfileFavoritePositionIsLoading, userProfileFavoritePositionHasErrored */ }) => (
-    <div className="usa-grid-full condensed-card-top">
-      <div className="usa-grid-full condensed-card-top-header-container">
-        <div
-          className={
-            'usa-width-two-thirds condensed-card-top-header condensed-card-top-header-left'
-          }
-        >
-          {position.title}
-        </div>
-        <div
-          className="usa-width-one-third condensed-card-top-header condensed-card-top-header-right"
-        >
-          {
-            /* !!favorites && <Favorite
-              compareArray={favorites}
-              onToggle={toggleFavorite}
-              isLoading={userProfileFavoritePositionIsLoading}
-              hasErrored={userProfileFavoritePositionHasErrored}
-              refKey={position.id}
-              hideText
-            /> */
-          }
-          Grade: {position.grade}
-        </div>
+const ResultsCondensedCardTop = ({ position }) => (
+  <div className="usa-grid-full condensed-card-top">
+    <div className="usa-grid-full condensed-card-top-header-container">
+      <div
+        className={
+          'usa-width-two-thirds condensed-card-top-header condensed-card-top-header-left'
+        }
+      >
+        {position.title}
+      </div>
+      <div
+        className="usa-width-one-third condensed-card-top-header condensed-card-top-header-right"
+      >
+        Grade: {position.grade}
       </div>
     </div>
+  </div>
 );
 
 ResultsCondensedCardTop.propTypes = {
