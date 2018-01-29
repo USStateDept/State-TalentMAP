f468e45a5067e6e3c9dd8c47813ba7f77bb670bf
Mike Joyce <mjoyce91@gmail.com>
2018-01-29 09:55:40 -0500

    BidderPortfolioGridItemAdditional -> ItemList

--- src/Components/BidderPortfolio/BidderPortfolioGridItemAdditional/ItemList/ItemList.jsx
+++ src/Components/BidderPortfolio/BidderPortfolioGridItemAdditional/ItemList/ItemList.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ItemListing from './ItemListing';
 
-const BidderPortfolioGridItemAdditional = ({ title, items }) => (
+const ItemList = ({ title, items }) => (
   <div className="usa-grid-full item-list-container">
     <div className="item-list-title">{title}</div>
     <ItemListing items={items} />
   </div>
 );
 
-BidderPortfolioGridItemAdditional.propTypes = {
+ItemList.propTypes = {
   title: PropTypes.string.isRequired,
   items: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
-export default BidderPortfolioGridItemAdditional;
+export default ItemList;
