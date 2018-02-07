0519b7be5c0f14d44126647ae0f5547c4f73eec4
Nat Burgwyn <burgwyn@gmail.com>
2018-02-07 15:36:07 -0500

    Fix test

--- src/Components/HomepagePositionsSection/HomepagePositionsSection.test.jsx
+++ src/Components/HomepagePositionsSection/HomepagePositionsSection.test.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactTestUtils from 'react-dom/test-utils';
+import { MemoryRouter } from 'react-router';
 import HomePagePositionsSection from './HomePagePositionsSection';
 
 describe('HomePagePositionsSection', () => {
   it('renders', () => {
-    const wrapper = ReactTestUtils.renderIntoDocument(<HomePagePositionsSection />);
+    const wrapper = ReactTestUtils.renderIntoDocument(<MemoryRouter><HomePagePositionsSection /></MemoryRouter>);
     expect(wrapper).toBeDefined();
   });
 });
