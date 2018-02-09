1d9cc0c7f389d7688a6f0c99918c8641c267de2f
Nat Burgwyn <burgwyn@gmail.com>
2018-02-09 14:26:29 -0500

    HomePagePositionsList

--- src/Components/HomePagePositionsList/HomePagePositionsList.test.jsx
+++ src/Components/HomePagePositionsList/HomePagePositionsList.test.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import ReactTestUtils from 'react-dom/test-utils';
+import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 import HomePagePositionsList from './HomePagePositionsList';
+import resultsObject from '../../__mocks__/resultsObject';
+import bidListObject from '../../__mocks__/bidListObject';
 
 describe('HomePagePositionsList', () => {
-    it('is defined', () => {
-        const wrapper = ReactTestUtils.renderIntoDocument(<HomePagePositionsList />);
-        expect(wrapper).toBeDefined();
-    });
+  const props = {
+    toggleFavorite: () => {},
+    userProfileFavoritePositionIsLoading: false,
+    userProfileFavoritePositionHasErrored: false,
+    toggleBid: () => {},
+    bidList: bidListObject.results,
+  };
+
+  it('is defined', () => {
+    const wrapper = shallow(<HomePagePositionsList maxLength={3} positions={resultsObject.results} {...props} />);
+    expect(wrapper).toBeDefined();
+  });
+
+  it('displays two rows', () => {
+    // test with 7 positions and a max of 6
+    const maxLength = 6;
+    const positions = Array(7).fill(resultsObject.results[0]);
+
+    const wrapper = shallow(<HomePagePositionsList maxLength={maxLength} positions={positions} {...props} />);
+    expect(wrapper.find('.condensed-card').length).toEqual(maxLength);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
 });
