c3ca286e1de9aa1e4b662e0b7dd1eceb975dadbb
Nat Burgwyn <burgwyn@gmail.com>
2018-02-12 13:42:03 -0500

    Adding PUBLIC_URL

--- src/server.js
+++ src/server.js
@@ -4,10 +4,12 @@ const path = require('path');
 // define the url consuming the API, provide local dev value as default
 const API_URL = process.env.API_URL || 'http://localhost:8000/api/v1';
 
+const PUBLIC_URL = process.env.PUBLIC_URL || '/talentmap/';
+
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(PUBLIC_URL, express.static(path.join(__dirname, '../build')));
 
 app.get('*', (request, response) => {
   response.sendFile(path.resolve(__dirname, '../build', 'index.html'));
