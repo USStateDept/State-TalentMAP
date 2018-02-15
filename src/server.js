435a41a267d45c4be51aade9134f437edbc1bc9b
Mike Joyce <mjoyce91@gmail.com>
2018-02-15 09:23:03 -0500

    Set OBC url redirect

--- src/server.js
+++ src/server.js
@@ -10,6 +10,10 @@ const API_ROOT = process.env.API_ROOT || 'http://localhost:8000';
 // define the prefix for the application
 const PUBLIC_URL = process.env.PUBLIC_URL || '/talentmap/';
 
+// define the OBC root url
+// example: https://www.obcurl.gov
+const OBC_URL = process.env.OBC_URL;
+
 // application port
 const port = process.env.PORT || 3000;
 
@@ -21,6 +25,13 @@ app.use(PUBLIC_URL, express.static(STATIC_PATH));
 // saml2 metadata
 app.get(`${PUBLIC_URL}metadata/`, (request, response) => {
   response.redirect(`${API_ROOT}/saml2/metadata`);
+});
+
+// OBC redirect
+app.get(`${PUBLIC_URL}obc/:id`, (request, response) => {
+  // set the id passed in the route and pass it to the redirect
+  const id = request.params.id;
+  response.redirect(`${OBC_URL}/${id}`);
 });
 
 app.get('*', (request, response) => {
