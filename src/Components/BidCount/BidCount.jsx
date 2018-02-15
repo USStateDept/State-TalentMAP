c23ed5fac760d6cbf150ebf2a185f8c03ad08e3b
Mike Joyce <mjoyce91@gmail.com>
2018-02-15 16:14:40 -0500

    Add additional styles

--- src/Components/BidCount/BidCount.jsx
+++ src/Components/BidCount/BidCount.jsx
@@ -4,7 +4,7 @@ import { BID_STATISTICS_OBJECT } from '../../Constants/PropTypes';
 
 const BidCount = ({ bidStatistics }) => (
   <div>
-    <div className="bid-count-label" id="bid-counts">Bid Counts:</div>
+    <div className="bid-count-label" id="bid-counts">Bid Count:</div>
     {/* set an aria-labelledby so that screen readers understand the purpose of the list */}
     <ul className="bid-count-list" aria-labelledby="bid-counts">
       <BidCountNumber type="totalBids" number={bidStatistics.total_bids || 0} />
