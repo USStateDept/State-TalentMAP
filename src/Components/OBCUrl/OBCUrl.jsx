a9c49542828e45b21a5ad28d1b73103a9ab35704
Mike Joyce <mjoyce91@gmail.com>
2018-02-16 13:51:02 -0500

    /obc/:id -> /obc/post/:id (#1438)

--- src/Components/OBCUrl/OBCUrl.jsx
+++ src/Components/OBCUrl/OBCUrl.jsx
@@ -5,7 +5,7 @@ const OBCUrl = ({ id }) => (
   // This is not an internal react route. It references a route that
   // should be defined on the same server that is serving the react application.
   // We open it in a new tab.
-  <a href={`/obc/${id}`} rel="noopener noreferrer" target="_blank">Post details</a>
+  <a href={`/obc/post/${id}`} rel="noopener noreferrer" target="_blank">Post details</a>
 );
 
 OBCUrl.propTypes = {
