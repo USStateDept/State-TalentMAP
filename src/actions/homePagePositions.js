343b57fa4f88c936e5d0620287f601ad6611bef0
Mike Joyce <mjoyce91@gmail.com>
2018-02-16 14:47:21 -0500

    Add tests

--- src/actions/homePagePositions.js
+++ src/actions/homePagePositions.js
@@ -4,6 +4,13 @@ import { fetchUserToken } from '../utilities';
 import { USER_SKILL_CODE_POSITIONS, USER_GRADE_RECENT_POSITIONS, SERVICE_NEED_POSITIONS,
 RECENTLY_POSTED_POSITIONS, FAVORITED_POSITIONS } from '../Constants/PropTypes';
 
+// Export our queries so that we can consistently test them.
+export const HIGHLIGHTED_POSITIONS_QUERY = 'highlighted/?limit=3';
+export const GET_SKILL_CODE_POSITIONS_QUERY = skillCodes => `?skill__in=${skillCodes}&limit=3`;
+export const FAVORITE_POSITIONS_QUERY = 'favorites/?limit=3';
+export const GET_GRADE_POSITIONS_QUERY = grade => `?grade__code__in=${grade}&limit=3&ordering=description__date_created`;
+export const RECENTLY_POSTED_POSITIONS_QUERY = '?limit=3&ordering=description__date_created';
+
 export function homePagePositionsHasErrored(bool) {
   return {
     type: 'HOME_PAGE_POSITIONS_HAS_ERRORED',
@@ -38,32 +45,36 @@ export function homePagePositionsFetchData(skills = [], grade = null) {
 
     // configure queries that match with properties in resultsTypes
     let queryTypes = [
-      { name: SERVICE_NEED_POSITIONS, query: 'highlighted/?limit=3' },
+      { name: SERVICE_NEED_POSITIONS, query: HIGHLIGHTED_POSITIONS_QUERY },
     ];
 
     // Search for positions that match the user's skill, if it exists.
     // Otherwise, search for positions with skill code 0060.
-    if (skills.length) {
+    if (skills && skills.length) {
       const ids = skills.map(s => s.id);
       const querySkillCodes = ids.join(',');
-      queryTypes.push({ name: USER_SKILL_CODE_POSITIONS, query: `?skill__in=${querySkillCodes}&limit=3` });
+      queryTypes.push(
+        { name: USER_SKILL_CODE_POSITIONS, query: GET_SKILL_CODE_POSITIONS_QUERY(querySkillCodes) },
+      );
     } else {
       // delete the property we're falling back against from resultsTypes and queryTypes
       delete resultsTypes[USER_SKILL_CODE_POSITIONS];
       queryTypes = queryTypes.filter(obj => obj.name !== USER_SKILL_CODE_POSITIONS);
       // return a generic query
-      queryTypes.push({ name: FAVORITED_POSITIONS, query: 'favorites/?limit=3' });
+      queryTypes.push({ name: FAVORITED_POSITIONS, query: FAVORITE_POSITIONS_QUERY });
     }
 
     // Do the same thing for grades. Set grade 3 to the default if the user does not have a grade.
     if (grade != null) {
-      queryTypes.push({ name: USER_GRADE_RECENT_POSITIONS, query: `?grade__code__in=${grade}&limit=3&ordering=description__date_created` });
+      queryTypes.push(
+        { name: USER_GRADE_RECENT_POSITIONS, query: GET_GRADE_POSITIONS_QUERY(grade) },
+      );
     } else {
       // delete the property we're falling back against
       delete resultsTypes[USER_GRADE_RECENT_POSITIONS];
       queryTypes = queryTypes.filter(obj => obj.name !== USER_GRADE_RECENT_POSITIONS);
       // return a generic query
-      queryTypes.push({ name: RECENTLY_POSTED_POSITIONS, query: '?limit=3&ordering=description__date_created' });
+      queryTypes.push({ name: RECENTLY_POSTED_POSITIONS, query: RECENTLY_POSTED_POSITIONS_QUERY });
     }
 
     // create a promise with all the queries we defined
--- src/actions/homePagePositions.test.js
+++ src/actions/homePagePositions.test.js
@@ -1,35 +1,98 @@
 import { setupAsyncMocks } from '../testUtilities/testUtilities';
-import * as actions from './homePagePositions';
+import { HIGHLIGHTED_POSITIONS_QUERY, GET_SKILL_CODE_POSITIONS_QUERY, FAVORITE_POSITIONS_QUERY,
+  GET_GRADE_POSITIONS_QUERY, RECENTLY_POSTED_POSITIONS_QUERY,
+  homePagePositionsFetchData, homePagePositionsIsLoading } from './homePagePositions';
 import resultsObject from '../__mocks__/resultsObject';
 
 const { mockStore, mockAdapter } = setupAsyncMocks();
 
+// set test values
+const TEST_SKILL_CODES = [{ id: 100 }, { id: 200 }];
+const TEST_SKILL_CODES_STRING = TEST_SKILL_CODES.join(',');
+const TEST_GRADE_CODE = '03';
+
+// set queries to test against
+const queries = [
+  HIGHLIGHTED_POSITIONS_QUERY,
+  GET_SKILL_CODE_POSITIONS_QUERY(TEST_SKILL_CODES_STRING),
+  FAVORITE_POSITIONS_QUERY,
+  GET_GRADE_POSITIONS_QUERY('03'),
+  RECENTLY_POSTED_POSITIONS_QUERY,
+];
+
 describe('async actions', () => {
   beforeEach(() => {
-    mockAdapter.onGet('http://localhost:8000/api/v1/position/?ordering=description__date_created&limit=6').reply(200,
-      resultsObject,
-    );
+    mockAdapter.reset();
 
-    mockAdapter.onGet('http://localhost:8000/api/v1/position/highlighted/?limit=3').reply(200,
-      resultsObject,
-    );
-
-    mockAdapter.onGet('http://localhost:8000/api/v1/position/?skill__in=20&limit=3').reply(200,
-      resultsObject,
-    );
-
-    mockAdapter.onGet('http://localhost:8000/api/v1/position/?grade=3&limit=3').reply(200,
-      resultsObject,
+    queries.forEach(query =>
+      mockAdapter.onGet(`http://localhost:8000/api/v1/position/${query}`).reply(200,
+        resultsObject,
+      ),
     );
   });
 
-  it('can fetch a position', (done) => {
+  it('fetches positions when skill and grade are not defined', (done) => {
     const store = mockStore({ results: [] });
 
     const f = () => {
       setTimeout(() => {
-        store.dispatch(actions.homePagePositionsFetchData());
-        store.dispatch(actions.homePagePositionsIsLoading());
+        store.dispatch(homePagePositionsFetchData());
+        store.dispatch(homePagePositionsIsLoading());
+        done();
+      }, 0);
+    };
+    f();
+  });
+
+  it('fetches positions when skill and grade exist', (done) => {
+    const store = mockStore({ results: [] });
+
+    const f = () => {
+      setTimeout(() => {
+        store.dispatch(homePagePositionsFetchData(TEST_SKILL_CODES, TEST_GRADE_CODE));
+        store.dispatch(homePagePositionsIsLoading());
+        done();
+      }, 0);
+    };
+    f();
+  });
+
+  it('handles errors fetching positions when skill and grade are not defined', (done) => {
+    const store = mockStore({ results: [] });
+
+    mockAdapter.reset();
+
+    queries.forEach(query =>
+      mockAdapter.onGet(`http://localhost:8000/api/v1/position/${query}`).reply(404,
+        null,
+      ),
+    );
+
+    const f = () => {
+      setTimeout(() => {
+        store.dispatch(homePagePositionsFetchData());
+        store.dispatch(homePagePositionsIsLoading());
+        done();
+      }, 0);
+    };
+    f();
+  });
+
+  it('handles errors fetching positions when skill and grade exist', (done) => {
+    const store = mockStore({ results: [] });
+
+    mockAdapter.reset();
+
+    queries.forEach(query =>
+      mockAdapter.onGet(`http://localhost:8000/api/v1/position/${query}`).reply(404,
+        null,
+      ),
+    );
+
+    const f = () => {
+      setTimeout(() => {
+        store.dispatch(homePagePositionsFetchData(TEST_SKILL_CODES, TEST_GRADE_CODE));
+        store.dispatch(homePagePositionsIsLoading());
         done();
       }, 0);
     };
