f9dbae046e37534cfa6350ed73493d4748b0c1b5
Mike Joyce <mjoyce91@gmail.com>
2018-02-21 11:37:27 -0500

    Remove unneeded sections from profile dashboard

--- src/Components/ProfileDashboard/UserProfile/UserProfileGeneralInformation/UserProfileGeneralInformation.jsx
+++ src/Components/ProfileDashboard/UserProfile/UserProfileGeneralInformation/UserProfileGeneralInformation.jsx
@@ -4,17 +4,15 @@ import { USER_PROFILE } from '../../../../Constants/PropTypes';
 import SectionTitle from '../../SectionTitle';
 import InformationDataPoint from '../../InformationDataPoint';
 import Status from '../Status';
-import EditProfile from '../EditProfile';
 import ProfilePicture from '../../../ProfilePicture';
 import StaticDevContent from '../../../StaticDevContent';
 import SkillCodeList from '../../../SkillCodeList';
 
-const UserProfileGeneralInformation = ({ userProfile, showEditLink, useGroup }) => (
+const UserProfileGeneralInformation = ({ userProfile, useGroup }) => (
   <div className="current-user-top current-user-section-container">
     <div className="section-padded-inner-container">
       <Status />
       <ProfilePicture />
-      { showEditLink && <EditProfile /> }
       <SectionTitle small title={`${userProfile.user.last_name}, ${userProfile.user.first_name}`} className="current-user-name" />
       {
         useGroup ?
@@ -36,12 +34,10 @@ const UserProfileGeneralInformation = ({ userProfile, showEditLink, useGroup }) 
 
 UserProfileGeneralInformation.propTypes = {
   userProfile: USER_PROFILE.isRequired,
-  showEditLink: PropTypes.bool,
   useGroup: PropTypes.bool,
 };
 
 UserProfileGeneralInformation.defaultProps = {
-  showEditLink: true,
   useGroup: false,
 };
 
