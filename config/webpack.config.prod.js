56b13585cee5fac4aa9e2a8eb66cdecbc4506abc
Mike Joyce <mjoyce91@gmail.com>
2018-02-23 12:09:49 -0500

    Add inclusion

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -158,6 +158,7 @@ module.exports = {
       // Process JS with Babel.
       {
         test: /\.(js|jsx)$/,
+        include: paths.appSrc,
         loader: require.resolve('babel-loader'),
 
       },
