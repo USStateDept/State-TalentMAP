ade5cfc8c57e1214a9cc7a3c8197b521f1f7322a
578556 <natburgwyn@c02w30gfhtdg.users.bah.com>
2018-02-26 15:33:10 -0500

    Moving routes

--- src/Containers/Routes/Routes.jsx
+++ src/Containers/Routes/Routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import routesArray from './RoutesMap';
+import routesArray from '../../routes';
 
 const Routes = props => (
   <Switch {...props}>
