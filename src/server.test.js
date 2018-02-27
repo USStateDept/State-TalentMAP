ccf97128ee1dcd89261497ac2240670027f9e104
578556 <natburgwyn@c02w30gfhtdg.users.bah.com>
2018-02-27 13:52:32 -0500

    Adding tests

--- src/server.test.js
+++ src/server.test.js
@@ -6,16 +6,36 @@ describe('proxy server routes', () => {
   beforeEach(() => {
     server = proxyServer;
   });
+
   afterEach(() => {
     // close the server after each test so that jest exits
     server.close();
   });
+
   it('responds to /talentmap/', (done) => {
     request(server).get('/talentmap/').expect(200, done);
   });
+
+  it('responds to /login', (done) => {
+    request(server).get('/talentmap/login').expect(302, done);
+  });
+
+  it('responds to /logout', (done) => {
+    request(server).get('/talentmap/logout').expect(302, done);
+  });
+
+  it('responds to /obc/country', (done) => {
+    request(server).get('/talentmap/obc/country/42').expect(302, done);
+  });
+
+  it('responds to /obc/post', (done) => {
+    request(server).get('/talentmap/obc/post/42').expect(302, done);
+  });
+
   it('responds to wildcard routes', (done) => {
     request(server).get('/file-does-not-exits.html').expect(404, done);
   });
+
   // this route depends on default environment variables
   it('redirects on /talentmap/metadata', (done) => {
     request(server).get('/talentmap/metadata').expect(302, done);
