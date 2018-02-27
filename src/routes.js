daae618bebf290d8f3c3272bfcc0aae8c34a0214
578556 <natburgwyn@C02W30GFHTDG.local>
2018-02-26 21:39:04 -0500

    Remove /login Add NODE_ENV Raw parser Implementing helmet Moving to /logout
    ```
    Rebased onto 660eeaeb
      squash e7bb3761 Adding helmet
      squash af812be5 Implementing helmet
      squash 1780883d Moving to /logout
      squash f6840e9e Adding /logout route
    	squash d76dd9ee 🐛 Fix redirect
    ```
    
    ```
    pick cb28be21 Disabling sagas
    squash 371af81d Adding axios
    squash ccf97128 Adding tests
    
    pick f68606b0 Using body-parser, reverting to 307 redirect
    ```

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 const routesArray = [
   { path: '/login', componentName: 'Login' },
   { path: '/', exact: true, componentName: 'Home', pageTitle: 'Home' },
-  { path: '/profile', componentName: 'Profile', pageTitle: 'Profile' },
   { path: '/results', componentName: 'Results', pageTitle: 'Search Results' },
+  { path: '/profile', componentName: 'Profile', pageTitle: 'Profile' },
   { path: '/details/:id', componentName: 'Position', pageTitle: 'Position details' },
+  { path: '/post/:id', componentName: 'Post' },
   { path: '/compare/:ids', componentName: 'Compare', pageTitle: 'Compare Positions' },
   { path: '/about', exact: true, componentName: 'About', pageTitle: 'About' },
   { path: '/tokenValidation', componentName: 'Login', pageTitle: 'Token Validation' },
