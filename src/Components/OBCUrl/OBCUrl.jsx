d24803139b8c6a7c92e145e4d46cffec77612226
578556 <natburgwyn@C02W30GFHTDG.local>
2018-02-27 22:36:53 -0500

    Updating label

--- src/Components/OBCUrl/OBCUrl.jsx
+++ src/Components/OBCUrl/OBCUrl.jsx
@@ -3,20 +3,23 @@ import PropTypes from 'prop-types';
 
 const OBCUrl = ({ id, type }) => {
   let url;
+  let label; // link text value
   // define the URL according to the type
   switch (type) {
     case 'country':
       url = `/obc/country/${id}`;
+      label = 'Country';
       break;
     default:
     case 'post':
       url = `/obc/post/${id}`;
+      label = 'Post';
   }
   return (
     // This is not an internal react route. It references a route that
     // should be defined on the same server that is serving the react application.
     // We open it in a new tab.
-    <a href={url} rel="noopener noreferrer" target="_blank">Post details</a>
+    <a href={url} rel="noopener noreferrer" target="_blank">{label} details</a>
   );
 };
 
