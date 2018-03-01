b7247c46372e95820fba7f4d657d1e24556f7870
578556 <natburgwyn@C02W30GFHTDG.local>
2018-02-28 22:29:05 -0500

    Intial commit

--- src/Components/Spinner/Spinner.jsx
+++ src/Components/Spinner/Spinner.jsx
@@ -1,36 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// uses https://design.cms.gov/components/spinner/
+import center from '../../assets/spinner/center.svg';
+import middle from '../../assets/spinner/middle.svg';
+import outer from '../../assets/spinner/outer.svg';
 
-const Spinner = ({ type, size, color, filled, inverse }) => (
+const Spinner = ({ type }) => (
   <div className={`tm-spinner tm-spinner-${type}`}>
-    <span
-      className={
-        `ds-c-spinner
-        ${size ? `ds-c-spinner--${size}` : ''}
-        ${filled ? 'ds-c-spinner--filled' : ''}
-        ${inverse ? 'tm-spinner-color-inverted' : ''}
-        ${color ? `ds-u-color--${color}` : ''}`
-      }
-    />
+    <img className="center" alt="center" src={center} />
+    <img className="middle" alt="middle" src={middle} />
+    <img className="outer" alt="outer" src={outer} />
   </div>
 );
 
 Spinner.propTypes = {
   type: PropTypes.string, // user defined classes stored elsewhere using "tm-spinner-" as a prefix
-  size: PropTypes.oneOf(['small', 'big', '']),
-  color: PropTypes.oneOf(['primary', 'success', 'muted', '']),
-  filled: PropTypes.bool,
-  inverse: PropTypes.bool,
 };
 
 Spinner.defaultProps = {
   type: '',
-  size: undefined,
-  color: undefined,
-  filled: false,
-  inverse: false,
 };
 
 export default Spinner;
--- src/Components/Spinner/Spinner.test.jsx
+++ src/Components/Spinner/Spinner.test.jsx
@@ -15,31 +15,4 @@ describe('Spinner', () => {
     expect(toJSON(spinner)).toMatchSnapshot();
     expect(spinner.find(`.tm-spinner-${type}`)).toBeDefined();
   });
-
-  it('can accept the size prop', () => {
-    const size = 'big';
-    const spinner = shallow(<Spinner size={size} />);
-    expect(toJSON(spinner)).toMatchSnapshot();
-    expect(spinner.find(`.ds-c-spinner--${size}`)).toBeDefined();
-  });
-
-  it('can accept the color prop', () => {
-    const color = 'primary';
-    const spinner = shallow(<Spinner color={color} />);
-    expect(toJSON(spinner)).toMatchSnapshot();
-    expect(spinner.find(`.ds-u-color--${color}`)).toBeDefined();
-  });
-
-  it('can accept the filled prop', () => {
-    const spinner = shallow(<Spinner filled />);
-    expect(toJSON(spinner)).toMatchSnapshot();
-    expect(spinner.find('.ds-c-spinner--filled')).toBeDefined();
-  });
-
-  it('can accept the inverse prop', () => {
-    const spinner = shallow(<Spinner inverse />);
-    expect(toJSON(spinner)).toMatchSnapshot();
-    expect(spinner.find('.ds-u-fill--background-inverse')).toBeDefined();
-    expect(spinner.find('.ds-u-color--base-inverse')).toBeDefined();
-  });
 });
