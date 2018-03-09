8979bebcdb178155299c1a279449f55f7f5af67f
Kevin Chiu <chiu_kevin@bah.com>
2018-03-09 14:26:57 -0500

    Add OBC url to position and post details pages

---
+++ src/Components/OBCUrl/OBCUrl.test.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import OBCUrl from './OBCUrl';
+
+describe('OBCUrlComponent', () => {
+  it('is defined', () => {
+    const wrapper = shallow(<OBCUrl
+      id={5}
+    />);
+    expect(wrapper).toBeDefined();
+  });
+
+  it('forms the url', () => {
+    const wrapper = shallow(<OBCUrl
+      id={5}
+    />);
+    expect(wrapper.find('a').props().href).toBe('/obc/5');
+  });
+
+  it('is defined when id is a string', () => {
+    const wrapper = shallow(<OBCUrl
+      id="5A"
+    />);
+    expect(wrapper).toBeDefined();
+  });
+
+  it('matches snapshot', () => {
+    const wrapper = shallow(<OBCUrl
+      id={5}
+    />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  it('matches snapshot when id is a string', () => {
+    const wrapper = shallow(<OBCUrl
+      id="5A"
+    />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+});
