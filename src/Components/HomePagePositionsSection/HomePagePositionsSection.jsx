ca6978a8420b1a776f8e19ff6d92a0b883afdd1e
burgwyn <burgwyn@gmail.com>
2018-03-15 23:15:10 -0400

    HomePagePositionsSection - title as h2

--- src/Components/HomePagePositionsSection/HomePagePositionsSection.jsx
+++ src/Components/HomePagePositionsSection/HomePagePositionsSection.jsx
@@ -39,10 +39,10 @@ const HomePagePositionsSection = ({ title, icon, viewMoreLink, positions, toggle
     <div className="usa-grid-full positions-section">
       <PositionsSectionTitle
         title={
-          <span className="positions-section-title">
+          <h2 className="positions-section-title">
             <FontAwesome name={icon} />
             {title}
-          </span>
+          </h2>
         }
         viewMoreLink={viewMoreLink}
       />
