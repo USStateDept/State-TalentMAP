2e197e02ab7815c121e0d505df043de1a2926cbc
Kevin Chiu <chiu_kevin@bah.com>
2018-03-19 04:06:45 -0400

    - Created reusable definition wrappers: `DefinitionList`, `Definition` - Created unit tests for all component use cases - Added `_lists.scss` and `@import` to scss entry file

--- src/Components/DefinitionList/Definition/Definition.jsx
+++ src/Components/DefinitionList/Definition/Definition.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { keys, omit } from 'lodash';
 
-const ResultsCardDataItem = ({ description, text }) => (
-  <div>
-    <strong>{description}</strong> {text}
-  </div>
-);
-
-ResultsCardDataItem.propTypes = {
-  description: PropTypes.string.isRequired,
-  text: PropTypes.node.isRequired,
+const defaults = {
+  term: '',
+  definition: '',
 };
 
-export default ResultsCardDataItem;
+const Definition = (props) => {
+  const options = omit(props, keys(defaults));
+  let { term, definition } = props;
+
+  term = (term || '').length && `${term}:`;
+  definition = definition || '';
+
+  return (
+    <div {...options}>
+      <dt>{term}</dt>
+      <dd>{definition}</dd>
+    </div>
+  );
+};
+
+Definition.propTypes = {
+  term: PropTypes.string,
+  definition: PropTypes.string,
+};
+
+Definition.defaultProps = defaults;
+
+export default Definition;
