d008bfd177a89a6d1f6c0c04f770186a4f37832d
Kevin Chiu <chiu_kevin@bah.com>
2018-03-19 04:06:45 -0400

    - Created reusable definition wrappers: `DefinitionList`, `Definition` - Created unit tests for all component use cases - Added `_lists.scss` and `@import` to scss entry file

--- src/Components/DefinitionList/DefinitionList.test.jsx
+++ src/Components/DefinitionList/DefinitionList.test.jsx
@@ -1,23 +1,64 @@
-import { shallow } from 'enzyme';
 import React from 'react';
+import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
-import ResultsCardDataItem from './ResultsCardDataItem';
+import { keys, map, zipObject } from 'lodash';
+import DefinitionList, { Definition } from './';
 
-describe('ResultsCardDataItemComponent', () => {
-  const title = 'title';
-  const items = [
-    { description: 'test', text: 'text' },
-  ];
+const mock = [
+  { term: 'Term #1', definition: 'Definition #1' },
+  { term: 'Term #2', definition: 'Definition #2' },
+  { term: 'Term #3', definition: 'Definition #3' },
+  { term: 'Term #4', definition: 'Definition #4' },
+];
 
-  it('can receive props', () => {
-    const wrapper =
-      shallow(<ResultsCardDataItem title={title} items={items} />);
-    expect(wrapper.instance().props.title).toBe(title);
+const mock$ = () => (
+  <DefinitionList>
+    <Definition {...mock[0]} />
+    <Definition {...mock[1]} />
+    <Definition {...mock[2]} />
+    <Definition {...mock[3]} />
+  </DefinitionList>
+);
+
+describe('DefinitionList', () => {
+  const selector = '.definitions';
+
+  it('can receive children prop', () => {
+    const wrapper = shallow(mock$());
+    expect(wrapper.find(selector).children('Definition')).toHaveLength(4);
+  });
+
+  it('can receive `items` as a collection', () => {
+    const wrapper = shallow(<DefinitionList items={mock} />);
+    expect(wrapper.find(selector).children('Definition')).toHaveLength(4);
+  });
+
+  it('can receive `items` as an object of key/values', () => {
+    const items = zipObject(
+      map(mock, 'term'),
+      map(mock, 'definition')
+    );
+
+    const wrapper = shallow(<DefinitionList items={items} />);
+    expect(wrapper.find(selector).children('Definition')).toHaveLength(4);
+  });
+
+  it('can recieve other props such as `className` and `id`', () => {
+    const props = {
+      id: 'definitions',
+      className: 'definitions-theme-1',
+    };
+
+    const wrapper = shallow(
+      <DefinitionList {...props} />
+    );
+
+    expect(wrapper.find(`#${props.id}`).exists()).toBe(true);
+    expect(wrapper.find(`.${props.className}`).exists()).toBe(true);
   });
 
   it('matches snapshot', () => {
-    const wrapper =
-      shallow(<ResultsCardDataItem title={title} items={items} />);
+    const wrapper = shallow(mock$());
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
 });
