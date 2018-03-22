baa90939434eaa586bf4ae38bc9f428cedf8cd78
burgwyn <burgwyn@gmail.com>
2018-03-21 21:06:56 -0400

    Add initals prop to Avatar as SFC

--- src/Components/Avatar/Avatar.jsx
+++ src/Components/Avatar/Avatar.jsx
@@ -1,57 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { EMPTY_FUNCTION } from '../../Constants/PropTypes';
+import InteractiveElement from '../InteractiveElement';
 
-export default class Avatar extends Component {
-  static propTypes = {
-    firstName: PropTypes.string,
-    lastName: PropTypes.string,
-    className: PropTypes.string,
-    onClick: PropTypes.func,
-  };
+const propTypes = {
+  initials: PropTypes.string,
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+};
 
-  static defaultProps = {
-    firstName: '',
-    lastName: '',
-    className: '',
-    onClick: EMPTY_FUNCTION,
-  };
+const defaultProps = {
+  initials: '',
+  firstName: '',
+  lastName: '',
+  className: '',
+  onClick: EMPTY_FUNCTION,
+};
 
-  get caption() {
-    const value = [
-      this.props.firstName,
-      this.props.lastName,
-    ];
+const Avatar = ({ initials, firstName, lastName, className, onClick }) => (
+  <InteractiveElement className={className} onClick={onClick} role="img" aria-label={`${firstName} ${lastName}`}>
+    <div className="tm-avatar">{initials}</div>
+  </InteractiveElement>
+);
 
-    return this.toText(value);
-  }
+Avatar.propTypes = propTypes;
 
-  get value() {
-    const value = [
-      this.getInitial(this.props.firstName),
-      this.getInitial(this.props.lastName),
-    ];
+Avatar.defaultProps = defaultProps;
 
-    return this.toText(value, '');
-  }
-
-  getInitial = value => (value || '').charAt(0);
-  toText = (text, delimiter = ' ') => text.join(delimiter).trim();
-
-  render() {
-    const alt = this.caption;
-    const text = this.value;
-    const props = {
-      className: this.props.className,
-      onClick: this.props.onClick,
-      role: 'img',
-      'aria-label': alt,
-    };
-
-    return (
-      <span {...props}>
-        <div className="tm-avatar">{text}</div>
-      </span>
-    );
-  }
-}
+export default Avatar;
