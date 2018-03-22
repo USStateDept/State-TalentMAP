f2bf4ec6ef8460ac9c4664571c6fe9e05c21f7b9
Mike Joyce <mjoyce91@gmail.com>
2018-03-22 18:01:36 -0400

    Fix eslint warnings

--- src/actions/autocomplete/missionAutocomplete.test.js
+++ src/actions/autocomplete/missionAutocomplete.test.js
@@ -1,7 +1,7 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import api from '../../api';
 import MockAdapter from 'axios-mock-adapter';
+import api from '../../api';
 import * as actions from './missionAutocomplete';
 
 const middlewares = [thunk];
