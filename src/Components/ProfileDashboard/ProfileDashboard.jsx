d8d03d90d4708be891bba0b8bb0affbf9a7bb2f6
Nat Burgwyn <>
2018-03-24 10:38:13 -0400

    Clean up merge issues

--- src/Components/ProfileDashboard/ProfileDashboard.jsx
+++ src/Components/ProfileDashboard/ProfileDashboard.jsx
@@ -1,60 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { USER_PROFILE, FAVORITE_POSITIONS_ARRAY } from '../../Constants/PropTypes';
+import {
+  USER_PROFILE,
+  FAVORITE_POSITIONS_ARRAY,
+} from '../../Constants/PropTypes';
 import UserProfile from './UserProfile';
 import Spinner from '../Spinner';
 import BidListComingSoon from './BidListComingSoon';
 import Favorites from './Favorites';
 import SavedSearches from './SavedSearches/SavedSearchesWrapper';
 
-const ProfileDashboard = ({ userProfile, isLoading, 
-  favoritePositions, favoritePositionsIsLoading }) => (
-    <div className="usa-grid-full user-dashboard user-dashboard-main profile-content-inner-container">
-      {
-        (isLoading || favoritePositionsIsLoading) ?
-            <Spinner type="homepage-position-results" size="big" />
-          :
-            <div className="usa-grid-full">
-              <div className="hello-greeting">
-                {
-                  `Hello, ${userProfile.display_name}`
-                }
-              </div>
-              <div className="usa-grid-full">
-                <div className="hello-greeting">
-                  {
-                    `Hello, ${userProfile.display_name}`
-                  }
-                </div>
-                <div className="usa-grid-full">
-                  <div
-                    className={`usa-width-five-twelfths user-dashboard-section-container
+const ProfileDashboard = ({
+  userProfile,
+  isLoading,
+  favoritePositions,
+  favoritePositionsIsLoading,
+}) => (
+  <div className="usa-grid-full user-dashboard user-dashboard-main profile-content-inner-container">
+    {isLoading || favoritePositionsIsLoading ? (
+      <Spinner type="homepage-position-results" size="big" />
+    ) : (
+      <div className="usa-grid-full">
+        <div className="hello-greeting">
+          {`Hello, ${userProfile.display_name}`}
+        </div>
+        <div className="usa-grid-full">
+          <div
+            className={`usa-width-five-twelfths user-dashboard-section-container
                         user-dashboard-column-1`}
-                  >
-                    <div className="usa-width-one-whole user-dashboard-section current-user-section">
-                      <UserProfile userProfile={userProfile} />
-                    </div>
-                    <div className="usa-width-one-whole user-dashboard-section">
-                      <BidListComingSoon />
-                    </div>
-                  </div>
-                  <div
-                    className={`usa-width-seven-twelfths user-dashboard-section-container
-                        user-dashboard-column-2`}
-                  >
-                    <div className="usa-width-one-whole user-dashboard-section favorites-section">
-                      <SavedSearches />
-                    </div>
-                    <div className="usa-width-one-whole user-dashboard-section favorites-section">
-                      <Favorites favorites={favoritePositions} />
-                    </div>
-                  </div>
-                </div>
-              </div>
+          >
+            <div className="usa-width-one-whole user-dashboard-section current-user-section">
+              <UserProfile userProfile={userProfile} />
             </div>
-      }
-    </div>
-  );
+            <div className="usa-width-one-whole user-dashboard-section">
+              <BidListComingSoon />
+            </div>
+          </div>
+          <div
+            className={`usa-width-seven-twelfths user-dashboard-section-container
+                        user-dashboard-column-2`}
+          >
+            <div className="usa-width-one-whole user-dashboard-section favorites-section">
+              <SavedSearches />
+            </div>
+            <div className="usa-width-one-whole user-dashboard-section favorites-section">
+              <Favorites favorites={favoritePositions} />
+            </div>
+          </div>
+        </div>
+      </div>
+    )}
+  </div>
+);
 
 ProfileDashboard.propTypes = {
   userProfile: USER_PROFILE.isRequired,
