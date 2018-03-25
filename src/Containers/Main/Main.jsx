eb24fa4c68aa3930542ec8b8a9bee1504ebfb83f
Nat Burgwyn <>
2018-03-25 11:52:31 -0400

    Set base properties

--- src/Containers/Main/Main.jsx
+++ src/Containers/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
+import Helmet from 'react-helmet';
 import Routes from '../../Containers/Routes/Routes';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
@@ -10,6 +11,7 @@ import FeedbackButton from '../../Containers/FeedbackButton';
 import AuthorizedWrapper from '../../Containers/AuthorizedWrapper';
 import checkIndexAuthorization from '../../lib/check-auth';
 import { store, history } from '../../store';
+import { getApplicationPath, getAssetPath } from '../../utilities';
 
 const isAuthorized = () => checkIndexAuthorization(store);
 
@@ -17,6 +19,12 @@ const Main = props => (
   <Provider store={store} history={history}>
     <ConnectedRouter history={history}>
       <div>
+        <Helmet titleTemplate="%s - TalentMAP" defaultTitle="TalentMAP">
+          <meta property="og:title" content="TalentMAP" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content={window.location.href} />
+          <meta property="og:image" content={`${getApplicationPath()}${getAssetPath('/assets/logos/png/logo_color.png')}`} />
+        </Helmet>
         <Header {...props} isAuthorized={isAuthorized} />
         <main id="main-content">
           <Routes {...props} isAuthorized={isAuthorized} />
