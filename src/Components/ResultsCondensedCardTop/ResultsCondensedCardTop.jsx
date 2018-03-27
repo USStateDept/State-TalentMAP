fcaf2916b77c3cc60d2297c5b609dac2a59d8e52
Nat Burgwyn <>
2018-03-26 23:19:22 -0400

    Moving the '|'

--- src/Components/ResultsCondensedCardTop/ResultsCondensedCardTop.jsx
+++ src/Components/ResultsCondensedCardTop/ResultsCondensedCardTop.jsx
@@ -20,7 +20,7 @@ const ResultsCondensedCardTop = ({ position, type }) => {
             'usa-width-two-thirds condensed-card-top-header condensed-card-top-header-left'
           }
         >
-          {useType && <span> | <FontAwesome name={icon} /></span>}
+          {useType && <span><FontAwesome name={icon} /> | </span>}
           <h3>{position.title}</h3>
         </div>
         <div
