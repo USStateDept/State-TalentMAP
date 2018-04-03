086013cbdfdd64224a3f6f0b105baa5227a5e029
burgwyn <burgwyn@gmail.com>
2018-04-03 14:56:38 -0400

    Add login flow

--- src/saml2-config.js
+++ src/saml2-config.js
@@ -9,19 +9,30 @@ const ASSERT_ENDPOINT = process.env.ASSERT_ENDPOINT || 'http://localhost:5000/';
 const certFile = fs.readFileSync(process.env.CERT_FILE);
 const keyFile = fs.readFileSync(process.env.KEY_FILE);
 
-const options = {
+// identity provide config
+const SSO_LOGIN_URL = process.env.SSO_LOGIN_URL || 'http://localhost:5000/login';
+const SSO_LOGOUT_URL = process.env.SSO_LOGOUT_URL || 'http://localhost:5000/login';
+
+// Create service provider with options
+const serviceProvider = new saml2.ServiceProvider({
   entity_id: ENTITY_ID,
   private_key: keyFile,
   certificate: certFile,
   assert_endpoint: ASSERT_ENDPOINT,
   force_authn: true,
-};
+});
 
-// Create service provider with options
-const serviceProvider = new saml2.ServiceProvider(options);
+const identityProvider = new saml2.IdentityProvider({
+  sso_login_url: SSO_LOGIN_URL,
+  sso_logout_url: SSO_LOGOUT_URL,
+});
 
 // Example use of service provider.
 // Call metadata to get XML metatadata used in configuration.
 const metadata = serviceProvider.create_metadata();
 
-module.exports = { metadata };
+const login = (handler) => {
+  serviceProvider.create_login_request_url(identityProvider, {}, handler);
+};
+
+module.exports = { metadata, login };
