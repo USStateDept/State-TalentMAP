3a8dad3c4609f04199a8d9022acc4ce15b0479e2
Mike Joyce <mjoyce91@gmail.com>
2018-04-06 08:47:06 -0400

    Remove bureau mapping, add tests for getPostName, diversify post locations in mocks and update snapshots

--- src/__mocks__/assignmentObject.js
+++ src/__mocks__/assignmentObject.js
@@ -8,11 +8,11 @@ const assignmentObject = {
     title: 'PROJECT DIRECTOR',
     post: {
       location: {
-        id: 103,
-        country: 'Sierra Leone',
-        code: '00A',
-        city: 'Freetown',
-        state: '',
+        id: 4,
+        country: 'United States',
+        code: '500',
+        city: 'Washington',
+        state: 'DC',
       },
     },
   },
