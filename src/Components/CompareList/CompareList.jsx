3977d226a124680ce2a0fdd8d4f68215dffc1e9e
burgwyn <burgwyn@gmail.com>
2018-04-17 22:55:41 -0400

    Update OBC url to support 'post-data'

--- src/Components/CompareList/CompareList.jsx
+++ src/Components/CompareList/CompareList.jsx
@@ -116,7 +116,7 @@ const CompareList = ({ compare, isLoading }) => {
                       compareArray.map(c => (
                         <td key={shortId.generate()}>
                           {getDifferentialPercentage(propOrDefault(c, 'post.differential_rate'), NO_POST_DIFFERENTIAL)}
-                          {propOrDefault(c, 'post.obc_id') ? <span> | <OBCUrl id={c.post.obc_id} label="Details" /></span> : null }
+                          {propOrDefault(c, 'post.obc_id') ? <span> | <OBCUrl type="post-data" id={c.post.obc_id} label="Details" /></span> : null }
                         </td>
                       ))
                     }
