ff15f061f1d03fd41a3c28f12beb7aeef4ee9c23
Mike Joyce <mjoyce91@gmail.com>
2018-04-23 09:12:14 -0400

    Update comment

--- src/Components/SearchFilters/SearchFiltersContainer/SearchFiltersContainer.jsx
+++ src/Components/SearchFilters/SearchFiltersContainer/SearchFiltersContainer.jsx
@@ -93,7 +93,7 @@ class SearchFiltersContainer extends Component {
     const overseasIsSelected = propOrDefault(overseasFilterData, 'isSelected', false);
     const domesticIsSelected = propOrDefault(domesticFilterData, 'isSelected', false);
 
-    // get skill codes
+    // get skill cones
     const skillCones = (this.props.filters || []).find(f => f.item.description === 'skillCone');
 
     // adding filters based on multiSelectFilterNames
