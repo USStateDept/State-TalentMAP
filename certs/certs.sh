319e0ad20a59ddbb1d60d049a62ad15d3d50d788
Kevin Chiu <chiu_kevin@bah.com>
2018-04-23 12:49:51 -0400

    - Escape spaces in path

--- certs/certs.sh
+++ certs/certs.sh
@@ -1,9 +1,9 @@
-CERT_PATH="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+CERT_PATH=$(printf %q "$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)")
 CONFIG=$CERT_PATH/talentmap-dev.cnf
 KEY=$CERT_PATH/talentmap-dev.key
 CSR=$CERT_PATH/talentmap-dev.csr
 CERT=$CERT_PATH/talentmap-dev.crt
 
-openssl genrsa -out $KEY 2048
-openssl req -new -config $CONFIG -key $KEY -out $CSR
-openssl x509 -req -sha256 -days 365 -in $CSR -signkey $KEY -out $CERT
+eval "openssl genrsa -out $KEY 2048"
+eval "openssl req -new -config $CONFIG -key $KEY -out $CSR"
+eval "openssl x509 -req -sha256 -days 365 -in $CSR -signkey $KEY -out $CERT"
