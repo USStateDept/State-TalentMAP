6bfdc794b65791acf94137667b35236c7be56958
Kevin Chiu <chiu_kevin@bah.com>
2018-04-24 16:12:23 -0400

    - Removed `axiosConfig.js` and moved logic to `api.js` - Cleaned up `sagas.js` - Updated `reducer.js` to remove merge return and change to hardsets

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { fetchUserToken, hasValidToken, propOrDefault } from './utilities';
+import { fetchUserToken, hasValidToken, propOrDefault, redirectToLoginRedirect } from './utilities';
 import { authRequest } from './login/actions';
 
 export const config = {
@@ -19,7 +19,7 @@ api.interceptors.request.use((request) => {
 
 api.interceptors.response.use(response => response, (error) => {
   switch (propOrDefault(error, 'response.status')) {
-    case 401:
+    case 401: {
       // Due to timing of import store before history is created, importing store here causes
       // exports of api to be undefined. So this causes an error for `userProfile.js` when
       // attempting to login. Went with the eslint quick re-enable to get around this.
@@ -27,12 +27,18 @@ api.interceptors.response.use(response => response, (error) => {
       require('./store').store.dispatch(authRequest(false));
       /* eslint-enable global-require */
       break;
+    }
 
-    default:
-      // TODO:
+    default: {
       // We don't want to stop the pipeline even if there's a problem with the dispatch
       // and if there is, that should be resolved. This is just a placeholder until we
       // actually need a default case to satisfy eslint.
+      const serverMessage = propOrDefault(error, 'response.data.detail');
+
+      if (serverMessage === 'Invalid token') {
+        redirectToLoginRedirect();
+      }
+    }
   }
 
   return Promise.reject(error);
