7ba399813f256da7ea9472d037d0cc8fbbd538d4
burgwyn <burgwyn@gmail.com>
2018-04-25 22:14:18 -0400

    Adding unit tests

--- src/login/actions.test.js
+++ src/login/actions.test.js
@@ -1,7 +1,7 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { authRequest, tokenValidationRequest } from './actions';
-import { LOGOUT_REQUESTING, TOKEN_VALIDATION_REQUESTING } from './constants';
+import { authRequest, tokenValidationRequest, authSuccess, authError } from './actions';
+import { LOGOUT_REQUESTING, TOKEN_VALIDATION_REQUESTING, LOGIN_SUCCESS, LOGIN_ERROR, LOGOUT_ERROR, LOGIN_REQUESTING } from './constants';
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
@@ -19,8 +19,31 @@ describe('login actions', () => {
     expect(result.token).toBe(token);
   });
 
+  it('can perform login', () => {
+    const result = store.dispatch(authRequest());
+    expect(result.type).toBe(LOGIN_REQUESTING);
+  });
+
   it('can perform logout', () => {
     const result = store.dispatch(authRequest(false));
     expect(result.type).toBe(LOGOUT_REQUESTING);
+  });
+
+  it('can handle login error', () => {
+    const result = store.dispatch(authError());
+    expect(result.type).toBe(LOGIN_ERROR);
+    expect(result.message).toEqual(null);
+  });
+
+  it('can handle logout error', () => {
+    const message = { body: 'error' };
+    const result = store.dispatch(authError(false, message));
+    expect(result.type).toBe(LOGOUT_ERROR);
+    expect(result.message).toEqual(message);
+  });
+
+  it('can handle authSuccess', () => {
+    const result = store.dispatch(authSuccess(true));
+    expect(result.type).toBe(LOGIN_SUCCESS);
   });
 });
