6121249fbef039108082554c1a6aeb2da6b523f0
Mike Joyce <mjoyce91@gmail.com>
2018-04-26 13:21:59 -0400

    Use profile dashboard from dev

--- src/Components/ProfileDashboard/BidList/BidListHeader/BidListHeader.jsx
+++ src/Components/ProfileDashboard/BidList/BidListHeader/BidListHeader.jsx
@@ -4,7 +4,7 @@ import FontAwesome from 'react-fontawesome';
 // Due date is static for now
 const BidListHeader = () => (
   <div className="usa-grid-full bid-list-header section-padded-inner-container-narrow">
-    <div className="usa-width-two-thirds">
+    <div className="usa-width-two-thirds bid-list-header-text">
       <FontAwesome name="clock-o" /> All bids are due 7.15.18
     </div>
     <div className="usa-width-one-third bid-list-header-button-container">
--- src/Components/ProfileDashboard/ProfileDashboard.jsx
+++ src/Components/ProfileDashboard/ProfileDashboard.jsx
@@ -1,48 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  USER_PROFILE,
-  FAVORITE_POSITIONS_ARRAY,
-} from '../../Constants/PropTypes';
+import { USER_PROFILE, NOTIFICATION_RESULTS, ASSIGNMENT_OBJECT, BID_RESULTS } from '../../Constants/PropTypes';
 import UserProfile from './UserProfile';
+import ExternalUserStatus from './ExternalUserStatus';
+import BidList from './BidList';
+import PositionInformation from './PositionInformation';
+import Notifications from './Notifications';
 import Spinner from '../Spinner';
-import BidListComingSoon from './BidListComingSoon';
-import Favorites from './Favorites';
-import SavedSearches from './SavedSearches/SavedSearchesWrapper';
+import StaticDevContent from '../StaticDevContent';
 
 const ProfileDashboard = ({
-  userProfile,
-  isLoading,
-  favoritePositions,
-  favoritePositionsIsLoading,
+  userProfile, isLoading, assignment, assignmentIsLoading, notifications,
+  notificationsIsLoading, bidList, bidListIsLoading,
 }) => (
   <div className="usa-grid-full user-dashboard user-dashboard-main profile-content-inner-container">
-    {isLoading || favoritePositionsIsLoading ? (
+    {isLoading || assignmentIsLoading || notificationsIsLoading || bidListIsLoading ? (
       <Spinner type="homepage-position-results" size="big" />
     ) : (
       <div className="usa-grid-full">
         <h2 className="sr-only">Dashboard</h2>
         <div className="usa-grid-full">
           <div
-            className={`usa-width-five-twelfths user-dashboard-section-container
-                        user-dashboard-column-1`}
+            className={`usa-width-one-fourth user-dashboard-section-container
+              user-dashboard-column-1`}
           >
             <div className="usa-width-one-whole user-dashboard-section current-user-section">
               <UserProfile userProfile={userProfile} />
             </div>
-            <div className="usa-width-one-whole user-dashboard-section">
-              <BidListComingSoon />
+            <div className="usa-width-one-whole user-dashboard-section cdo-section">
+              <StaticDevContent>
+                <ExternalUserStatus type="cdo" initials="LS" firstName="Leah" lastName="Shadtrach" small />
+              </StaticDevContent>
             </div>
           </div>
           <div
-            className={`usa-width-seven-twelfths user-dashboard-section-container
-                        user-dashboard-column-2`}
+            className={`usa-width-one-fourth user-dashboard-section-container
+              user-dashboard-column-2`}
           >
-            <div className="usa-width-one-whole user-dashboard-section favorites-section">
-              <SavedSearches />
+            <div className="usa-width-one-whole user-dashboard-section position-info-section">
+              <PositionInformation assignment={assignment} />
             </div>
-            <div className="usa-width-one-whole user-dashboard-section favorites-section">
-              <Favorites favorites={favoritePositions} />
+            <div className="usa-width-one-whole user-dashboard-section notifications-section">
+              <Notifications notifications={notifications} />
+            </div>
+          </div>
+          <div
+            className={`usa-width-one-half user-dashboard-section-container
+              user-dashboard-column-3`}
+          >
+            <div className="usa-width-one-whole user-dashboard-section bidlist-section">
+              <BidList bids={bidList} />
             </div>
           </div>
         </div>
@@ -54,14 +61,12 @@ const ProfileDashboard = ({
 ProfileDashboard.propTypes = {
   userProfile: USER_PROFILE.isRequired,
   isLoading: PropTypes.bool.isRequired,
-  favoritePositions: FAVORITE_POSITIONS_ARRAY,
-  favoritePositionsIsLoading: PropTypes.bool,
-};
-
-ProfileDashboard.defaultProps = {
-  favoritePositions: [],
-  favoritePositionsIsLoading: false,
-  filtersIsLoading: false,
+  assignment: ASSIGNMENT_OBJECT.isRequired,
+  assignmentIsLoading: PropTypes.bool.isRequired,
+  notifications: NOTIFICATION_RESULTS.isRequired,
+  notificationsIsLoading: PropTypes.bool.isRequired,
+  bidList: BID_RESULTS.isRequired,
+  bidListIsLoading: PropTypes.bool.isRequired,
 };
 
 export default ProfileDashboard;
