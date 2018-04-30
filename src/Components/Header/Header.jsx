8e5d645febbc7ae9526ca979e5071b8ad8d034ac
Mike Joyce <mjoyce91@gmail.com>
2018-04-30 09:15:36 -0400

    Add static content toggle back in

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { push } from 'react-router-redux';
+import ToggleContent from '../StaticDevContent/ToggleContent';
 import { userProfileFetchData } from '../../actions/userProfile';
 import { setSelectedSearchbarFilters } from '../../actions/selectedSearchbarFilters';
 import { authRequest } from '../../login/actions';
