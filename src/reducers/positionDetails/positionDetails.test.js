200a6abd5d6b8ab718cc934e0a3b1e52561d34e5
Mike Joyce <mjoyce91@gmail.com>
2018-05-03 09:03:23 -0400

    Add positionDetails reducer tests

--- src/reducers/positionDetails/positionDetails.test.js
+++ src/reducers/positionDetails/positionDetails.test.js
@@ -1,5 +1,7 @@
 import * as reducers from './positionDetails';
 
+const isEqual = require('lodash/isEqual');
+
 describe('reducers', () => {
   it('can set reducer POSITION_DETAILS_HAS_ERRORED', () => {
     expect(reducers.positionDetailsHasErrored(false, { type: 'POSITION_DETAILS_HAS_ERRORED', hasErrored: true })).toBe(true);
@@ -11,5 +13,22 @@ describe('reducers', () => {
 
   it('can set reducer POSITION_DETAILS_FETCH_DATA_SUCCESS', () => {
     expect(reducers.positionDetails([], { type: 'POSITION_DETAILS_FETCH_DATA_SUCCESS', positionDetails: true })).toBe(true);
+  });
+
+  it('can set reducer and merge changes for POSITION_DETAILS_PATCH_STATE when position exists', () => {
+    const result = (reducers.positionDetails(
+      [{ id: 1, prop: false, prop2: 2 }],
+      { type: 'POSITION_DETAILS_PATCH_STATE', positionDetails: { id: 1, prop: true, prop3: 3 } },
+    ));
+    expect(isEqual(result, [{ id: 1, prop: true, prop2: 2, prop3: 3 }])).toBe(true);
+  });
+
+  it('can set reducer and merge changes for POSITION_DETAILS_PATCH_STATE when position does not exist', () => {
+    const result = (reducers.positionDetails(
+      [{ id: 2, prop: false, prop2: 2 }],
+      { type: 'POSITION_DETAILS_PATCH_STATE', positionDetails: { id: 1, prop: true, prop3: 3 } },
+    ));
+    expect(isEqual(result, [{ id: 2, prop: false, prop2: 2 }, { id: 1, prop: true, prop3: 3 }]))
+      .toBe(true);
   });
 });
