53f4893259da2c3814a78ceac97c10aeb8d540e4
Rob Tirserio <rtirserio@gmail.com>
2018-11-13 08:48:47 -0500

    add the redux devtools extension when the env is development

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import { routerMiddleware } from 'react-router-redux';
@@ -18,11 +18,21 @@ const history = createHistory({ basename: process.env.PUBLIC_URL });
 
 const middleware = routerMiddleware(history);
 
+/* eslint-disable no-underscore-dangle */
+// Enables the redux devtools extension only in development environment
+const composeEnhancers = (
+  process.env.NODE_ENV === 'development'
+  && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+) || compose;
+/* eslint-enable no-underscore-dangle */
+
 function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk, middleware, sagaMiddleware),
+    composeEnhancers(
+      applyMiddleware(thunk, middleware, sagaMiddleware),
+    ),
   );
 }
 
