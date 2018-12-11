eb1eb6c212b196431117ea424c65043b7ca42f60
Mike Joyce <mjoyce91@gmail.com>
2018-12-11 11:42:44 -0500

    Add Bid Count to Bid Tracker cards

--- src/Components/BidTracker/BidTrackerCardTitle/BidTrackerCardTitle.jsx
+++ src/Components/BidTracker/BidTrackerCardTitle/BidTrackerCardTitle.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { BID_STATISTICS_OBJECT } from '../../../Constants/PropTypes';
+import BidCount from '../../BidCount';
+import { SUBMITTED_PROP } from '../../../Constants/BidData';
+import { getPostName } from '../../../utilities';
 
-const BidTrackerCardTitle = ({ title, id }) => (
-  <div className="bid-tracker-card-title-container">
-    <div className="bid-tracker-card-title-text">{title}</div>
-    <div className="bid-tracker-card-title-link">
-      <Link to={`/details/${id}`}>View position</Link>
+const BidTrackerCardTitle = ({ title, id, status, bidStatistics, post }) => (
+  <div className="usa-grid-full">
+    <div className="usa-grid-full bid-tracker-card-title-container">
+      <div className="bid-tracker-card-title-text">{title}</div>
+      <div className="bid-tracker-card-title-link">
+        <Link to={`/details/${id}`}>View position</Link>
+      </div>
     </div>
+    {status === SUBMITTED_PROP &&
+      <div className="usa-grid-full">
+        <div className="bid-tracker-card-title-bottom">
+          <strong>Post:</strong> {getPostName(post)}
+        </div>
+        <BidCount bidStatistics={bidStatistics} altStyle />
+      </div>
+    }
   </div>
 );
 
 BidTrackerCardTitle.propTypes = {
   title: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  bidStatistics: BID_STATISTICS_OBJECT.isRequired,
+  post: PropTypes.shape({}).isRequired,
 };
 
 export default BidTrackerCardTitle;
