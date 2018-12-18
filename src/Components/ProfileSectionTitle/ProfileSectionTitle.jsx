91a7b3541ca5802603a5a3d968bff4250d5b16b1
Rob Tirserio <rtirserio@gmail.com>
2018-12-18 11:02:02 -0500

    fix: update profile page based on qa

--- src/Components/ProfileSectionTitle/ProfileSectionTitle.jsx
+++ src/Components/ProfileSectionTitle/ProfileSectionTitle.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import FontAwesome from 'react-fontawesome';
 
-const ProfileSectionTitle = ({ title }) => (
+const ProfileSectionTitle = ({ title, icon }) => (
   <h2 className="usa-grid-full hello-greeting">
+    { icon && <FontAwesome size="lg" name={icon} /> }
     {title}
   </h2>
 );
 
 ProfileSectionTitle.propTypes = {
   title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+ProfileSectionTitle.defaultProps = {
+  icon: '',
 };
 
 export default ProfileSectionTitle;
