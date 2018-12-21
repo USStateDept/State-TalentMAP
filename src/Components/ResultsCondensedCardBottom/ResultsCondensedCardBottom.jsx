76f7bce3693314f3018d4b875309e548dd9843e3
Mike Joyce <mjoyce91@gmail.com>
2018-12-21 09:02:20 -0500

    Align bid count with data points in ResultsCondensedCard

--- src/Components/ResultsCondensedCardBottom/ResultsCondensedCardBottom.jsx
+++ src/Components/ResultsCondensedCardBottom/ResultsCondensedCardBottom.jsx
@@ -3,10 +3,12 @@ import PropTypes from 'prop-types';
 import CondensedCardData from '../CondensedCardData';
 import { POSITION_DETAILS, FAVORITE_POSITIONS_ARRAY } from '../../Constants/PropTypes';
 import Favorite from '../../Containers/Favorite';
+import ResultsCondensedCardStats from '../ResultsCondensedCardStats';
 
 const ResultsCondensedCardBottom = ({ position, favorites, refreshFavorites }) => (
   <div className="condensed-card-bottom-container">
     <div className="usa-grid-full condensed-card-bottom">
+      <ResultsCondensedCardStats bidStatisticsArray={position.bid_statistics} />
       <CondensedCardData position={position} />
       <div className="usa-grid-full condensed-card-buttons-section">
         <Favorite
