44d97c6c05ab263bbbfe94bfa7d8711396595d4a
Mike Joyce <mjoyce91@gmail.com>
2019-01-16 17:02:37 -0500

    Display link, if available, in the glossary

--- src/Components/Glossary/GlossaryListing/GlossaryListing.test.jsx
+++ src/Components/Glossary/GlossaryListing/GlossaryListing.test.jsx
@@ -18,6 +18,28 @@ describe('GlossaryListingComponent', () => {
     expect(wrapper).toBeDefined();
   });
 
+  it('displays a link if it is provided', () => {
+    const glossaryItems$ = [glossaryItems[0]];
+    glossaryItems$[0].link = 'google.com';
+    const wrapper = shallow(
+      <GlossaryListing
+        glossaryItems={glossaryItems$}
+      />,
+    );
+    expect(wrapper.find('a').exists()).toBe(true);
+  });
+
+  it('does not display a link if it is not provided', () => {
+    const glossaryItems$ = [glossaryItems[0]];
+    glossaryItems$[0].link = '';
+    const wrapper = shallow(
+      <GlossaryListing
+        glossaryItems={glossaryItems$}
+      />,
+    );
+    expect(wrapper.find('a').exists()).toBe(false);
+  });
+
   it('matches snapshot', () => {
     const wrapper = shallow(
       <GlossaryListing
