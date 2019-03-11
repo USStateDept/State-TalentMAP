d6c51850b72bbaef260c696a4eb3dcbe09a9569f
Mike Joyce <mjoyce91@gmail.com>
2019-03-11 15:13:20 -0400

    Update empty saved search list text (#101)

--- src/Components/EmptyListAlert/NoSavedSearches/NoSavedSearches.jsx
+++ src/Components/EmptyListAlert/NoSavedSearches/NoSavedSearches.jsx
@@ -3,7 +3,7 @@ import EmptyListAlert from '../EmptyListAlert';
 
 const NoSavedSearches = () => {
   const textLineTwo = `Reuse this job search in the future by clicking "Save this search".
-  Try naming your search something memorable, like "Summer Positions 2018".`;
+  Try naming your search something memorable, like "Summer Positions in My Grade".`;
   return (
     <EmptyListAlert
       textLineOne="You haven't saved any searches."
