dd468e2f42753fccbedc2d2c041f4cdd6fd9ffce
Mike Joyce <mjoyce91@gmail.com>
2019-10-02 11:09:44 -0400

    is_domestic and post don't need to be reset for PV any more

--- src/Components/SearchFilters/SearchFiltersContainer/SearchFiltersContainer.jsx
+++ src/Components/SearchFilters/SearchFiltersContainer/SearchFiltersContainer.jsx
@@ -56,8 +56,6 @@ class SearchFiltersContainer extends Component {
         ...config,
         is_available_in_bidcycle: null,
         is_available_in_current_bidcycle: null,
-        is_domestic: null,
-        position__post__in: null,
         projectedVacancy: value,
         ordering: 'ted',
       };
