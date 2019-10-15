e0fa9945ef987e10ff17a0a82273d83d0b362ed8
Nat Burgwyn <burgwyn@gmail.com>
2019-10-15 14:48:10 -0400

    Linting

--- src/Components/AdministratorPage/Stats/Stats.test.jsx
+++ src/Components/AdministratorPage/Stats/Stats.test.jsx
@@ -1,39 +1,39 @@
-import React from "react";
-import TestUtils from "react-dom/test-utils";
-import { Provider } from "react-redux";
-import { MemoryRouter } from "react-router-dom";
-import configureStore from "redux-mock-store";
-import thunk from "redux-thunk";
-import Stats, { formatNum } from "./Stats";
+import React from 'react';
+import TestUtils from 'react-dom/test-utils';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import Stats, { formatNum } from './Stats';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
-describe("formatNum", () => {
-  it("is defined", () => {
-    expect(formatNum(1000)).toBe("1,000");
+describe('formatNum', () => {
+  it('is defined', () => {
+    expect(formatNum(1000)).toBe('1,000');
   });
 });
 
-describe("Stats", () => {
+describe('Stats', () => {
   const props = {
     stats: [
-      { type: "total", title: "total", count: 1000 },
-      { type: "unique", title: "unique", count: 1000 },
+      { type: 'total', title: 'total', count: 1000 },
+      { type: 'unique', title: 'unique', count: 1000 },
     ],
     statsIsLoading: false,
     statsHasErrored: false,
     statsSuccess: true,
-    getStats: () => []
+    getStats: () => [],
   };
 
-  it("mounts", () => {
+  it('mounts', () => {
     const wrapper = TestUtils.renderIntoDocument(
       <Provider store={mockStore({})}>
         <MemoryRouter>
           <Stats {...props} />
         </MemoryRouter>
-      </Provider>
+      </Provider>,
     );
     expect(wrapper).toBeDefined();
   });
