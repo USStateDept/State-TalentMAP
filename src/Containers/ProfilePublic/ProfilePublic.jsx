61d8bd86152c42de1349e77d18782eff076b2b9c
Mike Joyce <mjoyce91@gmail.com>
2019-12-09 15:17:15 -0500

    Remove debug statement

--- src/Containers/ProfilePublic/ProfilePublic.jsx
+++ src/Containers/ProfilePublic/ProfilePublic.jsx
@@ -21,7 +21,7 @@ class ProfilePublic extends Component {
     const { assignments, bidList } = userProfile;
     const assignment = get(assignments, '[0]');
     return (
-      (hasErrored && 1 === false) ?
+      hasErrored ?
         <Alert type="error" title="User not found" />
       :
         <ProfileDashboard
