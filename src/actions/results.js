750ec428868373bb27b5eda0661382f2326bd0c7
Mike Joyce <mjoyce91@gmail.com>
2019-12-19 11:38:57 -0500

    Fix and clean up the position export urls

--- src/actions/results.js
+++ src/actions/results.js
@@ -74,9 +74,10 @@ export function resultsFetchSimilarPositions(id) {
 }
 
 export function downloadPositionData(query, isPV) {
-  const prefix = `fsbid/${!isPV ? '/fsbid/available_positions' : '/fsbid/projected_vacancies'}/export`;
+  const prefix = `/fsbid${isPV ? '/projected_vacancies' : '/available_positions'}/export/`;
+  console.log(prefix, query);
   return api()
-  .get(`${prefix}/?${query}`, {
+  .get(`${prefix}?${query}`, {
     cancelToken: new CancelToken((c) => { cancel = c; }),
     responseType: 'stream',
   })
