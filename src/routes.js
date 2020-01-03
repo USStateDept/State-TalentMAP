3c6c40e8a5a3493335167c2a024e380d4d9f5772
scott062 <scott.m.burdick@gmail.com>
2020-01-03 10:55:32 -0500

    Update route to be focused on biddingProcess - change later with FAQ implementation

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ const routesArray = [
   { path: '/about', exact: true, componentName: 'About', pageTitle: 'About' },
   { path: '/tokenValidation', componentName: 'TokenValidation', pageTitle: 'Token Validation' },
   { path: '/loginRedirect', componentName: 'LoginRedirect', pageTitle: 'Login Redirect' },
-  { path: '/faq', componentName: 'Faq', pageTitle: 'Faq' },
+  { path: '/biddingProcess', componentName: 'Faq', pageTitle: 'Bidding Process' },
 ];
 
 module.exports = routesArray;
