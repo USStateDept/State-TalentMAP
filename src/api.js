8291fa4ba06e12180627cf1a0d899d29bb13f394
Mike Joyce <mjoyce91@gmail.com>
2020-01-16 12:34:23 -0500

    Remove unused param from testing

--- src/api.js
+++ src/api.js
@@ -29,7 +29,7 @@ export const config = () => ({
 });
 
 // Called as a function so that sessionStorage can be set first
-const api = (useInterceptor = true) => {
+const api = () => {
   const api$ = axios.create(config());
 
   api$.interceptors.request.use((request) => {
@@ -77,32 +77,30 @@ const api = (useInterceptor = true) => {
     return request;
   });
 
-  if (useInterceptor) {
-    api$.interceptors.response.use(response => response, (error) => {
-      switch (propOrDefault(error, 'response.status')) {
-        case 401: {
-          // Due to timing of import store before history is created, importing store here causes
-          // exports of api to be undefined. So this causes an error for `userProfile.js` when
-          // attempting to login. Went with the eslint quick re-enable to get around this.
-          debouncedLogout();
-          break;
-        }
-
-        default: {
-          // We don't want to stop the pipeline even if there's a problem with the dispatch
-          // and if there is, that should be resolved. This is just a placeholder until we
-          // actually need a default case to satisfy eslint.
-          const serverMessage = propOrDefault(error, 'response.data.detail');
-
-          if (serverMessage === 'Invalid token') {
-            redirectToLoginRedirect();
-          }
-        }
+  api$.interceptors.response.use(response => response, (error) => {
+    switch (propOrDefault(error, 'response.status')) {
+      case 401: {
+        // Due to timing of import store before history is created, importing store here causes
+        // exports of api to be undefined. So this causes an error for `userProfile.js` when
+        // attempting to login. Went with the eslint quick re-enable to get around this.
+        debouncedLogout();
+        break;
       }
 
-      return Promise.reject(error);
-    });
-  }
+      default: {
+        // We don't want to stop the pipeline even if there's a problem with the dispatch
+        // and if there is, that should be resolved. This is just a placeholder until we
+        // actually need a default case to satisfy eslint.
+        const serverMessage = propOrDefault(error, 'response.data.detail');
+
+        if (serverMessage === 'Invalid token') {
+          redirectToLoginRedirect();
+        }
+      }
+    }
+
+    return Promise.reject(error);
+  });
 
   return api$;
 };
