8da17680a60fa72d648a9ed82acb434fec59ce9e
Elizabeth Jimenez <ejimenez@mpcadmins-MacBook-Air.local>
2020-01-21 14:10:26 -0500

    UI button expands and collapses all clients

--- src/Components/BidderPortfolio/BidderPortfolioGridList/BidderPortfolioGridList.jsx
+++ src/Components/BidderPortfolio/BidderPortfolioGridList/BidderPortfolioGridList.jsx
@@ -1,28 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import FontAwesome from 'react-fontawesome';
 import { Accordion, AccordionItem } from 'Components/Accordion';
 import { BIDDER_RESULTS } from '../../../Constants/PropTypes';
 import BidderPortfolioStatRow from '../BidderPortfolioStatRow';
 
-const BidderPortfolioGridList = ({ results, showEdit }) => (
-  <Accordion className="usa-grid-full accordion-inverse user-dashboard portfolio-row-list" isMultiselectable>
-    {
-      results.map(result => (
-        <AccordionItem
-          className="portfolio-row"
-          id={`${result.id}-row`}
-          key={result.id}
-          title={result.name}
-        >
-          <BidderPortfolioStatRow
-            userProfile={result}
-            showEdit={showEdit}
-          />
-        </AccordionItem>
-      ))
-    }
-  </Accordion>
-);
+class BidderPortfolioGridList extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleExpand = this.toggleExpand.bind(this);
+    this.state = {
+      expandAll: true,
+    };
+  }
+
+  toggleExpand() {
+    this.setState({ expandAll: !this.state.expandAll });
+  }
+
+  render() {
+    const { results, showEdit } = this.props;
+    const expandAll = this.state.expandAll;
+    return (
+      <div>
+        <button className="usa-accordion-button-all" title={expandAll ? 'Collapse All' : 'Expand All'} onClick={this.toggleExpand}>
+          <FontAwesome name={expandAll ? 'minus' : 'plus'} /></button>
+        <Accordion className="usa-grid-full accordion-inverse user-dashboard portfolio-row-list" isMultiselectable>
+          {
+              results.map(result => (
+                <AccordionItem
+                  className="portfolio-row"
+                  id={`${result.id}-row`}
+                  key={result.id}
+                  title={result.name}
+                  expanded={expandAll}
+                >
+                  <BidderPortfolioStatRow
+                    userProfile={result}
+                    showEdit={showEdit}
+                  />
+                </AccordionItem>
+              ))
+            }
+        </Accordion>
+      </div>
+    );
+  }
+}
 
 BidderPortfolioGridList.propTypes = {
   results: BIDDER_RESULTS.isRequired,
