f6b28c1bfaf26b84db8cacd30e7561c370edb6c4
scott062 <scott.m.burdick@gmail.com>
2020-02-25 10:38:32 -0500

    Sort the classifications and return only first 4 applicable to client

--- src/Components/BidderPortfolio/ClientBadgeList/ClientBadgeList.jsx
+++ src/Components/BidderPortfolio/ClientBadgeList/ClientBadgeList.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+// import { } from 'lodash';
 import ClientBadge from '../ClientBadge';
 import { CLASSIFICATIONS, CLIENT_CLASSIFICATIONS } from '../../../Constants/PropTypes';
 
 const ClientBadgeList = ({ classifications, clientClassifications }) => (
   <div className="usa-grid-full client-badge-list">
-    {classifications.map(c => (
-      <ClientBadge
-        type={c.code}
-        status={clientClassifications.indexOf(c.code) > -1}
-      />
+    {classifications.sort(c => (clientClassifications.includes(c.code) ? -1 : 1))
+                    .slice(0, 4)
+                    .map(c => (
+                      <ClientBadge
+                        type={c.code}
+                        status={clientClassifications.indexOf(c.code) > -1}
+                      />
     ))}
   </div>
 );
