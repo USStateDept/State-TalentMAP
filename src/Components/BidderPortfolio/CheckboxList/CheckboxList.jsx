4e2cb72d5f1c1ff6116ae1be463e4abd1e007537
scott062 <scott.m.burdick@gmail.com>
2020-02-25 10:54:44 -0500

    Use lodash indexOf and consolidate logic inside of mapping

--- src/Components/BidderPortfolio/CheckboxList/CheckboxList.jsx
+++ src/Components/BidderPortfolio/CheckboxList/CheckboxList.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
+import { indexOf } from 'lodash';
 import CheckBox from '../../CheckBox';
 import { CLASSIFICATIONS, CLIENT_CLASSIFICATIONS } from '../../../Constants/PropTypes';
 
 const CheckboxList = ({ list, clientClassifications }) => (
 
   <div className="client-checkbox-list">
-    {list.map(c => (
-      <CheckBox
-        id={c.id}
-        label={c.text}
-        small
-        value={clientClassifications.indexOf(c.code) > -1}
-        key={c.code}
-        disabled
-        checked={clientClassifications.indexOf(c.code) > -1}
-        className={'tm-checkbox-disabled-alternate'}
-      />
-    ))}
+    {list.map((c) => {
+      const checked = indexOf(clientClassifications, c.code) > -1;
+      return (
+        <CheckBox
+          id={c.id}
+          label={c.text}
+          small
+          value={checked}
+          key={c.code}
+          disabled
+          checked={checked}
+          className="tm-checkbox-disabled-alternate"
+        />
+      );
+    })
+  }
   </div>
 );
 
