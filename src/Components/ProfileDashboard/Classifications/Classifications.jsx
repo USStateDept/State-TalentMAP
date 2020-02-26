0aeb68f03ce17a024645422075af236da9f062ba
scott062 <scott.m.burdick@gmail.com>
2020-02-26 16:41:38 -0500

    Convert to functional componenent and remove linter overrides

--- src/Components/ProfileDashboard/Classifications/Classifications.jsx
+++ src/Components/ProfileDashboard/Classifications/Classifications.jsx
@@ -1,35 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SectionTitle from '../SectionTitle';
 import CheckboxList from '../../BidderPortfolio/CheckboxList';
 import { CLASSIFICATIONS, CLIENT_CLASSIFICATIONS } from '../../../Constants/PropTypes';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class Classifications extends Component {
-  // eslint-disable-next-line no-useless-constructor
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { classifications, clientClassifications } = this.props;
-    return (
-      <div className="usa-grid-full profile-section-container updates-container">
-        <div className="usa-grid-full section-padded-inner-container">
-          <div className="usa-width-one-whole">
-            <SectionTitle title="Bidder Classifications" icon="tasks" />
-          </div>
-          <div className="usa-width-one-whole">
-            <CheckboxList
-              list={classifications}
-              clientClassifications={clientClassifications}
-              id="updates"
-            />
-          </div>
-        </div>
+const Classifications = ({ classifications, clientClassifications }) => (
+  <div className="usa-grid-full profile-section-container updates-container">
+    <div className="usa-grid-full section-padded-inner-container">
+      <div className="usa-width-one-whole">
+        <SectionTitle title="Bidder Classifications" icon="tasks" />
       </div>
-    );
-  }
-}
+      <div className="usa-width-one-whole">
+        <CheckboxList
+          list={classifications}
+          clientClassifications={clientClassifications}
+          id="updates"
+        />
+      </div>
+    </div>
+  </div>
+);
 
 Classifications.propTypes = {
   classifications: CLASSIFICATIONS,
