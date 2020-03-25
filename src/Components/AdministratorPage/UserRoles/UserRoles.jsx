ec26dc7cc574351e7254a9af4f9562747f217966
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2020-03-25 13:19:21 -0400

    tests updated. UserRoles component updated based on tests.

--- src/Components/AdministratorPage/UserRoles/UserRoles.jsx
+++ src/Components/AdministratorPage/UserRoles/UserRoles.jsx
@@ -130,7 +130,14 @@ class UserRoles extends Component {
 
 UserRoles.propTypes = {
   totalUsers: PropTypes.number,
-  usersList: PropTypes.arrayOf(PropTypes.shape({})),
+  usersList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.node,
+      username: PropTypes.node,
+      last_name: PropTypes.node,
+      first_name: PropTypes.node,
+      groups: PropTypes.node,
+    })),
   usersIsLoading: PropTypes.bool,
   usersHasErrored: PropTypes.bool,
   modifyPermissionIsLoading: PropTypes.bool,
@@ -140,7 +147,13 @@ UserRoles.propTypes = {
 
 UserRoles.defaultProps = {
   totalUsers: 0,
-  usersList: [],
+  usersList: [{
+    id: '',
+    username: '',
+    last_name: '',
+    first_name: '',
+    groups: '',
+  }],
   usersIsLoading: false,
   usersHasErrored: false,
   modifyPermissionIsLoading: false,
@@ -149,7 +162,7 @@ UserRoles.defaultProps = {
 };
 
 const mapStateToProps = state => ({
-  usersList: state.usersSuccess.results,
+  usersList: get(state, 'usersSuccess.results', []),
   usersIsLoading: state.usersIsLoading,
   usersHasErrored: state.usersHasErrored,
   modifyPermissionIsLoading: state.modifyPermissionIsLoading,
--- src/Components/AdministratorPage/UserRoles/UserRow/UserRow.test.jsx
+++ src/Components/AdministratorPage/UserRoles/UserRow/UserRow.test.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import TestUtils from 'react-dom/test-utils';
-import { Provider } from 'react-redux';
-import { MemoryRouter } from 'react-router-dom';
-import configureStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
 import sinon from 'sinon';
 import toJSON from 'enzyme-to-json';
 import UserRow from './UserRow';
-
-const middlewares = [thunk];
-const mockStore = configureStore(middlewares);
 
 describe('UserRow', () => {
   const props = {
@@ -22,13 +14,6 @@ describe('UserRow', () => {
     modifyPermission: () => [],
     onClick: () => [],
   };
-
-  it('mounts', () => {
-    const wrapper = TestUtils.renderIntoDocument(<Provider store={mockStore({})}><MemoryRouter>
-      <UserRow {...props} />
-    </MemoryRouter></Provider>);
-    expect(wrapper).toBeDefined();
-  });
 
   it('is defined', () => {
     const wrapper = shallow(<UserRow.WrappedComponent {...props} />);
