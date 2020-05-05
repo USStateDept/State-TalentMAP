2158a3d800130cd27b9fb73ba1d7c7e7858341e2
Mike Joyce <mjoyce91@gmail.com>
2020-05-05 13:26:25 -0400

    Update test and remove unnecessary files

--- src/Components/SearchFilters/TandemSelectionFilter/TandemSelectionFilter.jsx
+++ src/Components/SearchFilters/TandemSelectionFilter/TandemSelectionFilter.jsx
@@ -46,4 +46,3 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps)(TandemSelectionFilter);
-
--- src/Components/SearchFilters/TandemSelectionFilter/TandemSelectionFilter.test.jsx
+++ src/Components/SearchFilters/TandemSelectionFilter/TandemSelectionFilter.test.jsx
@@ -1,34 +1,17 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import ProjectedVacancyFilter from './ProjectedVacancyFilter';
+import TandemSelectionFilter from './TandemSelectionFilter';
 
-describe('ProjectedVacancyFilter', () => {
+describe('TandemSelectionFilter', () => {
+  const props = {
+    onChange: () => {},
+    userProfile: { display_name: 'John Doe' },
+    isLoading: false,
+  };
   it('is defined', () => {
     const wrapper = shallow(
-      <ProjectedVacancyFilter />,
+      <TandemSelectionFilter.WrappedComponent {...props} />,
     );
-    expect(wrapper).toBeDefined();
-  });
-
-  it('is defined after receiving new props', () => {
-    const wrapper = shallow(
-      <ProjectedVacancyFilter />,
-    );
-    wrapper.instance().toggleRef = { updateVal: () => {} };
-    wrapper.update();
-    // matches first componentWillReceiveProps if statement
-    wrapper.setProps({ items: [{ isSelected: true, code: 1 }] });
-    expect(wrapper).toBeDefined();
-    // matches second componentWillReceiveProps if statement
-    wrapper.setProps({ items: [{ isSelected: false, code: 1 }] });
-    expect(wrapper).toBeDefined();
-  });
-
-  it('is defined after receiving new props when no items have the isSelected prop', () => {
-    const wrapper = shallow(
-      <ProjectedVacancyFilter />,
-    );
-    wrapper.setProps({ items: [{ code: 1 }] });
     expect(wrapper).toBeDefined();
   });
 });
