cd67b18cb49007975fa653c447f70472d260ee27
Mike Joyce <mjoyce91@gmail.com>
2020-05-11 12:27:18 -0400

    Deduplicate CDOs, since they can appear twice if they match multiple rolecodes

--- src/Components/BidderPortfolio/helpers.js
+++ src/Components/BidderPortfolio/helpers.js
@@ -1,14 +1,18 @@
 // Temporary helpers used for static data
 
-import { orderBy } from 'lodash';
+import { orderBy, uniqBy } from 'lodash';
 import { filterByProps } from '../../utilities';
 
 function filterUsers(term = '', cdos = []) {
   const getOrdered = a => orderBy(a, ['isCurrentUser', 'last_name']);
 
-  return term.length ?
+  const data = term.length ?
     getOrdered(filterByProps(term, ['first_name', 'last_name'], cdos)) :
     getOrdered(cdos);
+
+  // It is possible that a single CDO is returned twice if they match multiple rolecodes,
+  // so deduplicate here.
+  return uniqBy(data, 'id');
 }
 
 export default filterUsers;
