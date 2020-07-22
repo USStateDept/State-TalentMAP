9cbc97f206f20822632a9a05d797b8664fba7057
Mike Joyce <mjoyce91@gmail.com>
2020-07-22 11:08:36 -0400

    Move bureau position cards to the PositionManager page

--- src/Components/BureauPage/PositionManager/PositionManager.test.jsx
+++ src/Components/BureauPage/PositionManager/PositionManager.test.jsx
@@ -1,23 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import TestUtils from 'react-dom/test-utils';
-import { Provider } from 'react-redux';
-import { MemoryRouter } from 'react-router-dom';
-import configureStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
 import PositionManager from './PositionManager';
 
-const middlewares = [thunk];
-const mockStore = configureStore(middlewares);
-
 describe('BureauPage', () => {
-  it('mounts', () => {
-    const wrapper = TestUtils.renderIntoDocument(<Provider store={mockStore({})}><MemoryRouter>
-      <PositionManager />
-    </MemoryRouter></Provider>);
-    expect(wrapper).toBeDefined();
-  });
-
   it('is defined', () => {
     const wrapper = shallow(<PositionManager />);
     expect(wrapper).toBeDefined();
