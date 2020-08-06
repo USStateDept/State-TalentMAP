60434007fa6f80194ece33d8a0fdfe3e47ed1a68
Mike Joyce <mjoyce91@gmail.com>
2020-08-06 12:11:11 -0400

    Allow sorting search results and favorites results by location

--- src/Constants/Sort.js
+++ src/Constants/Sort.js
@@ -13,6 +13,8 @@ const POSITION_SEARCH_SORTS$ = {
     { value: '-ted', text: 'TED: Latest' },
     { value: 'position__position_number', text: 'Position number: Low to high' }, // numbers first, then A-Z
     { value: '-position__post__has_service_needs_differential', text: 'Featured positions', availableOnly: true }, // sort by service needs first
+    { value: 'location', text: 'Location: A-Z', nonTandemOnly: true },
+    { value: '-location', text: 'Location: Z-A', nonTandemOnly: true },
   ],
 };
 
@@ -39,6 +41,12 @@ export const POSITION_SEARCH_SORTS_DYNAMIC = {
 export const filterPVSorts = (sorts) => {
   const v = { ...sorts };
   v.options = filter(v.options, f => !f.availableOnly);
+  return v;
+};
+
+export const filterTandemSorts = (sorts) => {
+  const v = { ...sorts };
+  v.options = filter(v.options, f => !f.nonTandemOnly);
   return v;
 };
 
