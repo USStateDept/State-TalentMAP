8c2ff21846c4a54b6ff40d0ffc0a38540266a08b
Mike Joyce <joyce_michael@bah.com>
2020-10-28 11:55:09 -0400

    Remove React import

--- src/Components/BureauPage/ExportButton/ExportButton.jsx
+++ src/Components/BureauPage/ExportButton/ExportButton.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import PropTypes from 'prop-types';
 import ExportButton from 'Components/ExportButton';
 import { downloadBidderData } from 'actions/bureauPositionBids';
