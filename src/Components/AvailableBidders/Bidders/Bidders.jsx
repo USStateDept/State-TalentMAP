742eaaaa5f712623555e1d945bf2634ee03587e9
scott062 <scott.m.burdick@gmail.com>
2020-12-22 12:06:59 -0500

    Set up migration to hook

--- src/Components/AvailableBidders/Bidders/Bidders.jsx
+++ src/Components/AvailableBidders/Bidders/Bidders.jsx
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
+import { userSelector } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { get, keys } from 'lodash';
-import { formatDate } from 'utilities';
 import { EMPTY_FUNCTION } from 'Constants/PropTypes';
-import { NO_GRADE, NO_END_DATE } from 'Constants/SystemMessages';
 import ToggleButton from 'Components/ToggleButton';
 import ExportButton from 'Components/ExportButton';
-import EditBidder from 'Components/AvailableBidders/EditBidder';
+// import EditBidder from 'Components/AvailableBidders/EditBidder';
 import AvailableBidderRow from 'Components/AvailableBidders/AvailableBidderRow';
 import FA from 'react-fontawesome';
 import { Tooltip } from 'react-tippy';
@@ -15,18 +12,12 @@ import shortid from 'shortid';
 import swal from '@sweetalert/with-react';
 
 
-class Bidders extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      CDOView: true,
-      sharedWithBureau: true,
-      showModal: false,
-    };
-  }
+const Bidders = props => {
+  const [cdoView, setCdoView] = useState(true);
+  const [showModal, setShowModal] = useState(false);
 
-  showModal() {
-    this.setState({ showModal: !this.state.showModal });
+  const modalContent = () => {
+    setShowModal(!showModal);
     swal({
       title: 'Available Bidder Record Editor',
       buttons: ['Cancel', 'Save'],
@@ -67,172 +58,168 @@ class Bidders extends Component {
         </div>
       ),
     });
-  }
+  };
 
-  render() {
-    const { biddersIsLoading } = this.props;
+  const { biddersIsLoading } = this.props;
 
-    const tableHeaders = [
-      'Name',
-      'Status',
-      'Skill',
-      'Grade',
-      'TED',
-      'Post',
-      'OC Bureau',
-      'OC Reason',
-      'CDO',
-      'Comments',
-    ];
+  const tableHeaders = [
+    'Name',
+    'Status',
+    'Skill',
+    'Grade',
+    'TED',
+    'Post',
+    'OC Bureau',
+    'OC Reason',
+    'CDO',
+    'Comments',
+  ];
 
-    // Dummy Data
-    const bidder = {
-      id: '851',
-      name: 'Jenny Townpost',
-      initials: 'JT',
-      perdet_seq_number: 4,
-      grade: '00',
-      skills: [
-        {
-          code: '0020',
-          description: 'EXECUTIVE (CAREER)',
+  // Dummy Data
+  const bidder = {
+    id: '851',
+    name: 'Jenny Townpost',
+    initials: 'JT',
+    perdet_seq_number: 4,
+    grade: '00',
+    skills: [
+      {
+        code: '0020',
+        description: 'EXECUTIVE (CAREER)',
+      },
+    ],
+    employee_id: '851',
+    role_code: 'fsofficer',
+    pos_location: 'Klondike, Saudi Arabia',
+    hasHandshake: null,
+    classifications: [
+
+    ],
+    current_assignment: {
+      id: null,
+      position_id: '96350',
+      start_date: '2020-07-21T18:02:47.378000Z',
+      end_date: '4/02/2021',
+      position: {
+        grade: '06',
+        skill: 'REFUGEE AFFAIRS (6090)',
+        skill_code: '6090',
+        bureau: '(DS) BUREAU OF DIPLOMATIC SECURITY',
+        position_number: '96350',
+        title: 'MANAGEMENT ANALYSIS OFFICER',
+        post: {
+          code: '040530019',
+          post_overview_url: null,
+          post_bidding_considerations_url: null,
+          obc_id: null,
+          location: {
+            country: 'Saudi Arabia',
+            code: '040530019',
+            city: 'Klondike',
+            state: '',
+          },
         },
-      ],
-      employee_id: '851',
-      role_code: 'fsofficer',
-      pos_location: 'Klondike, Saudi Arabia',
-      hasHandshake: null,
-      classifications: [
-
-      ],
-      current_assignment: {
+        language: 'Spanish 3/3',
+      },
+    },
+    assignments: [
+      {
         id: null,
-        position_id: '96350',
+        position_id: '2703',
         start_date: '2020-07-21T18:02:47.378000Z',
         end_date: '4/02/2021',
         position: {
-          grade: '06',
-          skill: 'REFUGEE AFFAIRS (6090)',
-          skill_code: '6090',
-          bureau: '(DS) BUREAU OF DIPLOMATIC SECURITY',
-          position_number: '96350',
-          title: 'MANAGEMENT ANALYSIS OFFICER',
+          grade: 'OM',
+          skill: 'INTERNATIONAL TRANS & COMM (5030)',
+          skill_code: '5030',
+          bureau: '(EUR) BUR OF EUROPEAN AFF AND EURASIAN AFFAIRS',
+          position_number: '2703',
+          title: 'INFORMATION MANAGEMENT SPEC',
           post: {
-            code: '040530019',
+            code: 'TS8000000',
             post_overview_url: null,
             post_bidding_considerations_url: null,
             obc_id: null,
             location: {
-              country: 'Saudi Arabia',
-              code: '040530019',
-              city: 'Klondike',
+              country: 'Somalia',
+              code: 'TS8000000',
+              city: 'Gambrills',
               state: '',
             },
           },
           language: 'Spanish 3/3',
         },
       },
-      assignments: [
-        {
-          id: null,
-          position_id: '2703',
-          start_date: '2020-07-21T18:02:47.378000Z',
-          end_date: '4/02/2021',
-          position: {
-            grade: 'OM',
-            skill: 'INTERNATIONAL TRANS & COMM (5030)',
-            skill_code: '5030',
-            bureau: '(EUR) BUR OF EUROPEAN AFF AND EURASIAN AFFAIRS',
-            position_number: '2703',
-            title: 'INFORMATION MANAGEMENT SPEC',
-            post: {
-              code: 'TS8000000',
-              post_overview_url: null,
-              post_bidding_considerations_url: null,
-              obc_id: null,
-              location: {
-                country: 'Somalia',
-                code: 'TS8000000',
-                city: 'Gambrills',
-                state: '',
-              },
-            },
-            language: 'Spanish 3/3',
-          },
-        },
-      ],
-      cdo: {
-        name: 'Shadtrach, L',
-        email: 'shadtrachl@state.gov',
-      },
-    };
+    ],
+    cdo: {
+      name: 'Shadtrach, L',
+      email: 'shadtrachl@state.gov',
+    },
+  };
 
-    return (
-      <div className="usa-width-one-whole bidder-manager-bidders ab-lower-section">
-        <div className="ab-table-title-row">
-          <h3>{this.state.CDOView ? 'Internal CDO View' : 'External Bureau View'}</h3>
-          <div className="export-button-container">
-            <ExportButton />
-          </div>
+  return (
+    <div className="usa-width-one-whole bidder-manager-bidders ab-lower-section">
+      <div className="ab-table-title-row">
+        <h3>{cdoView ? 'Internal CDO View' : 'External Bureau View'}</h3>
+        <div className="export-button-container">
+          <ExportButton />
         </div>
-        <table className="bidder-manager-bidders">
-          <thead>
-            <tr />
-            <tr>
-              {
-                tableHeaders.map(item => (
-                  <th className="ab-headers" scope="col">{item} <FA name="sort" /></th>
-                ))
-              }
-              <th className="action-header">
-                <div className="bureau-view-toggle">
-                  <ToggleButton
-                    labelTextLeft={
-                      <Tooltip
-                        title="CDO View"
-                        arrow
-                        offset={-95}
-                        position="top-end"
-                        tabIndex="0"
-                      >
-                        <FA name="street-view" className="fa-lg" />
-                      </Tooltip>
-                    }
-                    labelTextRight={
-                      <Tooltip
-                        title="Bureau View"
-                        arrow
-                        offset={-95}
-                        position="top-end"
-                        tabIndex="0"
-                      >
-                        <FA name="building" className="fa-lg" />
-                      </Tooltip>
-                    }
-                    checked={!this.state.CDOView}
-                    onChange={() => this.setState({ CDOView: !this.state.CDOView })}
-                    onColor="#888"
-                  />
-                </div>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {[...Array(10).keys()].map((m) => {
-              return (<AvailableBidderRow
-                id={shortid.generate()}
-                bidder={bidder}
-                isLoading={biddersIsLoading}
-                showModal={() => this.showModal()}
-                CDOView={this.state.CDOView}
-              />);
-            })}
-          </tbody>
-        </table>
       </div>
-    );
-  }
-}
+      <table className="bidder-manager-bidders">
+        <thead>
+          <tr />
+          <tr>
+            {
+              tableHeaders.map(item => (
+                <th className="ab-headers" scope="col">{item} <FA name="sort" /></th>
+              ))
+            }
+            <th className="action-header">
+              <div className="bureau-view-toggle">
+                <ToggleButton
+                  labelTextLeft={
+                    <Tooltip
+                      title="CDO View"
+                      arrow
+                      offset={-95}
+                      position="top-end"
+                      tabIndex="0"
+                    >
+                      <FA name="street-view" className="fa-lg" />
+                    </Tooltip>
+                  }
+                  labelTextRight={
+                    <Tooltip
+                      title="Bureau View"
+                      arrow
+                      offset={-95}
+                      position="top-end"
+                      tabIndex="0"
+                    >
+                      <FA name="building" className="fa-lg" />
+                    </Tooltip>
+                  }
+                  checked={!cdoView}
+                  onChange={() => setCdoView(!cdoView)}
+                  onColor="#888"
+                />
+              </div>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {[...Array(10).keys()].map(() => (<AvailableBidderRow
+            id={shortid.generate()}
+            bidder={bidder}
+            isLoading={biddersIsLoading}
+            showModal={modalContent}
+            CDOView={cdoView}
+          />))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 Bidders.propTypes = {
   biddersIsLoading: PropTypes.bool,
@@ -246,3 +233,24 @@ Bidders.defaultProps = {
 };
 
 export default Bidders;
+
+// const mapStateToProps = state => ({
+//   bureauPositions: state.bureauPositions,
+//   bureauPositionsIsLoading: state.bureauPositionsIsLoading,
+//   bureauPositionsHasErrored: state.bureauPositionsHasErrored,
+//   bureauFilters: state.filters,
+//   bureauFiltersHasErrored: state.filtersHasErrored,
+//   bureauFiltersIsLoading: state.filtersIsLoading,
+//   bureauPermissions: state.userProfile.bureau_permissions,
+//   orgPermissions: state.userProfile.org_permissions,
+//   userSelections: state.bureauUserSelections,
+// });
+
+// export const mapDispatchToProps = dispatch => ({
+//   fetchBureauPositions: query => dispatch(bureauPositionsFetchData(query)),
+//   fetchFilters: (items, queryParams, savedFilters) =>
+//     dispatch(filtersFetchData(items, queryParams, savedFilters)),
+//   saveSelections: (selections) => dispatch(saveBureauUserSelections(selections)),
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(PositionManager);
