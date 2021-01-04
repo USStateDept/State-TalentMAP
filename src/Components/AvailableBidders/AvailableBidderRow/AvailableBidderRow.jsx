511c846db826b5c50f8d39c2c2b9d1573aad75e2
scott062 <scott.m.burdick@gmail.com>
2021-01-04 17:37:45 -0500

    Move modal logic to row componenet - troubleshoot modal not capturing state in action promise

--- src/Components/AvailableBidders/AvailableBidderRow/AvailableBidderRow.jsx
+++ src/Components/AvailableBidders/AvailableBidderRow/AvailableBidderRow.jsx
@@ -1,34 +1,94 @@
-// import React, { useState } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Skeleton from 'react-loading-skeleton';
 import { get, keys } from 'lodash';
 import { formatDate } from 'utilities';
 import { EMPTY_FUNCTION } from 'Constants/PropTypes';
+import { availableBiddersToggleUser, availableBidderEditData } from 'actions/cdo';
+import { useDispatch } from 'react-redux';
 import { NO_GRADE, NO_END_DATE, NO_CDO, NO_BUREAU, NO_USER_SKILL_CODE, NO_OC_REASON, NO_POST, NO_STATUS, NO_COMMENTS } from 'Constants/SystemMessages';
 // import EditBidder from 'Components/AvailableBidders/EditBidder';
 import FA from 'react-fontawesome';
 import { Tooltip } from 'react-tippy';
+import swal from '@sweetalert/with-react';
 
 
 const AvailableBidderRow = (props) => {
-  const { bidder, CDOView, isLoading, showModal } = props;
+  const { bidder, CDOView, isLoading } = props;
+
+  const [showModal, setShowModal] = useState(false);
+  const [status, setStatus] = useState(get(bidder, 'status', NO_STATUS));
+  const [ocBureau, setOCBureau] = useState(get(bidder, 'current_assignment.position.bureau', NO_BUREAU));
+  const [ocReason, setOCReason] = useState(get(bidder, 'oc_reason', NO_OC_REASON));
+  const [comment, setComment] = useState(get(bidder, 'comments', NO_COMMENTS));
+
+
+  const dispatch = useDispatch();
 
   const shared = get(bidder, 'is_shared', false);
   const ted = get(bidder, 'TED');
   const formattedTed = ted ? formatDate(ted) : NO_END_DATE;
+  const id = get(bidder, 'bidder_perdet');
 
   const sections = {
     Name: (<Link to={`/profile/public/${get(bidder, 'emp_id')}/bureau`}>{get(bidder, 'name')}</Link>),
-    Status: get(bidder, 'status', NO_STATUS),
+    Status: status,
     Skill: get(bidder, 'skills[0].description', NO_USER_SKILL_CODE),
     Grade: get(bidder, 'grade', NO_GRADE),
     TED: formattedTed,
     Current_Post: get(bidder, 'post.location.country', NO_POST),
-    OC_Bureau: get(bidder, 'current_assignment.position.bureau', NO_BUREAU),
-    OC_Reason: get(bidder, 'oc_reason', NO_OC_REASON),
+    OC_Bureau: ocBureau,
+    OC_Reason: ocReason,
     CDO: get(bidder, 'cdo.name', NO_CDO),
-    Comments: get(bidder, 'comments', NO_COMMENTS),
+    Comments: comment,
+  };
+
+  const userInputs = {
+    oc_bureau: ocBureau,
+    oc_reason: ocReason,
+    status,
+    comments: comment,
+  };
+
+  const availableBidderModal = () => {
+    setShowModal(!showModal);
+    swal({
+      title: 'Available Bidder Record Editor',
+      buttons: ['Cancel', 'Save'],
+      show: showModal,
+      content: (
+        <div>
+          <form>
+            <label htmlFor="name">Client Name:</label>
+            <input type="text" name="name" disabled value={get(bidder, 'name')} />
+            <label htmlFor="status">Status:</label>
+            <select id="status" onChange={(e) => setStatus(e.value)} defaultValue={status}>
+              <option value="OC">OC: Overcompliment</option>
+              <option value="UA">UA: Unassigned</option>
+              <option value="IT">IT: In Transit</option>
+              <option value="AWOL">AWOL: Absent without leave</option>
+            </select>
+            <label htmlFor="skill">Skill:</label>
+            <input type="text" name="skill" disabled value={sections.Skill} />
+            <label htmlFor="grade">Grade:</label>
+            <input type="text" name="grade" disabled value={sections.Grade} />
+            <label htmlFor="ted">TED:</label>
+            <input type="text" name="ted" disabled value={sections.TED} />
+            <label htmlFor="currentPost">Current Post:</label>
+            <input type="text" name="currentPost" disabled value={sections.Current_Post} />
+            <label htmlFor="ocBureau">OC Bureau:</label>
+            <input type="text" name="ocBureau" defaultValue={ocBureau} onChange={(e) => setOCBureau(e.value)} />
+            <label htmlFor="ocReason">OC Reason:</label>
+            <input type="text" name="ocReason" defaultValue={ocReason} onChange={(e) => setOCReason(e.value)} />
+            <label htmlFor="cdo">CDO:</label>
+            <input type="text" name="cdo" disabled value={sections.CDO} />
+            <label htmlFor="comment">Comment:</label>
+            <input type="text" name="comment" defaultValue={comment} onChange={(e) => setComment(e.value)} />
+          </form>
+        </div>
+      ),
+    }).then(() => dispatch(availableBidderEditData(id, userInputs)));
   };
 
   return (
@@ -52,7 +112,7 @@ const AvailableBidderRow = (props) => {
             position="top-end"
             tabIndex="0"
           >
-            <FA name="pencil-square-o" className="fa-lg" onClick={showModal} />
+            <FA name="pencil-square-o" className="fa-lg" onClick={availableBidderModal} />
           </Tooltip>
           <Tooltip
             title="Share with Bureaus"
@@ -61,7 +121,11 @@ const AvailableBidderRow = (props) => {
             position="top-end"
             tabIndex="0"
           >
-            { shared ? <FA name="building" className="fa-lg" /> : <FA name="building-o" className="fa-lg" />}
+            <FA
+              name={shared ? 'building' : 'building-o'}
+              className="fa-lg"
+              onClick={() => dispatch(availableBidderEditData(id, { is_shared: !shared }))}
+            />
           </Tooltip>
           <Tooltip
             title="Remove from Available Bidders List"
@@ -70,7 +134,7 @@ const AvailableBidderRow = (props) => {
             position="top-end"
             tabIndex="0"
           >
-            <FA name="trash-o" className="fa-lg" />
+            <FA name="trash-o" className="fa-lg" onClick={() => dispatch(availableBiddersToggleUser(id, true))} />
           </Tooltip>
         </div>
       </td>
@@ -83,7 +147,6 @@ AvailableBidderRow.propTypes = {
   bidder: PropTypes.object,
   CDOView: PropTypes.bool,
   isLoading: PropTypes.bool,
-  showModal: PropTypes.func,
 };
 
 AvailableBidderRow.defaultProps = {
--- src/Components/AvailableBidders/Bidders/Bidders.jsx
+++ src/Components/AvailableBidders/Bidders/Bidders.jsx
@@ -10,13 +10,11 @@ import AvailableBidderRow from 'Components/AvailableBidders/AvailableBidderRow';
 import FA from 'react-fontawesome';
 import { Tooltip } from 'react-tippy';
 import shortid from 'shortid';
-import swal from '@sweetalert/with-react';
 
 
 const Bidders = () => {
   // Local state
   const [cdoView, setCdoView] = useState(true);
-  const [showModal, setShowModal] = useState(false);
 
   // App state
   const biddersData = useSelector(state => state.availableBiddersFetchDataSuccess);
@@ -31,50 +29,6 @@ const Bidders = () => {
     dispatch(availableBiddersFetchData());
   }, []);
 
-
-  const modalContent = () => {
-    setShowModal(!showModal);
-    swal({
-      title: 'Available Bidder Record Editor',
-      buttons: ['Cancel', 'Save'],
-      show: showModal,
-      content: (
-        <div>
-          <form>
-            <label htmlFor="name">Client Name:</label>
-            <input type="text" name="name" disabled value="Jenny Townpost" />
-            <label htmlFor="status">Status:</label>
-            <select id="status">
-              <option value="OC">OC: Overcompliment</option>
-              <option value="UA">UA: Unassigned</option>
-              <option value="IT">IT: In Transit</option>
-              <option value="AWOL">AWOL: Absent without leave</option>
-            </select>
-            <label htmlFor="skill">Skill:</label>
-            <input type="text" name="skill" disabled value="0020 - EXECUTIVE (CAREER)" />
-            <label htmlFor="grade">Grade:</label>
-            <input type="text" name="grade" disabled value="00" />
-            <label htmlFor="division">Division:</label>
-            <input type="text" name="division" disabled value="ML" />
-            <label htmlFor="ted">TED:</label>
-            <input type="text" name="ted" disabled value="11/11/11" />
-            <label htmlFor="beginDate">Begin Date:</label>
-            <input type="text" name="beginDate" disabled value="11/11/13" />
-            <label htmlFor="currentPost">Current Post:</label>
-            <input type="text" name="currentPost" disabled value="Djibouti" />
-            <label htmlFor="ocBureau">OC Bureau:</label>
-            <input type="text" name="ocBureau" disabled value="(EUR) BUR OF EUROPEAN AFF AND EURASIAN AFFAIRS" />
-            <label htmlFor="ocReason">OC Reason:</label>
-            <input type="text" name="ocReason" disabled value="Compassionate Curtailment" />
-            <label htmlFor="cdo">CDO:</label>
-            <input type="text" name="cdo" disabled value="Leah Shadrach" />
-            <label htmlFor="comment">Comment:</label>
-            <input type="text" name="comment" />
-          </form>
-        </div>
-      ),
-    });
-  };
 
   const tableHeaders = [
     'Name',
@@ -145,7 +99,6 @@ const Bidders = () => {
             id={shortid.generate()}
             bidder={bidder}
             isLoading={biddersIsLoading}
-            showModal={modalContent}
             CDOView={cdoView}
           />))}
         </tbody>
@@ -164,13 +117,3 @@ Bidders.defaultProps = {
 };
 
 export default Bidders;
-
-
-// export const mapDispatchToProps = dispatch => ({
-//   fetchBureauPositions: query => dispatch(bureauPositionsFetchData(query)),
-//   fetchFilters: (items, queryParams, savedFilters) =>
-//     dispatch(filtersFetchData(items, queryParams, savedFilters)),
-//   saveSelections: (selections) => dispatch(saveBureauUserSelections(selections)),
-// });
-
-// export default connect(mapStateToProps, mapDispatchToProps)(PositionManager);
