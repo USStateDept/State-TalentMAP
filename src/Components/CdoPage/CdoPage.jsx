d6319fe9e4410c9e29fffe37c4491fe23d39119a
scott062 <scott.m.burdick@gmail.com>
2021-01-25 10:28:52 -0500

    Renaming again

--- src/Components/CdoPage/CdoPage.jsx
+++ src/Components/CdoPage/CdoPage.jsx
@@ -1,11 +1,11 @@
 import { Route, Switch } from 'react-router-dom';
 import BidderPortfolio from 'Containers/BidderPortfolio';
-import BidderManager from 'Components/AvailableBidders/BidderManager';
+import AvailableBidderContainer from 'Components/AvailableBidder/AvailableBidderContainer';
 
 const CdoPage = () => (
   <div className="usa-grid-full profile-content-container">
     <Switch>
-      <Route path="/profile/cdo/availablebidders" render={() => <BidderManager isCDO />} />
+      <Route path="/profile/cdo/availablebidders" render={() => <AvailableBidderContainer isCDO />} />
       <Route path="/profile/cdo/bidderportfolio" render={() => <BidderPortfolio />} />
     </Switch>
   </div>
