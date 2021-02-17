f4c2e23d05dfebbbe9c036afcd1944ebc7d206c9
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2021-02-17 11:08:24 -0500

    revert export syntax

--- src/Constants/Tooltips.js
+++ src/Constants/Tooltips.js
@@ -1,6 +1,5 @@
-const DECONFLICT_TOOLTIP_TEXT = {
+// eslint-disable-next-line import/prefer-default-export
+export const DECONFLICT_TOOLTIP_TEXT = {
   title: 'Deconflict Alert',
   text: 'Candidate Ranked #1 on, at least, one other Short List',
 };
-
-export default { DECONFLICT_TOOLTIP_TEXT };
