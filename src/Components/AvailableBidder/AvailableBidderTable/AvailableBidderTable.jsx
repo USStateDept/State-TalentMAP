f4060ac374198b44f0317ac36611971f4ca316ef
scott062 <scott.m.burdick@gmail.com>
2021-02-18 12:36:45 -0500

    Fix text for overcomplement and add empty alert for bureau AB

--- src/Components/AvailableBidder/AvailableBidderTable/AvailableBidderTable.jsx
+++ src/Components/AvailableBidder/AvailableBidderTable/AvailableBidderTable.jsx
@@ -108,7 +108,11 @@ const AvailableBidderTable = (props) => {
       <div className="usa-width-two-thirds">
         <Alert
           title="Available Bidders List is Empty"
-          messages={[{ body: 'Please navigate to the CDO Client Profiles to begin searching and adding bidders.' }]}
+          messages={[{
+            body: isCDO ?
+              'Please navigate to the CDO Client Profiles to begin searching and adding bidders.' :
+              'Please wait for CDOs to add available bidders.',
+          }]}
         />
       </div>
       :
