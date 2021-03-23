47acfdcd78a6db4c86920fca17bee6b2860058d9
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2021-03-23 16:57:26 -0400

    test updated

--- src/reducers/bidTracker/bidTracker.test.js
+++ src/reducers/bidTracker/bidTracker.test.js
@@ -2,6 +2,7 @@ import * as reducers from './bidTracker';
 
 describe('reducers', () => {
   it('can set reducer HANDSHAKE_OFFERED_NOTIFICATION', () => {
-    expect(reducers.handshakeOfferedNotification({ name: 'Alice', position_name: 'Assignment Officer', bidtracker: '/public/34/' }, { type: 'HANDSHAKE_OFFERED_NOTIFICATION', notificationInformation: { name: 'A', position_name: 'A/O', bidtracker: '/public/34/' } })).toBe(true);
+    expect(reducers.handshakeOfferedNotification({ name: 'Alice', position: { name: 'Assignment Officer', link: '/profile/bidtracker' } }, { type: 'HANDSHAKE_OFFERED_NOTIFICATION', notificationInformation: { name: 'Alice', position: { name: 'Assignment Officer', link: '/profile/bidtracker' } } }))
+      .toMatchObject({ name: 'Alice', position: { name: 'Assignment Officer', link: '/profile/bidtracker' } });
   });
 });
