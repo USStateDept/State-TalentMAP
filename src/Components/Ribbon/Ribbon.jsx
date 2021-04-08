2faf3588b2c8969a5007f79c58403646e532ff7e
Cory Covington <ccovington@metaphaseconsulting.com>
2021-04-08 12:27:39 -0400

    position details ribbon styling

--- src/Components/Ribbon/Ribbon.jsx
+++ src/Components/Ribbon/Ribbon.jsx
@@ -22,7 +22,7 @@ Ribbon.propTypes = {
   icon: PropTypes.string,
   text: PropTypes.string,
   containerProps: PropTypes.shape({}),
-  type: PropTypes.oneOf(['primary', 'secondary', 'tertiary', 'success']),
+  type: PropTypes.oneOf(['primary', 'secondary', 'tertiary', 'success', 'cn', 'htf', 'snd']),
   cutSide: PropTypes.oneOf(['left', 'right', 'both']),
   isWide: PropTypes.bool,
   showText: PropTypes.bool,
