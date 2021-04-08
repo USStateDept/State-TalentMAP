04fb6462df70ff379c8a458546557409bab4c1f7
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2021-04-08 11:23:30 -0400

    extracted icons obj

--- src/Components/BidderPortfolio/ClientBadgeList/ClientBadgeList.test.jsx
+++ src/Components/BidderPortfolio/ClientBadgeList/ClientBadgeList.test.jsx
@@ -1,6 +1,6 @@
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
-import Classifications from 'Constants/Classifications';
+import { Classifications } from 'Constants/Classifications';
 import ClientBadgeList from './ClientBadgeList';
 
 describe('ClientBadgeList', () => {
