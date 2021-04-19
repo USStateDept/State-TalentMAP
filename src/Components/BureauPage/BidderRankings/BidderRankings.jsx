80f3f3353d28b90776ced544a55d02e9986ce5d9
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2021-04-19 13:31:25 -0400

    fe showing be data

--- src/Components/BureauPage/BidderRankings/BidderRankings.jsx
+++ src/Components/BureauPage/BidderRankings/BidderRankings.jsx
@@ -21,6 +21,16 @@ const BidderRankings = ({ perdet }) => {
   // Actions
   const dispatch = useDispatch();
 
+  const positionTableHeaders = [
+    'Rank',
+    'Position',
+    'Post',
+    'Skill',
+    'Grade',
+    'Bid Cycle',
+    'Date of Submission',
+  ];
+
   function toggleRankingData() {
     setShowRankingData(!showRankingData);
   }
@@ -46,10 +56,54 @@ const BidderRankings = ({ perdet }) => {
         showRankingData &&
           <div className={'bidder-rankings-table-container'}>
             {
-              bidderRankingDataIsLoading ?
-                <Spinner type="bidder-rankings-table" size="small" />
-                :
-                <div>im hidden content</div>
+              bidderRankingDataIsLoading &&
+              <Spinner type="bidder-rankings-table" size="small" />
+            }
+            {
+              !bidderRankingDataIsLoading && bidderRankingData.results &&
+                <div>
+                  <table className={'candidate-manager-table'}>
+                    <thead>
+                      <tr className={'bidder-information'}>
+                        <th>SPECIAL AGENT</th>
+                        <th>Js current post</th>
+                        <th>EXECUTIVE (CAREER) (0020)</th>
+                        <th>00</th>
+                        <th />
+                        <th />
+                        <th />
+                      </tr>
+                      <tr className={'table-headers'}>
+                        {
+                          positionTableHeaders.map(item => (
+                            <th
+                              key={item}
+                              className="ab-headers"
+                              scope="col"
+                            >
+                              {item}
+                            </th>
+                          ))
+                        }
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {
+                        bidderRankingData.results.map(pos => (
+                          <tr>
+                            <td>{pos.ranking || 'unranked'}</td>
+                            <td>{pos.position.title}</td>
+                            <td>{pos.position.post.location.country}</td>
+                            <td>{pos.position.skill}</td>
+                            <td>{pos.position.grade}</td>
+                            <td>{pos.bidcycle}</td>
+                            <td>{pos.submitted_date}</td>
+                          </tr>
+                        ))
+                      }
+                    </tbody>
+                  </table>
+                </div>
             }
           </div>
       }
