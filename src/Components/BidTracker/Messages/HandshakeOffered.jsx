54e2e5e13cb1280c1ded3140119f3531053b66e9
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2021-05-07 15:28:00 -0400

    how dare you

--- src/Components/BidTracker/Messages/HandshakeOffered.jsx
+++ src/Components/BidTracker/Messages/HandshakeOffered.jsx
@@ -1,28 +1,18 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { get, includes } from 'lodash';
-import { history } from '../../../store';
 
-const HandshakeOffered = ({ name, message }) => {
-  const dismissToastsPaths = ['/profile/notifications', '/profile/bidtracker'];
-  history.listen((newLocation) => {
-    if (includes(dismissToastsPaths, get(newLocation, 'pathname'))) {
-      toast.dismiss();
-    }
-  });
-  return (
-    <span>
-      {name}, {message}
-      <div className="bottom-section">
-        Go to your Bid Tracker to accept/decline the Handshake
-        <Link to="/profile/bidtracker">
-          <button role="link" onClick={() => toast.dismiss()}>Bid Tracker</button>
-        </Link>
-      </div>
-    </span>
-  );
-};
+const HandshakeOffered = ({ name, message }) => (
+  <span>
+    {name}, {message}
+    <div className="bottom-section">
+      Go to your Bid Tracker to accept/decline the Handshake
+      <Link to="/profile/bidtracker">
+        <button role="link" onClick={() => toast.dismiss()}>Bid Tracker</button>
+      </Link>
+    </div>
+  </span>
+);
 
 HandshakeOffered.propTypes = {
   name: PropTypes.string.isRequired,
