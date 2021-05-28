b6390cbad37d1c96308913ba0132afbb86323e9d
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2021-05-28 10:25:35 -0400

    to lodash

--- src/Components/BidTracker/BidTrackerCardTop/BidTrackerCardTop.jsx
+++ src/Components/BidTracker/BidTrackerCardTop/BidTrackerCardTop.jsx
@@ -31,7 +31,7 @@ class BidTrackerCardTop extends Component {
     const { bid, hideDelete, showBidCount, useCDOView /* , questionText */ } = this.props;
     const { readOnly } = this.context;
     const { position_info } = bid;
-    const position = bid.position_info;
+    const position = get(bid, 'position_info.position');
     // const showQuestion = !!(questionText && questionText.text);
     const bidStatistics = get(position_info, 'bid_statistics[0]') || {};
     const post = get(position, 'post') || {};
