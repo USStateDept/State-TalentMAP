230fa42a8e4b5c29f899ecbfdf4aed2afb0261ba
Cory Covington <ccovington@metaphaseconsulting.com>
2021-06-16 15:40:09 -0400

    removing test code from another branch

--- src/Components/BureauPage/PositionManager/PositionManager.jsx
+++ src/Components/BureauPage/PositionManager/PositionManager.jsx
@@ -469,23 +469,6 @@ const PositionManager = props => {
                       includeSelectAll
                     />
                   </div>
-                  <div className="filter-div">
-                    <div className="label">Handshake Status:</div>
-                    <Picky
-                      placeholder="Select Handshake Status(es)"
-                      value={selectedPostIndicators}
-                      options={postIndicatorsOptions}
-                      onChange={setSelectedPostIndicators}
-                      numberDisplayed={2}
-                      multiple
-                      includeFilter
-                      dropdownHeight={255}
-                      renderList={renderSelectionList}
-                      valueKey="code"
-                      labelKey="description"
-                      includeSelectAll
-                    />
-                  </div>
                 </div>
               </div>
             </div>
