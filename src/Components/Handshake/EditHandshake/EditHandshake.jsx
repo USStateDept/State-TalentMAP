43000e0c721ea22b01e769dc29c3f67f1dfbe10c
scott062 <scott.m.burdick@gmail.com>
2021-06-22 23:11:05 -0400

    WIP add colors fix calendar

--- src/Components/Handshake/EditHandshake/EditHandshake.jsx
+++ src/Components/Handshake/EditHandshake/EditHandshake.jsx
@@ -1,20 +1,24 @@
 /* eslint-disable react/prop-types */
 // Remove after defining sections with real data
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 // import PropTypes from 'prop-types';
 // import { EMPTY_FUNCTION, FILTER } from 'Constants/PropTypes';
 import swal from '@sweetalert/with-react';
 import Calendar from 'react-calendar';
+import { differenceInCalendarDays } from 'date-fns';
 // import TimeInput from 'react-keyboard-time-input';
 
 const EditHandshake = props => {
   const { submitAction, personID, positionID } = props;
-  const [value, setValue] = useState(new Date());
+  // Offer date is defaulted to `now` until future business rules clarify functionality
+  // Expected to be able to dynamically determine if we should offer now or future HS start date
+  const [offerDate, setOfferDate] = useState(new Date());
+  const [expirationDate, setExpirationDate] = useState(new Date());
 
-  // useEffect(() => {
-  //   setValue(new Date());
-  //   setDateTime(new Date());
-  // }, [props]);
+  useEffect(() => {
+    setOfferDate();
+    setExpirationDate();
+  }, [props]);
 
   const submit = (e) => {
     e.preventDefault();
@@ -27,9 +31,11 @@ const EditHandshake = props => {
 
   const cancel = (e) => {
     e.preventDefault();
-    setValue(Date.now());
     swal.close();
   };
+
+  // TO-DO: Replace with business rule for enforcing hard-stop to bureau HS offers per cycle
+  const fakeBureauTimeline = [new Date(2021, 5, 1), new Date(2021, 6, 29)];
 
   // const getHSStartDate = () => {
   //   const date = dateTime;
@@ -51,6 +57,20 @@ const EditHandshake = props => {
   //   return time.toTimeString();
   // };
 
+  const isSameDay = (a, b) => differenceInCalendarDays(a, b) === 0;
+
+  const tileClassName = ({ date, view }) => {
+    if (view === 'month') {
+      if (isSameDay(new Date(), date)) {
+        return 'react-calender__tile--startDate';
+      }
+      if (fakeBureauTimeline.find(dDate => isSameDay(dDate, date))) {
+        return 'react-calendar__tile--cycleTimeline';
+      }
+    }
+    return '';
+  };
+
   return (
     <div>
       <form className="bureau-hs-form">
@@ -59,23 +79,21 @@ const EditHandshake = props => {
           <input
             type="text"
             name="handshakeStartDate"
-            defaultValue={value}
-            disabled
+            value={offerDate}
           />
-          <input
+          {/* <input
             type="text"
             name="handshakeStartTime"
             defaultValue={value}
             disabled
-          />
+          /> */}
         </div>
         <div>
           <label htmlFor="handshakeEndDate">Handshake expiration:</label>
           <input
             type="text"
             name="handshakeEndDate"
-            defaultValue={value}
-            disabled
+            value={expirationDate}
           />
           {/* <TimeInput
             id="time-input"
@@ -85,8 +103,11 @@ const EditHandshake = props => {
         </div>
         <div className="calendar-wrapper">
           <Calendar
-            onChange={() => {}}
-            value={value}
+            minDate={new Date()}
+            maxDate={fakeBureauTimeline[1]}
+            onChange={setExpirationDate}
+            value={expirationDate}
+            tileClassName={tileClassName}
           />
         </div>
 
