27132ce393fbffbff7115a32cb4c9384f4177492
Cory Covington <ccovington@metaphaseconsulting.com>
2021-10-06 13:09:13 -0400

    adding toggle functionality for grade/status

--- src/Components/AvailableBidder/AvailableBidderStats/AvailableBidderStats.jsx
+++ src/Components/AvailableBidder/AvailableBidderStats/AvailableBidderStats.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-console */
-/* eslint-disable max-len */
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { get } from 'lodash';
@@ -14,6 +12,12 @@ import { Row } from '../../Layout';
 
 const AvailableBidderStats = () => {
   const [showMore, setShowMore] = useState(false);
+  const [selectedStat, setSelectedStat] = useState('Status');
+
+  const statOptions = [
+    'Grade',
+    'Status',
+  ];
 
   // App state
   const biddersData = useSelector(state => state.availableBiddersFetchDataSuccess);
@@ -21,17 +25,17 @@ const AvailableBidderStats = () => {
 
   const stats = get(biddersData, 'stats', {});
 
-  const statsSum = Object.values(get(stats, 'grade', {})).reduce((a, b) => a + b, 0);
+  const statsSum = Object.values(get(stats, selectedStat, {})).reduce((a, b) => a + b, 0);
 
   let data = [
     {
-      status: [
+      Status: [
         { name: 'Overcompliment (OC)', key: 'OC', value: 0, color: '#112E51' },
         { name: 'Unassigned (UA)', key: 'UA', value: 0, color: '#205493' },
         { name: 'In Transit (IT)', key: 'IT', value: 0, color: '#9BDAF1' },
         { name: 'Absent without leave (AWOL)', key: 'AWOL', value: 0, color: '#02BFE7' },
       ],
-      grade: [
+      Grade: [
         { name: 'Grade 01', key: '01', value: 0, color: '#112E51' },
         { name: 'Grade 02', key: '02', value: 0, color: '#205493' },
         { name: 'Grade 03', key: '03', value: 0, color: '#87BCDE' },
@@ -48,7 +52,7 @@ const AvailableBidderStats = () => {
     },
   ];
 
-  data = data[0].grade.map(m => ({ ...m, value: get(stats.grade, m.key, 0) }));
+  data = data[0][selectedStat].map(m => ({ ...m, value: get(stats[selectedStat], m.key, 0) }));
 
   const data$ = data.map(m => {
     // handling division by zero
@@ -61,14 +65,6 @@ const AvailableBidderStats = () => {
 
   const chartData$ = data$.filter(f => f.value > 0);
   const isNoBidders = !get(biddersData, 'results', []).length;
-
-  const statOptions = [
-    'Grade',
-    'Status',
-  ];
-  const selectedStat = 'Status';
-
-  const showGradeChart = true;
 
   return (
     !availableBiddersIsLoading && !statsSum && !!isNoBidders ?
@@ -83,68 +79,17 @@ const AvailableBidderStats = () => {
                 </h3>
               </InteractiveElement>
             </div>
-            {/* {
-              showMore &&
-            <div className="usa-grid-full section statistics-section">
-              {
-                !!availableBiddersIsLoading && <LoadingText />
-              }
-              {
-                !availableBiddersIsLoading && !statsSum && 'There are no available bidders categorized by status.'
-              }
-              {
-                !availableBiddersIsLoading && !!statsSum &&
-                <div className="usa-grid-full flex">
-                  <div className="usa-width-one-fourth legend-container">
-                    <div className="usa-grid-full legend">
-                      <h4>Available Bidders Status Stats ({statsSum})</h4>
-                      {
-                        data$.map(m => (
-                          <div className="flex legend-item">
-                            <div
-                              className="legend-square"
-                              style={{ backgroundColor: m.color }}
-                            />
-                            <div className="legend-text">{`(${m.value}) ${m.name} ${m.percent}`}</div>
-                          </div>
-                        ))
-                      }
-                    </div>
-                  </div>
-                  <div className="usa-width-one-third chart-container">
-                    <PieChart width={400} height={400}>
-                      <Pie
-                        data={chartData$}
-                        cx={200}
-                        cy={200}
-                        labelLine={false}
-                        outerRadius={180}
-                        fill="#8884d8"
-                        dataKey="value"
-                      >
-                        {
-                        // eslint-disable-next-line react/no-array-index-key
-                          chartData$.map((entry, index) => <Cell key={`cell-${index}`} fill={entry.color} />)
-                        }
-                      </Pie>
-                    </PieChart>
-                  </div>
-                </div>
-              }
-            </div>
-            } */}
             {
               showMore &&
             <div className="usa-grid-full section statistics-section">
               {
-                showGradeChart &&
                 <div className="filter-div">
                   <div className="label">Chart Stats:</div>
                   <Picky
                     placeholder="Select Stats"
                     value={selectedStat}
                     options={statOptions}
-                    // onChange={setSelectedSkills}
+                    onChange={setSelectedStat}
                     numberDisplayed={2}
                     multiple
                     includeFilter
@@ -166,7 +111,7 @@ const AvailableBidderStats = () => {
                 <div className="usa-grid-full flex">
                   <div className="usa-width-one-fourth legend-container">
                     <div className="usa-grid-full legend">
-                      <h4>Available Bidders Grade Stats ({statsSum})</h4>
+                      <h4>Available Bidders {selectedStat} Stats ({statsSum})</h4>
                       {
                         data$.map(m => (
                           <div className="flex legend-item">
