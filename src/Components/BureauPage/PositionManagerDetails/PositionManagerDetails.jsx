a537e5f2b699110c804229bab5f7df63b0a95a19
scott062 <scott.m.burdick@gmail.com>
2021-10-29 14:00:41 -0400

    Replace isArchived with FP for bureau pos manager

--- src/Components/BureauPage/PositionManagerDetails/PositionManagerDetails.jsx
+++ src/Components/BureauPage/PositionManagerDetails/PositionManagerDetails.jsx
@@ -85,7 +85,7 @@ class PositionManagerDetails extends Component {
       bureauPosition, ranking, rankingIsLoading,
       classifications, classificationsIsLoading } = this.props;
     const isProjectedVacancy = false;
-    const isArchived = false;
+    const isFilled = get(bureauPosition, 'status_code') === 'FP';
     const OBCUrl$ = get(bureauPosition, 'position.post.post_overview_url');
     const title = get(bureauPosition, 'position.title');
     const filtersSelected = !!keys(filters).length;
@@ -116,7 +116,7 @@ class PositionManagerDetails extends Component {
                   <div className="usa-grid-full header-title-container padded-main-content">
                     <div className="position-details-header-title">
                       {isProjectedVacancy && <span>Projected Vacancy</span>}
-                      {isArchived && <span>Filled Position</span>}
+                      {isFilled && <span>Filled Position</span>}
                       <h1>{title}</h1>
                     </div>
                     <div className="post-title">
