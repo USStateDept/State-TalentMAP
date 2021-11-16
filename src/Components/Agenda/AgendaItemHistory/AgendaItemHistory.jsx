e659a968293eb98b77c7b5e92191ee0995ce6f37
Cory Covington <ccovington@metaphaseconsulting.com>
2021-11-16 14:24:00 -0500

    code cleanup

--- src/Components/Agenda/AgendaItemHistory/AgendaItemHistory.jsx
+++ src/Components/Agenda/AgendaItemHistory/AgendaItemHistory.jsx
@@ -1,69 +1,37 @@
-// /* eslint-disable no-unused-vars */
-/* eslint-disable max-len */
-// import { Component } from 'react';
-// import PropTypes from 'prop-types';
-// import { get } from 'lodash';
-// import { BIDDER_LIST, BIDDER_PORTFOLIO_COUNTS, CLASSIFICATIONS } from 'Constants/PropTypes';
-// import TotalResults from 'Components/TotalResults/TotalResults';
-// import ErrorBoundary from 'Components/ErrorBoundary';
-// import Spinner from '../../Spinner';
-// import BidderPortfolioContainer from '../../../Components/BidderPortfolio/BidderPortfolioContainer';
-// import TopNav from '../TopNav';
-// import BidControls from '../BidControls';
-// import EditButtons from '../EditButtons';
+import { useState } from 'react';
 import BidderPortfolioSearch from '../../../Components/BidderPortfolio/BidderPortfolioSearch';
 import ProfileSectionTitle from '../../ProfileSectionTitle';
 import ExportLink from '../../BidderPortfolio/ExportLink';
 import BackButton from '../../BackButton';
+import ResultsViewBy from '../../ResultsViewBy/ResultsViewBy';
 
-const AgendaItemHistory = () => (
-  <div>
-    <div className="bidder-portfolio-page card-view">
-      <BidderPortfolioSearch />
-      <BackButton />
-      <div className="usa-grid-full bidder-portfolio-container profile-content-inner-container">
-        <div className="usa-grid-full">
-          <div className="usa-width-one-half">
-            <ProfileSectionTitle title="Last, First - Agenda Item History" icon="user-circle-o" />
-          </div>
-          <div className="usa-width-one-half" style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <ExportLink disabled={false} />
+const AgendaItemHistory = () => {
+  const [viewType, setViewType] = useState('true');
+  const view = viewType ? 'card' : 'row';
+
+  return (
+    <div>
+      <div className="bidder-portfolio-page card-view">
+        <div className="usa-grid-full results-search-bar-container">
+          <ProfileSectionTitle title="Last, First - Agenda Item History" icon="user-circle-o" />
+          <BidderPortfolioSearch />
+        </div>
+        <div className="usa-grid-full bidder-portfolio-container profile-content-inner-container">
+          <div className="usa-grid-full">
+            <div className="usa-width-one-whole portfolio-sort-container results-dropdown">
+              <BackButton />
+              <div>
+                <ResultsViewBy initial={view} onClick={() => setViewType(!viewType)} />
+              </div>
+              <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <ExportLink disabled={false} />
+              </div>
+            </div>
           </div>
         </div>
-        {/* {
-          !navDataIsLoading &&
-          <div>
-            {useClientCounts &&
-              <TopNav bidderPortfolioCounts={bidderPortfolioCounts} />
-            }
-            <BidControls />
-          </div>
-        } */}
-        {/* <div className={`usa-grid-full bidder-portfolio-listing ${loadingClass}`}>
-          {
-            !isLoading && !hideControls &&
-            <div className="total-results-container">
-              <TotalResults
-                total={get(bidderPortfolio, 'count')}
-                pageNumber={pageNumber}
-                pageSize={pageSize}
-              />
-            </div>
-          }
-          {
-            isLoading &&
-            <Spinner type="homepage-position-results" size="big" />
-          }
-          {
-            !isLoading &&
-            <ErrorBoundary>
-              <BidderPortfolioContainer />
-            </ErrorBoundary>
-          }
-        </div> */}
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default AgendaItemHistory;
