96b02734a576260f085e6dd3e8413c8a95b0ffc0
Cory Covington <ccovington@metaphaseconsulting.com>
2021-11-22 17:12:31 -0500

    snaps and employee agenda clean up

--- src/Components/Agenda/EmployeeAgendaSearch/EmployeeAgendaSearch.jsx
+++ src/Components/Agenda/EmployeeAgendaSearch/EmployeeAgendaSearch.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import BidderPortfolioSearch from '../../BidderPortfolio/BidderPortfolioSearch';
 import ProfileSectionTitle from '../../ProfileSectionTitle';
 
 const EmployeeAgendaSearch = ({ isCDO }) => {
@@ -8,17 +7,14 @@ const EmployeeAgendaSearch = ({ isCDO }) => {
 
   return (
     <div>
-      <div className="employee-agenda-page">
-        <BidderPortfolioSearch />
-        <div className="usa-grid-full profile-content-inner-container">
-          <ProfileSectionTitle title="Employee Agenda Search" icon="user-circle-o" />
-        </div>
-        <div className="profile-content-inner-container">
-          <div className="usa-grid-full">
-            <h4>
-              <Link to={`/profile/${userRole}/agendaitemhistory/perdet`}>View Employee History</Link>
-            </h4>
-          </div>
+      <div className="usa-grid-full profile-content-inner-container">
+        <ProfileSectionTitle title="Employee Agenda Search" icon="user-circle-o" />
+      </div>
+      <div className="profile-content-inner-container">
+        <div className="usa-grid-full">
+          <h4>
+            <Link to={`/profile/${userRole}/agendaitemhistory/perdet`}>View Employee History</Link>
+          </h4>
         </div>
       </div>
     </div>
