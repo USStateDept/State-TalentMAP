ba2640f0d2336ff4c7ebc0d078eb1e4df430fbe8
Cory Covington <ccovington@metaphaseconsulting.com>
2021-11-23 10:23:43 -0500

    pr feedback/code simplification

--- src/Components/Agenda/AgendaItemHistory/AgendaItemHistory.jsx
+++ src/Components/Agenda/AgendaItemHistory/AgendaItemHistory.jsx
@@ -15,26 +15,18 @@ const AgendaItemHistory = () => {
     <div>
       <div className="usa-grid-full profile-content-inner-container">
         <ProfileSectionTitle title="Last Name, First Name - Agenda Item History" icon="user-circle-o" />
-      </div>
-      <div className="usa-grid-full profile-content-inner-container">
-        <div className="usa-grid-full">
-          <div className="usa-width-one-whole">
-            <BackButton />
-            <div className="usa-width-one-whole results-dropdown agenda-controls-container">
-              For Pagination Results
-              <div className="agenda-controls-right">
-                <div className="agenda-results-controls">
-                  <ResultsViewBy initial={view} onClick={() => setViewType(!viewType)} />
-                  {/* still needs to be intergrated into */}
-                  <SelectForm
-                    id="agenda-item-history-results"
-                    options={sorts.options}
-                    label="Sort by:"
-                  />
-                  <ExportLink disabled />
-                </div>
-              </div>
-            </div>
+        <BackButton />
+        <div className="usa-width-one-whole results-dropdown agenda-controls-container">
+          For Pagination Results
+          <div className="agenda-results-controls">
+            <ResultsViewBy initial={view} onClick={() => setViewType(!viewType)} />
+            {/* still needs to be intergrated into */}
+            <SelectForm
+              id="agenda-item-history-results"
+              options={sorts.options}
+              label="Sort by:"
+            />
+            <ExportLink disabled />
           </div>
         </div>
       </div>
--- src/Components/Agenda/EmployeeAgendaSearch/EmployeeAgendaSearch.jsx
+++ src/Components/Agenda/EmployeeAgendaSearch/EmployeeAgendaSearch.jsx
@@ -6,17 +6,11 @@ const EmployeeAgendaSearch = ({ isCDO }) => {
   const userRole = isCDO ? 'cdo' : 'ao';
 
   return (
-    <div>
-      <div className="usa-grid-full profile-content-inner-container">
-        <ProfileSectionTitle title="Employee Agenda Search" icon="user-circle-o" />
-      </div>
-      <div className="profile-content-inner-container">
-        <div className="usa-grid-full">
-          <h4>
-            <Link to={`/profile/${userRole}/agendaitemhistory/perdet`}>View Employee History</Link>
-          </h4>
-        </div>
-      </div>
+    <div className="usa-grid-full profile-content-inner-container">
+      <ProfileSectionTitle title="Employee Agenda Search" icon="user-circle-o" />
+      <h4>
+        <Link to={`/profile/${userRole}/agendaitemhistory/perdet`}>View Employee History</Link>
+      </h4>
     </div>
   );
 };
