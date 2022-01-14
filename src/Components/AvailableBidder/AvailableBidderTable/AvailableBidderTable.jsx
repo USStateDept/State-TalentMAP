d862e434c961f767137a47089d3905cd1f6317dc
Mike Joyce <joyce_michael@bah.com>
2022-01-14 09:06:44 -0500

    Fix double data fetch (#1920)
    * Fix double data fetch
    
    * tests, safety checks
    
    Co-authored-by: SidelineCory24 <55964163+SidelineCory24@users.noreply.github.com>

--- src/Components/AvailableBidder/AvailableBidderTable/AvailableBidderTable.jsx
+++ src/Components/AvailableBidder/AvailableBidderTable/AvailableBidderTable.jsx
@@ -13,6 +13,7 @@ import Alert from 'Components/Alert/Alert';
 import FA from 'react-fontawesome';
 import { Tooltip } from 'react-tippy';
 import shortid from 'shortid';
+import { useMount, usePrevious } from 'hooks';
 
 
 const AvailableBidderTable = props => {
@@ -34,20 +35,24 @@ const AvailableBidderTable = props => {
   const isLoading = availableBiddersIsLoading || filtersIsLoading;
 
 
-  const bureaus = filterData.filters.find(f => f.item.description === 'region');
+  const bureaus = (get(filterData, 'filters') || []).find(f => f.item.description === 'region');
 
   const bidders = isLoading ? [...new Array(10)] : get(biddersData, 'results', []);
+
+  const prevSort = usePrevious(sort);
 
   // Actions
   const dispatch = useDispatch();
 
-  useEffect(() => {
+  useMount(() => {
     dispatch(availableBiddersFetchData(isCDO, sort));
     dispatch(filtersFetchData(filterData, {}));
-  }, []);
+  });
 
   useEffect(() => {
-    dispatch(availableBiddersFetchData(isCDO, sort));
+    if (prevSort && sort && sort !== prevSort) {
+      dispatch(availableBiddersFetchData(isCDO, sort));
+    }
   }, [sort]);
 
   const tableHeaders = isCDO ? [
