ad0364de07e4dc039bf955a283042b6fa0c7dfb0
Cory Covington <ccovington@metaphaseconsulting.com>
2022-01-25 15:48:52 -0500

    hiding/expanding containers

--- src/Components/Agenda/AgendaItemMaintenanceContainer/AgendaItemMaintenanceContainer.jsx
+++ src/Components/Agenda/AgendaItemMaintenanceContainer/AgendaItemMaintenanceContainer.jsx
@@ -1,17 +1,29 @@
+import { useState } from 'react';
 import BackButton from '../../BackButton';
 
-const AgendaItemMaintenanceContainer = () => (
-  <div className="agenda-item-maintenace-container">
-    <BackButton />
-    <div>
-      <div className="maintenance-container-left">
-        Left Maintenance Container
-      </div>
-      <div className="maintenance-container-right">
-        Right Maintenance Container
+const AgendaItemMaintenanceContainer = () => {
+  const [view, setView] = useState(true);
+  const text = view ? 'Dual View' : 'Single View';
+
+  return (
+    <div className="agenda-item-maintenace-container">
+      <BackButton />
+      <button onClick={() => setView(!view)}>
+        {text}
+      </button>
+      <div>
+        <div className={`maintenance-container-left${view ? '' : '-expanded'}`}>
+          Left Maintenance Container
+        </div>
+        {
+          view &&
+          <div className="maintenance-container-right">
+            Right Maintenance Container
+          </div>
+        }
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default AgendaItemMaintenanceContainer;
