34c61b92e6f73e7f974bbe6721804b1b139287f4
Cory Covington <ccovington@metaphaseconsulting.com>
2022-01-25 22:57:06 -0500

    adding expand/collapse arrow

--- src/Components/Agenda/AgendaItemMaintenanceContainer/AgendaItemMaintenanceContainer.jsx
+++ src/Components/Agenda/AgendaItemMaintenanceContainer/AgendaItemMaintenanceContainer.jsx
@@ -1,22 +1,38 @@
 import { useState } from 'react';
+import FontAwesome from 'react-fontawesome';
+import InteractiveElement from 'Components/InteractiveElement';
+import { Tooltip } from 'react-tippy';
 import BackButton from '../../BackButton';
 
 const AgendaItemMaintenanceContainer = () => {
-  const [view, setView] = useState(true);
-  const text = view ? 'Dual View' : 'Single View';
+  const [expandContainer, setExpandContainer] = useState(true);
+
+  function toggleExpand() {
+    setExpandContainer(!expandContainer);
+  }
+
+  const rotate = expandContainer ? 'rotate(-180deg)' : 'rotate(0)';
 
   return (
     <div className="agenda-item-maintenace-container">
       <BackButton />
-      <button onClick={() => setView(!view)}>
-        {text}
-      </button>
       <div>
-        <div className={`maintenance-container-left${view ? '' : '-expanded'}`}>
+        <div className={`maintenance-container-left${expandContainer ? '' : '-expanded'}`}>
           Left Maintenance Container
+          <InteractiveElement onClick={toggleExpand}>
+            <Tooltip
+              title={expandContainer ? 'Expand container' : 'Collapse container'}
+              arrow
+            >
+              <FontAwesome
+                style={{ transform: rotate, transition: 'all 0.65s linear' }}
+                name="arrow-circle-left"
+              />
+            </Tooltip>
+          </InteractiveElement>
         </div>
         {
-          view &&
+          expandContainer &&
           <div className="maintenance-container-right">
             Right Maintenance Container
           </div>
