f914d94d8ccf101cbee541db57ddf0eea70503d1
Mike Joyce <joyce_michael@bah.com>
2022-02-11 15:20:03 -0500

    Use fuse.js for searching frequent positions

--- src/Components/Agenda/AgendaItemResearchPane/FrequentPositions/FrequentPositions.jsx
+++ src/Components/Agenda/AgendaItemResearchPane/FrequentPositions/FrequentPositions.jsx
@@ -1,19 +1,65 @@
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import FA from 'react-fontawesome';
+import Fuse from 'fuse.js';
 import InteractiveElement from 'Components/InteractiveElement';
+import FieldSet from 'Components/FieldSet/FieldSet';
+import TextInput from 'Components/TextInput';
 import { EMPTY_FUNCTION } from 'Constants/PropTypes';
+
+const fuseOptions = {
+  shouldSort: false,
+  findAllMatches: true,
+  tokenize: true,
+  includeScore: false,
+  threshold: 0.25,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 32,
+  minMatchCharLength: 1,
+  keys: [
+    'org', 'position_number', 'position_title',
+  ],
+};
 
 const FrequentPositions = (props) => {
   const headers = ['', 'Organization', 'Position Number', 'Position Title'];
 
   const { positions, onClick } = props;
 
+  const [positions$, setPositions$] = useState(positions);
+  const [term, setTerm] = useState('');
+
   const onClick$ = pos => {
     onClick(pos);
   };
 
+  const fuse$ = new Fuse(positions, fuseOptions);
+  const search = a => setPositions$(fuse$.search(a));
+
+  useEffect(() => {
+    setPositions$(positions);
+  }, [positions]);
+
+  const positions$$ = term ? positions$ : positions;
+
   return (
     <div className="frequent-positions-table">
+      <FieldSet
+        className="glossary-fieldset"
+        legend="Enter a keyword to search"
+        legendSrOnly
+      >
+        <TextInput
+          changeText={e => { search(e); setTerm(e); }}
+          value={term}
+          labelSrOnly
+          placeholder="Search for Frequent Positions"
+          inputProps={{
+            autoComplete: 'off',
+          }}
+        />
+      </FieldSet>
       <table>
         <thead>
           <tr>
@@ -22,7 +68,7 @@ const FrequentPositions = (props) => {
         </thead>
         <tbody>
           {
-            positions.map(m => (
+            positions$$.map(m => (
               <tr>
                 <td>
                   <InteractiveElement title="Add to Agenda Item" onClick={() => onClick$(m)}>
