2bf6ff9f0e78de9bdff9deef3626e5b62f3f8e48
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2022-02-22 08:59:45 -0500

    updated tests

--- src/Components/Agenda/AgendaItemCard/AgendaItemCard.test.jsx
+++ src/Components/Agenda/AgendaItemCard/AgendaItemCard.test.jsx
@@ -3,13 +3,31 @@ import toJSON from 'enzyme-to-json';
 import AgendaItemCard from './AgendaItemCard';
 
 describe('AgendaItemCard Component', () => {
+  const props = {
+    agenda: {
+      legs: [
+        {
+          pos_title: 'this long string should be shortened',
+        },
+        {
+          pos_title: 'short',
+        },
+      ],
+    },
+  };
+
   it('is defined', () => {
     const wrapper = shallow(<AgendaItemCard />);
     expect(wrapper).toBeDefined();
   });
 
-  xit('matches snapshot', () => {
+  it('matches snapshot', () => {
     const wrapper = shallow(<AgendaItemCard />);
     expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  it('properly renders titles', () => {
+    const wrapper = shallow(<AgendaItemCard {...props} />);
+    expect(wrapper.find('.ai-history-card-title').text()).toBe('this long string s...0short');
   });
 });
