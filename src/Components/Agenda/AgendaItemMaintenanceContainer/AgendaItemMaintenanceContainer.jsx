fb772fe701dbe627d1d8f2294544b62cae2a6e79
Cory Covington <ccovington@metaphaseconsulting.com>
2022-03-28 10:39:45 -0400

    merge conflict update

--- src/Components/Agenda/AgendaItemMaintenanceContainer/AgendaItemMaintenanceContainer.jsx
+++ src/Components/Agenda/AgendaItemMaintenanceContainer/AgendaItemMaintenanceContainer.jsx
@@ -19,7 +19,6 @@ const AgendaItemMaintenanceContainer = (props) => {
 
   const isCDO = get(props, 'isCDO');
   const bidder = 'LastName, FirstName';
-  const perdet = 6;
 
   function toggleExpand() {
     setLegsContainerExpanded(!legsContainerExpanded);
@@ -39,71 +38,73 @@ const AgendaItemMaintenanceContainer = (props) => {
   };
 
   return (
-    <div className="padded-main-content results-single-search homepage-offset">
-      <div className="usa-grid-full results-search-bar-container">
-        <ProfileSectionTitle title="Panel and Agenda Item Maintenance" icon="calendar" />
-        <MediaQuery breakpoint="screenXlgMin" widthType="max">
-          {matches => (
-            <div className={`ai-maintenance-container${matches ? ' stacked' : ''}`}>
-              <div className={`maintenance-container-left${(legsContainerExpanded || matches) ? '-expanded' : ''}`}>
-                <AgendaItemMaintenancePane leftExpanded={(legsContainerExpanded || matches)} />
-                <h3>
-                  {isCDO ? <Link to={`/profile/public/${perdet}`}>{bidder}</Link> : bidder}
-                </h3>
-                <AgendaItemTimeline />
+    <>
+      <div className="padded-main-content results-single-search homepage-offset">
+        <div className="usa-grid-full results-search-bar-container">
+          <ProfileSectionTitle title="Panel and Agenda Item Maintenance" icon="calendar" />
+          <MediaQuery breakpoint="screenXlgMin" widthType="max">
+            {matches => (
+              <div className={`ai-maintenance-container${matches ? ' stacked' : ''}`}>
+                <div className={`maintenance-container-left${(legsContainerExpanded || matches) ? '-expanded' : ''}`}>
+                  <AgendaItemMaintenancePane leftExpanded={(legsContainerExpanded || matches)} />
+                  <h3>
+                    {isCDO ? <Link to={`/profile/public/${id}`}>{bidder}</Link> : bidder}
+                  </h3>
+                  <AgendaItemTimeline />
+                </div>
+                <div className={`expand-arrow${matches ? ' hidden' : ''}`}>
+                  <InteractiveElement onClick={toggleExpand}>
+                    <Tooltip
+                      title={legsContainerExpanded ? 'Expand Research' : 'Collapse Research'}
+                      arrow
+                    >
+                      <FontAwesome
+                        style={{ transform: rotate, transition: 'all 0.65s linear' }}
+                        name="arrow-circle-left"
+                        size="lg"
+                      />
+                    </Tooltip>
+                  </InteractiveElement>
+                </div>
+                <div className={`maintenance-container-right${(legsContainerExpanded && !matches) ? ' hidden' : ''}`}>
+                  <AgendaItemResearchPane perdet={id} />
+                </div>
               </div>
-              <div className={`expand-arrow${matches ? ' hidden' : ''}`}>
-                <InteractiveElement onClick={toggleExpand}>
-                  <Tooltip
-                    title={legsContainerExpanded ? 'Expand Research' : 'Collapse Research'}
-                    arrow
-                  >
-                    <FontAwesome
-                      style={{ transform: rotate, transition: 'all 0.65s linear' }}
-                      name="arrow-circle-left"
-                      size="lg"
-                    />
-                  </Tooltip>
-                </InteractiveElement>
-              </div>
-              <div className={`maintenance-container-right${(legsContainerExpanded && !matches) ? ' hidden' : ''}`}>
-                <AgendaItemResearchPane perdet={id} />
-              </div>
-            </div>
-          )}
-        </MediaQuery>
-      </div>
-    </div>
-    <MediaQuery breakpoint="screenXlgMin" widthType="max">
-      {matches => (
-        <div className={`ai-maintenance-container${matches ? ' stacked' : ''}`}>
-          <div className={`maintenance-container-left${(legsContainerExpanded || matches) ? '-expanded' : ''}`}>
-            <AgendaItemMaintenancePane
-              leftExpanded={(legsContainerExpanded || matches)}
-              onAddRemarksClick={openRemarksResearchTab}
-            />
-            <AgendaItemTimeline />
-          </div>
-          <div className={`expand-arrow${matches ? ' hidden' : ''}`}>
-            <InteractiveElement onClick={toggleExpand}>
-              <Tooltip
-                title={legsContainerExpanded ? 'Expand Research' : 'Collapse Research'}
-                arrow
-              >
-                <FontAwesome
-                  style={{ transform: rotate, transition: 'all 0.65s linear' }}
-                  name="arrow-circle-left"
-                  size="lg"
-                />
-              </Tooltip>
-            </InteractiveElement>
-          </div>
-          <div className={`maintenance-container-right${(legsContainerExpanded && !matches) ? ' hidden' : ''}`}>
-            <AgendaItemResearchPane perdet={id} ref={researchPaneRef} />
-          </div>
+            )}
+          </MediaQuery>
         </div>
-      )}
-    </MediaQuery>
+      </div>
+      <MediaQuery breakpoint="screenXlgMin" widthType="max">
+        {matches => (
+          <div className={`ai-maintenance-container${matches ? ' stacked' : ''}`}>
+            <div className={`maintenance-container-left${(legsContainerExpanded || matches) ? '-expanded' : ''}`}>
+              <AgendaItemMaintenancePane
+                leftExpanded={(legsContainerExpanded || matches)}
+                onAddRemarksClick={openRemarksResearchTab}
+              />
+              <AgendaItemTimeline />
+            </div>
+            <div className={`expand-arrow${matches ? ' hidden' : ''}`}>
+              <InteractiveElement onClick={toggleExpand}>
+                <Tooltip
+                  title={legsContainerExpanded ? 'Expand Research' : 'Collapse Research'}
+                  arrow
+                >
+                  <FontAwesome
+                    style={{ transform: rotate, transition: 'all 0.65s linear' }}
+                    name="arrow-circle-left"
+                    size="lg"
+                  />
+                </Tooltip>
+              </InteractiveElement>
+            </div>
+            <div className={`maintenance-container-right${(legsContainerExpanded && !matches) ? ' hidden' : ''}`}>
+              <AgendaItemResearchPane perdet={id} ref={researchPaneRef} />
+            </div>
+          </div>
+        )}
+      </MediaQuery>
+    </>
   );
 };
 
