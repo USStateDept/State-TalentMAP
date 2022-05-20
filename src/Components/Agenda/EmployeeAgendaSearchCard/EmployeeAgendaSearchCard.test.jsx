da83a8e4942e25d1d4a32c40b105f3665438c875
Cory Covington <ccovington@metaphaseconsulting.com>
2022-05-20 12:33:10 -0400

    test and snapshot updates

--- src/Components/Agenda/EmployeeAgendaSearchCard/EmployeeAgendaSearchCard.test.jsx
+++ src/Components/Agenda/EmployeeAgendaSearchCard/EmployeeAgendaSearchCard.test.jsx
@@ -1,15 +1,34 @@
-import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
+import TestUtils from 'react-dom/test-utils';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import EmployeeAgendaSearchCard from './EmployeeAgendaSearchCard';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
 
 describe('EmployeeAgendaSearchCards', () => {
   it('is defined', () => {
-    const wrapper = shallow(<EmployeeAgendaSearchCard />);
+    const wrapper = TestUtils.renderIntoDocument(
+      <Provider store={mockStore({})}>
+        <MemoryRouter>
+          <EmployeeAgendaSearchCard />
+        </MemoryRouter>
+      </Provider>,
+    );
     expect(wrapper).toBeDefined();
   });
 
   it('matches snapshot', () => {
-    const wrapper = shallow(<EmployeeAgendaSearchCard />);
+    const wrapper = TestUtils.renderIntoDocument(
+      <Provider store={mockStore({})}>
+        <MemoryRouter>
+          <EmployeeAgendaSearchCard />
+        </MemoryRouter>
+      </Provider>,
+    );
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
 });
