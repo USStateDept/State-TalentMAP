8c2243c1a61b5120ff899675b16ee1486dee7dcf
pveland621 <31417027+pveland621@users.noreply.github.com>
2022-06-13 15:50:00 -0400

    Fixing collapsed menu sorting

--- src/Components/ProfileMenu/ProfileMenuCollapsed/ProfileMenuCollapsed.jsx
+++ src/Components/ProfileMenu/ProfileMenuCollapsed/ProfileMenuCollapsed.jsx
@@ -1,9 +1,15 @@
 import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
-import { intersection } from 'lodash';
+import { filter, get, intersection, lowerCase, remove, sortBy } from 'lodash';
 import NavLinksContainer from '../NavLinksContainer';
 import NavLink from '../NavLink';
 import { GET_PROFILE_MENU } from '../../../Constants/Menu';
+
+let getProfileMenuSort = filter(GET_PROFILE_MENU(), { text: 'Profile' });
+const getProfileMenuSort$ = sortBy(remove(GET_PROFILE_MENU(),
+  menu => get(menu, 'text') !== 'Profile'), [(menu) => lowerCase(get(menu, 'text'))],
+);
+getProfileMenuSort = [...getProfileMenuSort, ...getProfileMenuSort$];
 
 const ProfileMenuCollapsed = ({ expand, roles, isGlossaryEditor }) => (
   <div className="usa-grid-full profile-menu profile-menu-collapsed">
@@ -14,7 +20,7 @@ const ProfileMenuCollapsed = ({ expand, roles, isGlossaryEditor }) => (
     </div>
     <NavLinksContainer>
       {
-        GET_PROFILE_MENU().map((item) => {
+        getProfileMenuSort.map((item) => {
           const int = intersection(item.roles, roles);
           const hasIntersection = !!int.length || !item.roles.length;
           const props = {
