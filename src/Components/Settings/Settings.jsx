7e139fe7faac6781acfcba908b740332598e5b0c
pveland621 <31417027+pveland621@users.noreply.github.com>
2022-06-15 10:00:36 -0400

    WIP commit

--- src/Components/Settings/Settings.jsx
+++ src/Components/Settings/Settings.jsx
@@ -1,90 +1,80 @@
-import { Component } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import ProfileSectionTitle from 'Components/ProfileSectionTitle';
 import { fetchJWT, fetchUserToken } from 'utilities';
+// import { get } from 'lodash';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+// import PropTypes from 'prop-types';
+// import { USER_PROFILE } from '../../Constants/PropTypes';
 // mport api from '../../api';
 
-class RadioList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: '',
-      copied: false,
-    };
-  }
+const RadioList = () => {
+  const [copied, setCopied] = useState(false);
+  // const [value, setValue] = useState('');
 
-  getTokens = () => {
+  const getTokens = () => {
     const token = fetchUserToken();
     const jwt = fetchJWT();
     const json = {
       token, jwt,
     };
     return JSON.stringify(json, undefined, 2);
-  }
+  };
 
-  fetchUserInfo = () => {
-    // get role here
-    // const getBureauPermissions = () => api().get('/fsbid/employee/bureau_permissions/');
-    // const getOrgPermissions = () => api().get('/fsbid/employee/org_permissions/');
+  const user = useSelector(state => state.userProfile);
+  // const { userProfile } = this.props;
+  // const name = get(userProfile, 'user.first_name');
 
-    // i think this is where promise stuff goes
-  }
-
-  render() {
-    const tokens = this.getTokens();
-    return (
-      <div className="usa-grid-full favorite-positions-container profile-content-inner-container">
-        <div className="usa-grid-full favorites-top-section">
-          <div className="favorites-title-container">
-            <ProfileSectionTitle title="Settings" icon="cogs" />
-          </div>
-        </div>
-        <div className="usa-grid-full">
-          <p>
-            Use this page to copy your auth tokens for troubleshooting. Click the button below to
-            copy your auth token to your clipboard. Do not share this for any reason besides
-            troubleshooting by the TalentMAP or Help Desk teams.
-          </p>
-          <pre style={{ width: 500 }}>
-            <span style={{ backgroundColor: 'white' }}>
-              {tokens}
-            </span>
-          </pre>
-          <CopyToClipboard
-            text={tokens}
-            onCopy={() => this.setState({ copied: true })}
-          >
-            <button>Copy to clipboard</button>
-          </CopyToClipboard>
-          {this.state.copied ? <span>Copied!</span> : null}
-        </div>
-        <div>
-          Roles
-          <div>
-            Role 1
-            Role 2
-            Role 3
-          </div>
-        </div>
-        <div>
-          Bureau Permissions
-          <div>
-            Permission 1
-            Permission 2
-            Permission 3
-          </div>
-        </div>
-        <div>
-          Org Permissions
-          <div>
-            Permission 1
-            Permission 2
-            Permission 3
-          </div>
+  return (
+    <div className="usa-grid-full favorite-positions-container profile-content-inner-container">
+      <div className="usa-grid-full favorites-top-section">
+        <div className="favorites-title-container">
+          <ProfileSectionTitle title="Settings" icon="cogs" />
         </div>
       </div>
-    );
-  }
-}
+      <div className="usa-grid-full">
+        <p>
+          Use this page to copy your auth tokens for troubleshooting. Click the button below to
+          copy your auth token to your clipboard. Do not share this for any reason besides
+          troubleshooting by the TalentMAP or Help Desk teams.
+        </p>
+        <pre style={{ width: 500 }}>
+          <span style={{ backgroundColor: 'white' }}>
+            {getTokens()}
+          </span>
+        </pre>
+        <CopyToClipboard
+          text={getTokens()}
+          onCopy={setCopied(true)}
+        >
+          <button>Copy to clipboard</button>
+        </CopyToClipboard>
+        {copied ? <span>Copied!</span> : null}
+      </div>
+      <div>
+        Roles
+        <div>
+          {user.permission_groups}
+        </div>
+      </div>
+      {/* <div>
+        Bureau Permissions
+        <div>
+          Permission 1
+          Permission 2
+          Permission 3
+        </div>
+      </div>
+      <div>
+        Org Permissions
+        <div>
+          Permission 1
+          Permission 2
+          Permission 3
+        </div>
+      </div> */}
+    </div>
+  );
+};
 
 export default RadioList;
