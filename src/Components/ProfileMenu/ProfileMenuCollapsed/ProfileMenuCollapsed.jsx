cd657a1eaa1bebdbe136528cd46dfbbfe701be90
pveland621 <31417027+pveland621@users.noreply.github.com>
2022-06-16 16:44:26 -0400

    Refactoring code for readability

--- src/Components/ProfileMenu/ProfileMenuCollapsed/ProfileMenuCollapsed.jsx
+++ src/Components/ProfileMenu/ProfileMenuCollapsed/ProfileMenuCollapsed.jsx
@@ -5,11 +5,11 @@ import NavLinksContainer from '../NavLinksContainer';
 import NavLink from '../NavLink';
 import { GET_PROFILE_MENU } from '../../../Constants/Menu';
 
-let getProfileMenuSort = filter(GET_PROFILE_MENU(), { text: 'Profile' });
-const getProfileMenuSort$ = sortBy(remove(GET_PROFILE_MENU(),
+const getProfileOption = filter(GET_PROFILE_MENU(), { text: 'Profile' });
+const sortProfileMenu = sortBy(remove(GET_PROFILE_MENU(),
   menu => get(menu, 'text') !== 'Profile'), [(menu) => lowerCase(get(menu, 'text'))],
 );
-getProfileMenuSort = [...getProfileMenuSort, ...getProfileMenuSort$];
+const sortedMenu = [...getProfileOption, ...sortProfileMenu];
 
 const ProfileMenuCollapsed = ({ expand, roles, isGlossaryEditor }) => (
   <div className="usa-grid-full profile-menu profile-menu-collapsed">
@@ -20,7 +20,7 @@ const ProfileMenuCollapsed = ({ expand, roles, isGlossaryEditor }) => (
     </div>
     <NavLinksContainer>
       {
-        getProfileMenuSort.map((item) => {
+        sortedMenu.map((item) => {
           const int = intersection(item.roles, roles);
           const hasIntersection = !!int.length || !item.roles.length;
           const props = {
