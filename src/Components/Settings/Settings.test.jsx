914546520fac7a44851bce00e4ad8d1b74d2c285
pveland621 <31417027+pveland621@users.noreply.github.com>
2022-06-17 14:44:57 -0400

    Updating snapshots

--- src/Components/Settings/Settings.test.jsx
+++ src/Components/Settings/Settings.test.jsx
@@ -1,18 +1,33 @@
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import Settings from './Settings';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
 
 describe('SettingsComponent', () => {
   it('is defined', () => {
     const wrapper = shallow(
-      <Settings />,
+      <Provider store={mockStore({})}>
+        <MemoryRouter>
+          <Settings />
+        </MemoryRouter>
+      </Provider>,
     );
     expect(wrapper).toBeDefined();
   });
 
   it('matches snapshot', () => {
     const wrapper = shallow(
-      <Settings />,
+      <Provider store={mockStore({})}>
+        <MemoryRouter>
+          <Settings />
+        </MemoryRouter>
+      </Provider>,
     );
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
