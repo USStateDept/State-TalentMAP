1c758a219baaba73aaf4aa8b618256a835e1146a
pveland621 <31417027+pveland621@users.noreply.github.com>
2022-07-05 17:16:35 -0400

    EVEN MORE prop validation

--- src/Components/Agenda/RemarksPill/RemarksPill.jsx
+++ src/Components/Agenda/RemarksPill/RemarksPill.jsx
@@ -18,17 +18,15 @@ const RemarksPill = props => {
 };
 
 RemarksPill.propTypes = {
-  remark: PropTypes.arrayOf(
-    PropTypes.shape({
-      seq_num: PropTypes.number,
-      rc_code: PropTypes.string,
-      order_num: PropTypes.number,
-      short_desc_text: PropTypes.string,
-      mutually_exclusive_ind: PropTypes.string,
-      text: PropTypes.string,
-      active_ind: PropTypes.string,
-    }),
-  ),
+  remark: PropTypes.shape({
+    seq_num: PropTypes.number,
+    rc_code: PropTypes.string,
+    order_num: PropTypes.number,
+    short_desc_text: PropTypes.string,
+    mutually_exclusive_ind: PropTypes.string,
+    text: PropTypes.string,
+    active_ind: PropTypes.string,
+  }),
   isEditable: PropTypes.bool,
   updateSelection: PropTypes.func,
 };
