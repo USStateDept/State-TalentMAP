5b695a800d4c7111d7dfc44656920c7dd45ab11e
pveland621 <31417027+pveland621@users.noreply.github.com>
2022-07-07 14:35:05 -0400

    Initial setup commit

--- src/Components/Agenda/RemarksPill/RemarksPill.test.jsx
+++ src/Components/Agenda/RemarksPill/RemarksPill.test.jsx
@@ -1,6 +1,16 @@
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
+import sinon from 'sinon';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import configureStore from 'redux-mock-store';
+import { MemoryRouter } from 'react-router-dom';
+import TestUtils from 'react-dom/test-utils';
 import RemarksPill from './RemarksPill';
+// import AgendaItemMaintenanceContainer from '../AgendaItemMaintenanceContainer';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
 
 describe('RemarksPill', () => {
   const props = {
@@ -13,6 +23,8 @@ describe('RemarksPill', () => {
       short_desc_text: 'Promo Bd Recognized',
       text: 'Potential recognized by last promo board',
     },
+    isEditable: true,
+    userSelection: () => {},
   };
 
   it('is defined', () => {
@@ -27,11 +39,39 @@ describe('RemarksPill', () => {
 
   it('matches snapshot when remarks are provided', () => {
     const wrapper = shallow(<RemarksPill {...props} />);
+    // console.log(toJSON(wrapper));
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
 
-  it('displays a remark', () => {
+  xit('displays a remark', () => {
     const wrapper = shallow(<RemarksPill {...props} />);
     expect(wrapper.find('div').text()).toBe(props.remark.text);
+  });
+
+  it('shows an x when a remark isEditable', () => {
+    const wrapper = shallow(<RemarksPill {...props} />);
+    const expected = `${props.remark.text}<FontAwesome />`;
+    expect(wrapper.find('div').text()).toBe(expected);
+  });
+
+  xit('callback', () => {
+    const spy = sinon.spy();
+    const wrapper = shallow(<RemarksPill updateSelection={spy} />);
+    const button = wrapper.find('fa fa-times');
+    // console.log(button);
+    button.simulate('click');
+    // wrapper.updateSelection();
+    sinon.assert.calledOnce(spy);
+  });
+
+  xit('is defined', () => {
+    const wrapper = TestUtils.renderIntoDocument(
+      <Provider store={mockStore({})}>
+        <MemoryRouter>
+          <RemarksPill {...props} />
+        </MemoryRouter>
+      </Provider>,
+    );
+    expect(wrapper).toBeDefined();
   });
 });
