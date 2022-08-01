c4fcc185e26772f1da4c393aea1bcd4cd1c9a904
elizabeth <elizabeth.jimenez.cs@gmail.com>
2022-08-01 10:02:53 -0400

    tests and snaps

--- src/Components/Agenda/AgendaItemLegsForm/AgendaItemLegsForm.test.jsx
+++ src/Components/Agenda/AgendaItemLegsForm/AgendaItemLegsForm.test.jsx
@@ -1,15 +1,29 @@
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
-import AgendaItemLegs from './AgendaItemLegs';
+import AgendaItemLegsForm from './AgendaItemLegsForm';
+
+
+const legs = [{
+  id: 41387,
+  pos_title: 'REGIONAL SECURITY OFFICER',
+  pos_num: '57019000',
+  org: 'A',
+  eta: '2019-05-05T00:00:00.000Z',
+  ted: '2020-07-05T00:00:00.000Z',
+  tod: '1 YEAR',
+  grade: '00',
+  action: 'Extend',
+  travel: null,
+}];
 
 describe('AgendaItemLegs Component', () => {
   it('is defined', () => {
-    const wrapper = shallow(<AgendaItemLegs />);
+    const wrapper = shallow(<AgendaItemLegsForm legs={legs} />);
     expect(wrapper).toBeDefined();
   });
 
   it('matches snapshot', () => {
-    const wrapper = shallow(<AgendaItemLegs />);
+    const wrapper = shallow(<AgendaItemLegsForm legs={legs} />);
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
 });
