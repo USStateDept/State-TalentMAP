262577c0704273834db47981cbf2260bb9eecf59
Dihora, Mayur [USA] <dihora_mayur@bah.com>
2022-09-13 15:32:53 -0400

    TM-3373, TM-3374 - Set Default Sort in SavedSearch

--- src/Containers/SavedSearches/SavedSearches.jsx
+++ src/Containers/SavedSearches/SavedSearches.jsx
@@ -9,6 +9,8 @@ import { SAVED_SEARCH_PARENT_OBJECT } from 'Constants/PropTypes';
 import { DEFAULT_USER_PROFILE, POSITION_RESULTS_OBJECT } from 'Constants/DefaultProps';
 import { formQueryString } from 'utilities';
 import SavedSearchesMap from '../SavedSearchesMap';
+import { SAVED_SEARCH_SORTS } from '../../Constants/Sort';
+
 
 // Wrapper for anything related to saved searches
 // Make sure to update Components/ResultsMultiSearchHeader/bypassRoutes.js with any routes
@@ -17,7 +19,7 @@ class SavedSearchesContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      defaultSort: '',
+      defaultSort: SAVED_SEARCH_SORTS.defaultSort,
     };
   }
 
@@ -32,11 +34,13 @@ class SavedSearchesContainer extends Component {
   }
 
   getSavedSearches() {
+    // console.log('sort option is: ', this.state.defaultSort);
     this.props.savedSearchesFetchData(this.state.defaultSort);
   }
 
   getSortedSearches = type => {
     if (type.target && type.target.value) {
+      // console.log('sort option is: ', type.target.value);
       this.props.savedSearchesFetchData(type.target.value);
       this.setState({ defaultSort: type.target.value });
     }
