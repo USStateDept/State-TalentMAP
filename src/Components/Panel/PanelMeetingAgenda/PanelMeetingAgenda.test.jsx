215843e3c01bce7c54741ba1ebe7fb5ea9df8a3a
Cory Covington <ccovington@metaphaseconsulting.com>
2022-10-20 13:25:36 -0400

    test and snapshot updates

--- src/Components/Panel/PanelMeetingAgenda/PanelMeetingAgenda.test.jsx
+++ src/Components/Panel/PanelMeetingAgenda/PanelMeetingAgenda.test.jsx
@@ -1,34 +1,20 @@
-import TestUtils from 'react-dom/test-utils';
 import { shallow } from 'enzyme';
-import { MemoryRouter } from 'react-router-dom';
 import toJSON from 'enzyme-to-json';
 import PanelMeetingAgenda from './PanelMeetingAgenda';
 
 describe('PanelMeetingAgendaComponent', () => {
   it('is defined', () => {
-    const wrapper = TestUtils.renderIntoDocument(
-      <MemoryRouter>
-        <PanelMeetingAgenda isCDO />
-      </MemoryRouter>,
-    );
+    const wrapper = shallow(<PanelMeetingAgenda />);
     expect(wrapper).toBeDefined();
   });
 
   it('matches snapshot when CDO', () => {
-    const wrapper = shallow(
-      <MemoryRouter>
-        <PanelMeetingAgenda isCDO />
-      </MemoryRouter>,
-    );
+    const wrapper = shallow(<PanelMeetingAgenda isCDO />);
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
 
   it('matches snapshot when not CDO', () => {
-    const wrapper = shallow(
-      <MemoryRouter>
-        <PanelMeetingAgenda isCDO={false} />
-      </MemoryRouter>,
-    );
+    const wrapper = shallow(<PanelMeetingAgenda isCDO={false} />);
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
 });
