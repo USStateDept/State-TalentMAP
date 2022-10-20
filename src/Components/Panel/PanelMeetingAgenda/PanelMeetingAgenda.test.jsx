9c2342b95dbbec559c0832b6b94eebd5656bb424
Cory Covington <ccovington@metaphaseconsulting.com>
2022-10-20 13:37:12 -0400

    test updates again

--- src/Components/Panel/PanelMeetingAgenda/PanelMeetingAgenda.test.jsx
+++ src/Components/Panel/PanelMeetingAgenda/PanelMeetingAgenda.test.jsx
@@ -1,20 +1,9 @@
 import { shallow } from 'enzyme';
-import toJSON from 'enzyme-to-json';
 import PanelMeetingAgenda from './PanelMeetingAgenda';
 
 describe('PanelMeetingAgendaComponent', () => {
   it('is defined', () => {
     const wrapper = shallow(<PanelMeetingAgenda />);
     expect(wrapper).toBeDefined();
-  });
-
-  it('matches snapshot when CDO', () => {
-    const wrapper = shallow(<PanelMeetingAgenda isCDO />);
-    expect(toJSON(wrapper)).toMatchSnapshot();
-  });
-
-  it('matches snapshot when not CDO', () => {
-    const wrapper = shallow(<PanelMeetingAgenda isCDO={false} />);
-    expect(toJSON(wrapper)).toMatchSnapshot();
   });
 });
