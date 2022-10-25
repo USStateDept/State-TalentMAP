90d5baefa157c6d18b94d48a43454cd9550bc505
pveland621 <31417027+pveland621@users.noreply.github.com>
2022-10-25 15:51:55 -0400

    snapshots

--- src/Components/Panel/PanelMeetingAgenda/PanelMeetingAgenda.test.jsx
+++ src/Components/Panel/PanelMeetingAgenda/PanelMeetingAgenda.test.jsx
@@ -1,15 +1,46 @@
+import TestUtils from 'react-dom/test-utils';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import PanelMeetingAgenda from './PanelMeetingAgenda';
 
-describe('PanelMeetingSearchRow', () => {
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+describe('PanelMeetingAgenda', () => {
   it('is defined', () => {
-    const wrapper = shallow(<PanelMeetingAgenda />);
+    const wrapper = TestUtils.renderIntoDocument(
+      <Provider store={mockStore({})}>
+        <MemoryRouter>
+          <PanelMeetingAgenda isCDO />
+        </MemoryRouter>
+      </Provider>,
+    );
     expect(wrapper).toBeDefined();
   });
 
-  it('matches snapshot', () => {
-    const wrapper = shallow(<PanelMeetingAgenda />);
+  it('matches snapshot when CDO', () => {
+    const wrapper = shallow(
+      <Provider store={mockStore({})}>
+        <MemoryRouter>
+          <PanelMeetingAgenda isCDO />
+        </MemoryRouter>
+      </Provider>,
+    );
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  it('matches snapshot when not CDO', () => {
+    const wrapper = shallow(
+      <Provider store={mockStore({})}>
+        <MemoryRouter>
+          <PanelMeetingAgenda isCDO={false} />
+        </MemoryRouter>
+      </Provider>,
+    );
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
 });
