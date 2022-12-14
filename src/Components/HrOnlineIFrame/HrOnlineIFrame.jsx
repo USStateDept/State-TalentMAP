f6a32c0806f8b004bd32984e968be97a84a09478
Scott Burdick <scott.m.burdick@gmail.com>
2022-12-14 13:50:56 -0500

    Update proptypes

--- src/Components/HrOnlineIFrame/HrOnlineIFrame.jsx
+++ src/Components/HrOnlineIFrame/HrOnlineIFrame.jsx
@@ -1,8 +1,14 @@
-const HrOnlineIFrame = env => {
+import PropTypes from 'prop-types';
+
+const HrOnlineIFrame = ({ env }) => {
   const url = `https://gtm${env}hronline-usdos.msappproxy.net/${env}/hrdata/handshakechild.html`;
   return (
     <iFrame src={url} loading="eager" hidden />
   );
+};
+
+HrOnlineIFrame.PropTypes = {
+  env: PropTypes.string.isRequired,
 };
 
 export default HrOnlineIFrame;
