7cd7809e272e82f7ec7066dcb4bccf4a27ac4447
pveland621 <31417027+pveland621@users.noreply.github.com>
2022-12-14 12:02:32 -0500

    card view set, initial design for row view pending feedback from team

--- src/Components/Agenda/EmployeeAgendaSearchCard/EmployeeAgendaSearchCard.jsx
+++ src/Components/Agenda/EmployeeAgendaSearchCard/EmployeeAgendaSearchCard.jsx
@@ -115,19 +115,14 @@ const EmployeeAgendaSearchCard = ({ isCDO, result, showCreate, viewType, showEdi
           <dt>Agenda Status:</dt>
           <dd className="employee-card-status">
             {agendaStatus}
-            {
-              // showEdit &&
-              // <Link to={`/profile/${userRole}/createagendaitem/${perdet}`}>
-              //   <FA name="pencil" />
-              // </Link>
-            }
           </dd>
         </div>
-        {/* <div className="employee-card-data-point"> */}
-        <div className="edit-button">
-          <FA name="pencil" />
-        </div>
-        {/* </div> */}
+        {
+          showEdit &&
+          <div className="edit-button">
+            <FA name="pencil" />
+          </div>
+        }
       </div>
       <div className="employee-agenda-card-bottom">
         <div className="button-container">
--- src/Components/Agenda/EmployeeAgendaSearchRow/EmployeeAgendaSearchRow.jsx
+++ src/Components/Agenda/EmployeeAgendaSearchRow/EmployeeAgendaSearchRow.jsx
@@ -107,15 +107,17 @@ const EmployeeAgendaSearchRow = ({ isCDO, result, showCreate, viewType, showEdit
             <FA name="sticky-note-o" />
             <dt>Agenda Status:</dt>
             <dd>{agendaStatus}</dd>
-            {
-              showEdit &&
-              <Link to={`/profile/${userRole}/createagendaitem/${perdet}`}>
-                <FA name="pencil" />
-              </Link>
-            }
           </div>
         </div>
         <div className="button-container">
+          {
+            showEdit &&
+            <div className="row-edit-button">
+              <Link to={`/profile/${userRole}/createagendaitem/${perdet}`}>
+                <FA name="pencil" />
+              </Link>
+            </div>
+          }
           <div className="view-agenda-item-container">
             <LinkButton className="view-agenda-item-button" toLink={`/profile/${userRole}/agendaitemhistory/${perdet}`}>View History</LinkButton>
           </div>
