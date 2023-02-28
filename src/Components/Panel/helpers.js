af82fedffb8a88223149afa20147d426dc7e61c5
Scott Burdick <scott.m.burdick@gmail.com>
2023-02-28 09:32:21 -0500

    Clean up format function

--- src/Components/Panel/helpers.js
+++ src/Components/Panel/helpers.js
@@ -1,4 +1,4 @@
-import { findLastIndex, get, includes } from 'lodash';
+import { findLastIndex, get } from 'lodash';
 import { formatDate } from 'utilities';
 import { isPast } from 'date-fns-v2';
 
@@ -13,46 +13,36 @@ export const formatPanelMeetingTrackerData = (meetingDates = []) => {
     const meetingDate = formatDate(get(pmd, 'pmd_dttm'), 'MM/DD/YYYY HH:mm') || '';
     const code = get(pmd, 'mdt_code');
     const isPast$ = isPast(new Date(pmd.pmd_dttm));
-    if (includes(['CUT', 'OFF'], code)) {
-      const { description = '' } = pre;
-      if ((code === 'OFF') && isPast$) {
-        pre.description = meetingDate;
-        pre.isActive = isPast$;
-      } else if (!description && (code === 'CUT' && isPast$)) {
-        pre.description = meetingDate;
-        pre.isActive = isPast$;
-      } else if (!description && (code === 'CUT')) {
-        pre.description = meetingDate;
-        pre.isActive = isPast$;
-      }
-    } else if (includes(['ADD', 'OFFA'], code)) {
-      const { description = '' } = add;
-      if ((code === 'OFFA') && isPast$) {
-        add.description = meetingDate;
-        add.isActive = isPast$;
-      } else if (!description && (code === 'ADD' && isPast$)) {
-        add.description = meetingDate;
-        add.isActive = isPast$;
-      } else if (!description && (code === 'ADD')) {
-        add.description = meetingDate;
-        add.isActive = isPast$;
-      }
-    } else if (code === 'MEET') {
+
+    if ((code === 'OFF') && isPast$) {
+      pre.description = meetingDate;
+      pre.isActive = isPast$;
+    }
+    if (code === 'CUT') {
+      pre.description ??= meetingDate;
+      pre.isActive ??= isPast$;
+    }
+    if ((code === 'OFFA') && isPast$) {
+      add.description = meetingDate;
+      add.isActive = isPast$;
+    }
+    if (code === 'ADD') {
+      add.description ??= meetingDate;
+      add.isActive ??= isPast$;
+    }
+    if (code === 'MEET') {
       panel.description = meetingDate;
       panel.isActive = isPast$;
-    } else if (includes(['POSS', 'POST'], code)) {
-      const { description = '' } = post;
-      if ((code === 'POST') && isPast$) {
-        post.description = meetingDate;
-        post.isActive = isPast$;
-      } else if (!description && (code === 'POSS' && isPast)) {
-        post.description = meetingDate;
-        post.isActive = isPast$;
-      } else if (!description && (code === 'POSS')) {
-        post.description = meetingDate;
-        post.isActive = isPast$;
-      }
-    } else if (code === 'COMP') {
+    }
+    if ((code === 'POST') && isPast$) {
+      post.description = meetingDate;
+      post.isActive = isPast$;
+    }
+    if (code === 'POSS') {
+      post.description ??= meetingDate;
+      post.isActive ??= isPast$;
+    }
+    if (code === 'COMP') {
       complete.description = meetingDate;
       complete.isActive = isPast$;
     }
