c2d5e05e6b5467a8c16a7d51890f8dbd16ee1172
elizabeth <elizabeth.jimenez.cs@gmail.com>
2023-02-28 14:33:05 -0500

    test and snap

--- src/Components/Panel/PanelMeetingTracker/PanelMeetingTracker.test.jsx
+++ src/Components/Panel/PanelMeetingTracker/PanelMeetingTracker.test.jsx
@@ -1,6 +1,6 @@
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
-import PanelMeetingSearchRow from './PanelMeetingTracker';
+import PanelMeetingTracker from './PanelMeetingTracker';
 
 describe('PanelMeetingTracker', () => {
   it('is defined', () => {
