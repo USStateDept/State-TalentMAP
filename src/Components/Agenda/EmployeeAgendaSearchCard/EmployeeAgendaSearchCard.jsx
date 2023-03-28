957aa17c828b7036a5828d16cae83a81d8061f2e
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2023-03-28 17:18:44 -0400

    Update EmployeeAgendaSearchCard.jsx

--- src/Components/Agenda/EmployeeAgendaSearchCard/EmployeeAgendaSearchCard.jsx
+++ src/Components/Agenda/EmployeeAgendaSearchCard/EmployeeAgendaSearchCard.jsx
@@ -135,7 +135,7 @@ const EmployeeAgendaSearchCard = ({ isCDO, result, showCreate, viewType }) => {
             (showAgendaItemMaintenance && agendaIDExist) ?
               <dd>
                 <Link to={`/profile/${userRole}/createagendaitem/${perdet}/${agendaID}`} className="agenda-edit-button">
-                  {agendaStatus || <FA name="sticky-note" />}
+                  {agendaStatus || "No Status" />}
                 </Link>
               </dd>
               :
