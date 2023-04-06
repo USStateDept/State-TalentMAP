4b717e6aeee9a9a8255162d7baafcd78112c305d
pveland621 <31417027+pveland621@users.noreply.github.com>
2023-04-06 17:15:19 -0400

    restyling, getting rid of display: grid

--- src/Components/AdministratorPage/EditRemark/EditRemark.jsx
+++ src/Components/AdministratorPage/EditRemark/EditRemark.jsx
@@ -12,31 +12,31 @@ const EditRemark = (props) => {
 
   return (
     <div className="edit-remark-modal">
-      <div className="help-text whole-row">
+      <div className="help-text">
         <span>* indicates a required field</span>
       </div>
-      <div className="edit-remark-input whole-row">
+      <div className="edit-remark-input">
         <label htmlFor="edit-remark-description">*Remark Description:</label>
         <input
           id="edit-remark-description"
           placeholder="Enter Remark Description"
         />
       </div>
-      <div className="edit-remark-input whole-row">
+      <div className="edit-remark-input">
         <label htmlFor="edit-remark-short-description">Remark Short Description:</label>
         <input
           id="edit-remark-short-description"
           placeholder="Enter Remark Short Description"
         />
       </div>
-      <div className="edit-remark-input whole-row">
+      <div className="edit-remark-input">
         <label htmlFor="edit-remark-insertion">Remark Insertion Text:</label>
         <input
           id="edit-remark-insertion"
           placeholder="Enter Remark Insertion Text"
         />
       </div>
-      <div className="edit-remark-input whole-row">
+      <div className="edit-remark-input">
         <label htmlFor="edit-remark-categories">*Remark Category:</label>
         <select id="edit-remark-categories">
           {
@@ -48,13 +48,15 @@ const EditRemark = (props) => {
           }
         </select>
       </div>
-      <div className="edit-remark-checkboxes">
-        <CheckBox label="Active Indicator" />
-        <CheckBox label="Mutually Exclusive Indicator" />
-      </div>
-      <div className="modal-control-buttons">
-        <button>Submit</button>
-        <button className="usa-button-secondary" onClick={closeRemarkModal}>Cancel</button>
+      <div className="edit-remark-checkboxes-controls">
+        <div className="edit-remark-checkboxes">
+          <CheckBox label="Active Indicator" />
+          <CheckBox label="Mutually Exclusive Indicator" />
+        </div>
+        <div className="modal-controls">
+          <button>Submit</button>
+          <button className="usa-button-secondary" onClick={closeRemarkModal}>Cancel</button>
+        </div>
       </div>
     </div>
   );
