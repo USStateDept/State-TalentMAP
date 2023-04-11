785298eab151d716dbf2e4075c9ba364b117598e
pveland621 <31417027+pveland621@users.noreply.github.com>
2023-04-11 14:37:15 -0400

    new remark insertions design

--- src/Components/AdministratorPage/EditRemark/EditRemark.jsx
+++ src/Components/AdministratorPage/EditRemark/EditRemark.jsx
@@ -1,40 +1,67 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
+import FA from 'react-fontawesome';
+import { isNull } from 'lodash';
 import swal from '@sweetalert/with-react';
+import InteractiveElement from 'Components/InteractiveElement';
 import CheckBox from 'Components/CheckBox';
 
 const EditRemark = (props) => {
   const { rmrkCategories } = props;
+
+  const [rmrkInsertionList, setRmrkInsertionList] = useState([]);
+
+  const [descriptionInput, setDescriptionInput] = useState('');
+  const [insertionInput, setInsertionInput] = useState('');
+
+  const [showInsertionInput, setShowInsertionInput] = useState(false);
+
 
   const closeRemarkModal = (e) => {
     e.preventDefault();
     swal.close();
   };
 
+  const onRemoveInsertionClick = (i) => {
+    const returnArray = [...rmrkInsertionList];
+    returnArray.splice(i, 1);
+    setRmrkInsertionList(returnArray);
+  };
+
+  const submitInsertion = () => {
+    setShowInsertionInput(false);
+    setRmrkInsertionList([...rmrkInsertionList, `{${insertionInput}}`]);
+    setDescriptionInput(`${descriptionInput}{${insertionInput}}`);
+    setInsertionInput('');
+  };
+
+  const onInputKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      submitInsertion();
+    } else if (e.key === 'Escape') {
+      setShowInsertionInput(false);
+      setInsertionInput('');
+    }
+  };
+
+  const updateInsertionInput = (e) => {
+    const value = e.target.value;
+    if (!isNull(value)) {
+      setInsertionInput(value);
+    }
+  };
+
+  const updateDescriptionInput = (e) => {
+    const value = e.target.value;
+    if (!isNull(value)) {
+      setDescriptionInput(value);
+    }
+  };
+
   return (
     <div className="edit-remark-modal">
       <div className="help-text">
         <span>* indicates a required field</span>
-      </div>
-      <div className="edit-remark-input">
-        <label htmlFor="edit-remark-description">*Remark Description:</label>
-        <input
-          id="edit-remark-description"
-          placeholder="Enter Remark Description"
-        />
-      </div>
-      <div className="edit-remark-input">
-        <label htmlFor="edit-remark-short-description">Remark Short Description:</label>
-        <input
-          id="edit-remark-short-description"
-          placeholder="Enter Remark Short Description"
-        />
-      </div>
-      <div className="edit-remark-input">
-        <label htmlFor="edit-remark-insertion">Remark Insertion Text:</label>
-        <input
-          id="edit-remark-insertion"
-          placeholder="Enter Remark Insertion Text"
-        />
       </div>
       <div className="edit-remark-input">
         <label htmlFor="edit-remark-categories">*Remark Category:</label>
@@ -47,6 +74,72 @@ const EditRemark = (props) => {
             ))
           }
         </select>
+      </div>
+      <div className="edit-remark-input">
+        <label htmlFor="edit-remark-description">*Remark Description:</label>
+        <input
+          id="edit-remark-description"
+          placeholder="Enter Remark Description"
+          onChange={updateDescriptionInput}
+          value={descriptionInput}
+        />
+      </div>
+      <div className="edit-remark-input">
+        <label htmlFor="add-insertion-container" />
+        <div className="add-insertion-container">
+          <button
+            id="add-insertion-button"
+            onClick={() => setShowInsertionInput(true)}
+            className="add-insertion-button"
+          >
+            Add Remark Insertion
+          </button>
+          {showInsertionInput &&
+            <div className="add-insertion-input-container">
+              <input
+                placeholder="Enter Remark Insertion"
+                onKeyDown={onInputKeyDown}
+                onChange={updateInsertionInput}
+              />
+              <InteractiveElement
+                onClick={submitInsertion}
+                className="insertion-icon"
+                type="span"
+                role="button"
+                title="Add Insertion"
+                id="add-insertion"
+              >
+                <FA name="plus" />
+              </InteractiveElement>
+            </div>
+          }
+        </div>
+      </div>
+      <div className="edit-remark-input">
+        <label htmlFor="edit-remark-short-description">Remark Short Description:</label>
+        <input
+          id="edit-remark-short-description"
+          placeholder="Enter Remark Short Description"
+        />
+      </div>
+      <div className="edit-remark-input">
+        <label htmlFor="edit-remark-insertion">Remark Insertions:</label>
+        <div id="edit-remark-insertion" className="remark-insertion-list">
+          {rmrkInsertionList?.map((insertion, i) => (
+            <div className="remark-insertion">
+              {insertion}
+              <InteractiveElement
+                className="insertion-icon"
+                type="span"
+                role="button"
+                title="Remove Insertion"
+                onClick={() => onRemoveInsertionClick(i)}
+              >
+                <FA name="minus" />
+              </InteractiveElement>
+            </div>
+          ))}
+        </div>
       </div>
       <div className="edit-remark-checkboxes-controls">
         <div className="edit-remark-checkboxes">
