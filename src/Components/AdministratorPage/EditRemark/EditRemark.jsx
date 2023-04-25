d0527564fa81af44c4c881135c9fe641c1aa52e5
pveland621 <31417027+pveland621@users.noreply.github.com>
2023-04-25 16:42:22 -0400

    spacing

--- src/Components/AdministratorPage/EditRemark/EditRemark.jsx
+++ src/Components/AdministratorPage/EditRemark/EditRemark.jsx
@@ -26,7 +26,7 @@ const EditRemark = (props) => {
     const returnArray = [...rmrkInsertionList];
     returnArray.splice(i, 1);
     setRmrkInsertionList(returnArray);
-    
+
     const regex = new RegExp('{[^}]*}', 'g');
     let x = 0;
     // eslint-disable-next-line no-plusplus
