e586f521384848f8c2d883bb1302bf4523730043
elizabeth <elizabeth.jimenez.cs@gmail.com>
2023-05-02 13:04:52 -0400

    removing static content

--- src/Components/BidderPortfolio/TopNav/Actions/ActionsButton/ActionsButton.jsx
+++
@@ -1,9 +0,0 @@
-import StaticDevContent from '../../../../StaticDevContent';
-
-const ActionsButton = () => (
-  <StaticDevContent>
-    <button className="tm-button-alt">Actions...</button>
-  </StaticDevContent>
-);
-
-export default ActionsButton;
