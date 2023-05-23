137aa7f1b71135339c5b8240f499891da4d49704
pveland621 <31417027+pveland621@users.noreply.github.com>
2023-05-23 15:28:43 -0400

    snapshots

--- src/Components/Panel/PanelMeetingSearchRow/PanelMeetingSearchRow.test.jsx
+++ src/Components/Panel/PanelMeetingSearchRow/PanelMeetingSearchRow.test.jsx
@@ -1,15 +1,22 @@
-import { shallow } from 'enzyme';
-import toJSON from 'enzyme-to-json';
+import TestUtils from 'react-dom/test-utils';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { BrowserRouter as Router } from 'react-router-dom';
 import PanelMeetingSearchRow from './PanelMeetingSearchRow';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
 
 describe('PanelMeetingSearchRow', () => {
   it('is defined', () => {
-    const wrapper = shallow(<PanelMeetingSearchRow />);
+    const wrapper = TestUtils.renderIntoDocument(
+      <Provider store={mockStore({})}>
+        <Router>
+          <PanelMeetingSearchRow />
+        </Router>
+      </Provider>,
+    );
     expect(wrapper).toBeDefined();
-  });
-
-  it('matches snapshot', () => {
-    const wrapper = shallow(<PanelMeetingSearchRow />);
-    expect(toJSON(wrapper)).toMatchSnapshot();
   });
 });
