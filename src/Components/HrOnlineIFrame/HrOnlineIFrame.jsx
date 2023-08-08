63da84bd063529e441746f36a0a3f82cfd7b9f1d
elizabeth <elizabeth.jimenez.cs@gmail.com>
2023-08-08 09:20:05 -0400

    useLayoutEffect

--- src/Components/HrOnlineIFrame/HrOnlineIFrame.jsx
+++ src/Components/HrOnlineIFrame/HrOnlineIFrame.jsx
@@ -1,14 +1,14 @@
 import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
+import { useLayoutEffect, useState } from 'react';
 
 const HrOnlineIFrame = ({ env }) => {
   const domain = env.toLowerCase() === 'prd' ? '' : env;
   const [url, setUrl] = useState(`https://gtm${domain}hronline-usdos.msappproxy.net/${env}/HRData/api/v1/getuser`);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setUrl(`https://gtm${domain}hronline-usdos.msappproxy.net/${env}/hrdata/handshakechild.html`);
-    }, 1000);
+  useLayoutEffect(() => {
+      setTimeout(() => {
+        setUrl(`https://gtm${domain}hronline-usdos.msappproxy.net/${env}/hrdata/handshakechild.html`);
+      }, 2000);
   }, []);
 
   return (
