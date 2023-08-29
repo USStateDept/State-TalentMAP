9ebc2b13b2788e11e2555f41015a2f373e99e0d0
musiqe31 <agbakwucharles@gmail.com>
2023-08-29 08:55:05 -0500

    reformat code

--- src/Components/BidderPortfolio/BidderPortfolioStatRow/BidderPortfolioStatRow.jsx
+++ src/Components/BidderPortfolio/BidderPortfolioStatRow/BidderPortfolioStatRow.jsx
@@ -1,51 +1,43 @@
-import { useState } from "react";
-import { get } from "lodash";
-import PropTypes from "prop-types";
-import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import { Cusp, Eligible } from "Components/Ribbon";
-import {
-  NO_GRADE,
-  NO_LANGUAGE,
-  NO_POST,
-  NO_TOUR_END_DATE,
-} from "Constants/SystemMessages";
-import { formatDate } from "utilities";
-import FA from "react-fontawesome";
-import TextareaAutosize from "react-textarea-autosize";
-import { toastSuccess } from "actions/toast";
-import ToggleButton from "Components/ToggleButton";
-import InteractiveElement from "Components/InteractiveElement";
-import { BIDDER_OBJECT, CLASSIFICATIONS } from "../../../Constants/PropTypes";
-import SkillCodeList from "../../SkillCodeList";
-import ClientBadgeList from "../ClientBadgeList";
-import CheckboxList from "../CheckboxList";
-import SearchAsClientButton from "../SearchAsClientButton";
-import AddToInternalListButton from "../AddToInternalListButton";
+import { useState } from 'react';
+import { get } from 'lodash';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Cusp, Eligible } from 'Components/Ribbon';
+import { NO_GRADE, NO_LANGUAGE, NO_POST, NO_TOUR_END_DATE } from 'Constants/SystemMessages';
+import { formatDate } from 'utilities';
+import FA from 'react-fontawesome';
+import TextareaAutosize from 'react-textarea-autosize';
+import { toastSuccess } from 'actions/toast';
+import ToggleButton from 'Components/ToggleButton';
+import InteractiveElement from 'Components/InteractiveElement';
+import { BIDDER_OBJECT, CLASSIFICATIONS } from '../../../Constants/PropTypes';
+import SkillCodeList from '../../SkillCodeList';
+import ClientBadgeList from '../ClientBadgeList';
+import CheckboxList from '../CheckboxList';
+import SearchAsClientButton from '../SearchAsClientButton';
+import AddToInternalListButton from '../AddToInternalListButton';
 
 const BidderPortfolioStatRow = ({ userProfile, showEdit, classifications }) => {
   const dispatch = useDispatch();
-  const currentAssignmentText = get(userProfile, "pos_location");
-  const clientClassifications = get(userProfile, "classifications");
-  const perdet = get(userProfile, "perdet_seq_number");
-  const id = get(userProfile, "employee_id");
-  const ted = formatDate(get(userProfile, "current_assignment.end_date"));
-  const languages = get(userProfile, "current_assignment.position.language");
-  const bidder = get(userProfile, "shortened_name") || "None listed";
-  const orgShortDesc = get(
-    userProfile,
-    "current_assignment.position.organization"
-  );
-  const email = get(userProfile, "cdos")[0]?.cdo_email || "None listed";
+  const currentAssignmentText = get(userProfile, 'pos_location');
+  const clientClassifications = get(userProfile, 'classifications');
+  const perdet = get(userProfile, 'perdet_seq_number');
+  const id = get(userProfile, 'employee_id');
+  const ted = formatDate(get(userProfile, 'current_assignment.end_date'));
+  const languages = get(userProfile, 'current_assignment.position.language');
+  const bidder = get(userProfile, 'shortened_name') || 'None listed';
+  const orgShortDesc = get(userProfile, 'current_assignment.position.organization');
+  const email = get(userProfile, 'cdos')[0]?.cdo_email || 'None listed';
   const [edit, setEdit] = useState(false);
   const [showMore, setShowMore] = useState(false);
   const [included, setIncluded] = useState(true);
-  const [comments, setComments] = useState("");
-  const [altEmail, setAltEmail] = useState("");
+  const [comments, setComments] = useState('');
+  const [altEmail, setAltEmail] = useState('');
 
   const editClient = (e) => {
     e.preventDefault();
-    setEdit((previous) => !previous);
+    setEdit(previous => !previous);
   };
 
   const saveEdit = () => {
@@ -67,13 +59,13 @@ const BidderPortfolioStatRow = ({ userProfile, showEdit, classifications }) => {
       <div className="stat-card-header">
         <h3 className="stat-card-client">Client Overview</h3>
         <ToggleButton
-          labelTextRight={!included ? "Excluded" : "Included"}
+          labelTextRight={!included ? 'Excluded' : 'Included'}
           checked={included}
           onChange={() => setIncluded(!included)}
           onColor="#0071BC"
         />
       </div>
-      {showMore && (
+      { showMore &&
         <div className="stat-card-data-point stat-card-data-point--name stat-card-data-space">
           <Link to={`/profile/public/${perdet}`}>{bidder}</Link>
           <Link to="#" onClick={(e) => editClient(e)}>
@@ -81,42 +73,35 @@ const BidderPortfolioStatRow = ({ userProfile, showEdit, classifications }) => {
             Edit
           </Link>
         </div>
-      )}
+      }
       <div className="bidder-portfolio-ribbon-container">
-        <div className="ribbon-container-condensed">{ribbon()}</div>
+        <div className="ribbon-container-condensed">
+          {ribbon()}
+        </div>
       </div>
       <div>
         <div>
           <div className="stat-card-data-point">
-            <dt>Employee ID:</dt>
-            <dd>{id}</dd>
+            <dt>Employee ID:</dt><dd>{id}</dd>
           </div>
           <div className="stat-card-data-point">
-            <dt>Skill:</dt>
-            <dd>
-              <SkillCodeList skillCodes={userProfile.skills} />
-            </dd>
+            <dt>Skill:</dt><dd><SkillCodeList skillCodes={userProfile.skills} /></dd>
           </div>
           <div className="stat-card-data-point">
-            <dt>Grade:</dt>
-            <dd>{userProfile.grade || NO_GRADE}</dd>
+            <dt>Grade:</dt><dd>{userProfile.grade || NO_GRADE}</dd>
           </div>
           <div className="stat-card-data-point">
-            <dt>Languages:</dt>
-            <dd>{languages || NO_LANGUAGE}</dd>
+            <dt>Languages:</dt><dd>{languages || NO_LANGUAGE}</dd>
           </div>
           <div className="stat-card-data-point">
-            <dt>TED:</dt>
-            <dd>{ted || NO_TOUR_END_DATE}</dd>
+            <dt>TED:</dt><dd>{ted || NO_TOUR_END_DATE}</dd>
           </div>
           <div className="stat-card-data-point">
-            <dt>Location (Org):</dt>
-            <dd>
-              {currentAssignmentText || NO_POST} ({orgShortDesc})
-            </dd>
+            <dt>Location (Org):</dt><dd>{currentAssignmentText || NO_POST} ({orgShortDesc})</dd>
           </div>
         </div>
-        {!showEdit && (
+        {
+          !showEdit &&
           <div className="bidder-portfolio-stat-row-updates">
             <h4>Classifications: </h4>
             <ClientBadgeList
@@ -124,16 +109,20 @@ const BidderPortfolioStatRow = ({ userProfile, showEdit, classifications }) => {
               classifications={classifications}
             />
           </div>
-        )}
-        {!showEdit && (
+        }
+        {
+          !showEdit &&
           <div className="button-container">
             <SearchAsClientButton user={userProfile} />
             <AddToInternalListButton refKey={perdet} />
           </div>
-        )}
-        {showEdit && <CheckboxList id={userProfile.id} />}
+        }
+        {
+          showEdit &&
+          <CheckboxList id={userProfile.id} />
+        }
       </div>
-      {showMore && (
+      { showMore &&
         <div>
           <div className="stat-card-data-point stat-card-comments">
             <dt>Comments:</dt>
@@ -144,18 +133,15 @@ const BidderPortfolioStatRow = ({ userProfile, showEdit, classifications }) => {
                 maxLength="255"
                 name="note"
                 placeholder="No Notes"
-                defaultValue={comments === "" ? "" : comments}
+                defaultValue={comments === '' ? '' : comments}
                 onChange={(e) => setComments(e.target.value)}
               />
             </dd>
           </div>
           <div className="stat-card-data-point">
-            <dt>DOS Email:</dt>
-            <dd>
-              <a href="mailto: example@gmail.com">{email}</a>
-            </dd>
+            <dt>DOS Email:</dt><dd><a href="mailto: example@gmail.com">{email}</a></dd>
           </div>
-          <div className={!edit && "stat-card-data-point"}>
+          <div className={!edit && 'stat-card-data-point'} >
             <dt>Alt Email:</dt>
             {edit ? (
               <input
@@ -169,26 +155,16 @@ const BidderPortfolioStatRow = ({ userProfile, showEdit, classifications }) => {
             )}
           </div>
         </div>
-      )}
-      {showSaveAndCancel && (
+      }
+      { showSaveAndCancel &&
         <div className="stat-card-btn-container">
-          <button
-            className="stat-card-cancel-btn"
-            onClick={() => setEdit(false)}
-          >
-            Cancel
-          </button>
-          <button onClick={saveEdit} disabled={!comments && !altEmail}>
-            Save
-          </button>
+          <button className="stat-card-cancel-btn" onClick={() => setEdit(false)}>Cancel</button>
+          <button onClick={saveEdit} disabled={!comments && !altEmail}>Save</button>
         </div>
-      )}
+      }
       <div className="toggle-more-container">
-        <InteractiveElement
-          className="toggle-more"
-          onClick={() => setShowMore(!showMore)}
-        >
-          <FA name={`chevron-${showMore ? "up" : "down"}`} />
+        <InteractiveElement className="toggle-more" onClick={() => setShowMore(!showMore)}>
+          <FA name={`chevron-${showMore ? 'up' : 'down'}`} />
         </InteractiveElement>
       </div>
     </div>
