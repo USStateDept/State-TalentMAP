6cd223f072f5b6bedc48328ba13d3f39301c27d4
Scott Burdick <scott.m.burdick@gmail.com>
2023-09-01 10:47:01 -0400

    Update mapping for org

--- src/Components/BureauPage/SearchPostAccess/SearchPostAccess.jsx
+++ src/Components/BureauPage/SearchPostAccess/SearchPostAccess.jsx
@@ -52,7 +52,7 @@ const SearchPostAccess = () => {
 
   const getQuery = () => ({
     bureaus: selectedBureaus.map(bureau => (bureau?.code)),
-    org: selectedOrgs.map(org => (org?.code)),
+    org: selectedOrgs.map(org => (org?.description)),
     locations: selectedLocations.map(loc => (loc?.code)),
     roles: selectedRoles.map(role => (role?.code)),
     persons: selectedPersons.map(person => (person?.code)),
@@ -313,13 +313,13 @@ const SearchPostAccess = () => {
                             onCheckBoxClick={() => handleSelectPost(post)}
                           />
                         </td>
-                        <td>{post?.access_type || '---'}</td>
-                        <td>{post?.bureau || '---'}</td>
-                        <td>{post?.post || '---'}</td>
-                        <td>{post?.employee || '---'}</td>
-                        <td>{post?.role || '---'}</td>
-                        <td>{post?.position || '---'}</td>
-                        <td>{post?.title || '---'}</td>
+                        <td>{post?.access_type}</td>
+                        <td>{post?.bureau}</td>
+                        <td>{post?.post}</td>
+                        <td>{post?.employee}</td>
+                        <td>{post?.role}</td>
+                        <td>{post?.position}</td>
+                        <td>{post?.title}</td>
                       </tr>
                     ))
                   }
