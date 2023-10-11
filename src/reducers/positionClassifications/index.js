ba04d76efa01d5d9ac2c45cb621d99337162f6fe
Jeanie Lam <jlam@metaphaseconsulting.com>
2023-10-11 02:05:25 -0400

    Updated position classification reducers

--- src/reducers/positionClassifications/index.js
+++ src/reducers/positionClassifications/index.js
@@ -1,11 +1,17 @@
 import {
   positionClassifications,
+  positionClassificationsEdit,
+  positionClassificationsEditHasErrored,
+  positionClassificationsEditIsLoading,
   positionClassificationsHasErrored,
   positionClassificationsIsLoading,
 } from './positionClassifications';
 
 export default {
+  positionClassifications,
+  positionClassificationsEdit,
+  positionClassificationsEditHasErrored,
+  positionClassificationsEditIsLoading,
   positionClassificationsHasErrored,
   positionClassificationsIsLoading,
-  positionClassifications,
 };
