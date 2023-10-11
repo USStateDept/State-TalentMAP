e20108317be46375f0dc4176ea09a59cffc95fb7
Jeanie Lam <jlam@metaphaseconsulting.com>
2023-10-11 02:04:59 -0400

    Updated position classification actions with real endpoints

--- src/actions/positionClassifications.js
+++ src/actions/positionClassifications.js
@@ -1,43 +1,47 @@
 import { batch } from 'react-redux';
-import { uniqBy } from 'lodash';
+import {
+  UPDATE_POSITION_CLASSIFICATION_ERROR,
+  UPDATE_POSITION_CLASSIFICATION_ERROR_TITLE,
+  UPDATE_POSITION_CLASSIFICATION_SUCCESS,
+  UPDATE_POSITION_CLASSIFICATION_SUCCESS_TITLE,
+} from '../Constants/SystemMessages';
 import api from '../api';
+import { toastError, toastSuccess } from './toast';
 
 export function positionClassificationsHasErrored(bool) {
   return {
-    type: 'POSITION_CLASSIFICATIONS_HAS_ERRORED',
+    type: 'POSITION_CLASSIFICATIONS_FETCH_DATA_HAS_ERRORED',
     hasErrored: bool,
   };
 }
 
 export function positionClassificationsIsLoading(bool) {
   return {
-    type: 'POSITION_CLASSIFICATIONS_IS_LOADING',
+    type: 'POSITION_CLASSIFICATIONS_FETCH_DATA_IS_LOADING',
     isLoading: bool,
   };
 }
 
-export function positionClassificationsFetchDataSuccess(classifications) {
+export function positionClassificationsFetchDataSuccess(results) {
   return {
     type: 'POSITION_CLASSIFICATIONS_FETCH_DATA_SUCCESS',
-    classifications,
+    results,
   };
 }
 
-export function fetchPositionClassifications() {
+export function positionClassifications(id) {
   return (dispatch) => {
     batch(() => {
       dispatch(positionClassificationsIsLoading(true));
       dispatch(positionClassificationsHasErrored(false));
     });
 
-    api()
-      .get('/fsbid/reference/positionclassifications/')
+    api().get(`/fsbid/position_classifications/${id}/`)
       .then(({ data }) => {
-        const data$ = uniqBy(data, 'code');
         batch(() => {
           dispatch(positionClassificationsHasErrored(false));
           dispatch(positionClassificationsIsLoading(false));
-          dispatch(positionClassificationsFetchDataSuccess(data$));
+          dispatch(positionClassificationsFetchDataSuccess(data));
         });
       })
       .catch(() => {
@@ -45,6 +49,62 @@ export function fetchPositionClassifications() {
           dispatch(positionClassificationsHasErrored(true));
           dispatch(positionClassificationsIsLoading(false));
         });
+      });
+  };
+}
+
+export function positionClassificationsEditErrored(bool) {
+  return {
+    type: 'POSITION_CLASSIFICATIONS_EDIT_HAS_ERRORED',
+    hasErrored: bool,
+  };
+}
+export function positionClassificationsEditLoading(bool) {
+  return {
+    type: 'POSITION_CLASSIFICATIONS_EDIT_IS_LOADING',
+    isLoading: bool,
+  };
+}
+export function positionClassificationsEditSuccess(results) {
+  return {
+    type: 'POSITION_CLASSIFICATIONS_EDIT_SUCCESS',
+    results,
+  };
+}
+export function positionClassificationsEdit(data) {
+  return (dispatch) => {
+    batch(() => {
+      dispatch(positionClassificationsEditLoading(true));
+      dispatch(positionClassificationsEditErrored(false));
+    });
+
+    api().post('/fsbid/position_classifications/edit/', data)
+      .then(() => {
+        const toastTitle = UPDATE_POSITION_CLASSIFICATION_SUCCESS_TITLE;
+        const toastMessage = UPDATE_POSITION_CLASSIFICATION_SUCCESS;
+        batch(() => {
+          dispatch(positionClassificationsEditErrored(false));
+          dispatch(positionClassificationsEditSuccess(true));
+          dispatch(toastSuccess(toastMessage, toastTitle));
+          dispatch(positionClassificationsEditSuccess());
+          dispatch(positionClassificationsEditLoading(false));
+        });
+      })
+      .catch((err) => {
+        if (err?.message === 'cancel') {
+          batch(() => {
+            dispatch(positionClassificationsEditLoading(true));
+            dispatch(positionClassificationsEditErrored(false));
+          });
+        } else {
+          const toastTitle = UPDATE_POSITION_CLASSIFICATION_ERROR_TITLE;
+          const toastMessage = UPDATE_POSITION_CLASSIFICATION_ERROR;
+          batch(() => {
+            dispatch(positionClassificationsEditErrored(true));
+            dispatch(toastError(toastMessage, toastTitle));
+            dispatch(positionClassificationsEditLoading(false));
+          });
+        }
       });
   };
 }
