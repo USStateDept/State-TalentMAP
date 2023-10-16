c99ce4bbdbf614651386dfacab8eca29e8cfa6d6
Jeanie Lam <jlam@metaphaseconsulting.com>
2023-10-16 19:06:54 -0400

    Added panel meeting reducers

--- src/reducers/panelMeetings/panelMeetings.js
+++ src/reducers/panelMeetings/panelMeetings.js
@@ -1,3 +1,5 @@
+// ======================== Panel Meeting List ========================
+
 export function panelMeetingsFetchDataErrored(state = false, action) {
   switch (action.type) {
     case 'PANEL_MEETINGS_FETCH_HAS_ERRORED':
@@ -23,6 +25,8 @@ export function panelMeetings(state = {}, action) {
   }
 }
 
+// ======================== Panel Meeting Selections ========================
+
 export function panelMeetingsSelections(state = {}, action) {
   switch (action.type) {
     case 'PANEL_MEETINGS_SELECTIONS_SAVE_SUCCESS':
@@ -31,6 +35,8 @@ export function panelMeetingsSelections(state = {}, action) {
       return state;
   }
 }
+
+// ======================== Panel Meeting Filters ========================
 
 export function panelMeetingsFiltersFetchDataErrored(state = false, action) {
   switch (action.type) {
@@ -51,6 +57,84 @@ export function panelMeetingsFiltersFetchDataLoading(state = false, action) {
 export function panelMeetingsFilters(state = {}, action) {
   switch (action.type) {
     case 'PANEL_MEETINGS_FILTERS_FETCH_SUCCESS':
+      return action.results;
+    default:
+      return state;
+  }
+}
+
+// ======================== Panel Meeting ========================
+
+export function panelMeetingFetchDataErrored(state = false, action) {
+  switch (action.type) {
+    case 'PANEL_MEETING_FETCH_HAS_ERRORED':
+      return action.hasErrored;
+    default:
+      return state;
+  }
+}
+export function panelMeetingFetchDataLoading(state = false, action) {
+  switch (action.type) {
+    case 'PANEL_MEETING_FETCH_IS_LOADING':
+      return action.isLoading;
+    default:
+      return state;
+  }
+}
+export function panelMeeting(state = {}, action) {
+  switch (action.type) {
+    case 'PANEL_MEETING_FETCH_SUCCESS':
+      return action.results;
+    default:
+      return state;
+  }
+}
+
+// ======================== Panel Meeting Actions ========================
+
+export function runOfficialPreliminaryErrored(state = false, action) {
+  switch (action.type) {
+    case 'RUN_OFFICIAL_PRELIMINARY_ERRORED':
+      return action.hasErrored;
+    default:
+      return state;
+  }
+}
+export function runOfficialPreliminarySuccess(state = {}, action) {
+  switch (action.type) {
+    case 'RUN_OFFICIAL_PRELIMINARY_SUCCESS':
+      return action.results;
+    default:
+      return state;
+  }
+}
+export function runOfficialAddendumErrored(state = false, action) {
+  switch (action.type) {
+    case 'RUN_OFFICIAL_ADDENDUM_ERRORED':
+      return action.hasErrored;
+    default:
+      return state;
+  }
+}
+export function runOfficialAddendumSuccess(state = {}, action) {
+  switch (action.type) {
+    case 'RUN_OFFICIAL_ADDENDUM_SUCCESS':
+      return action.results;
+    default:
+      return state;
+  }
+}
+export function runPostPanelProcessingErrored(state = false, action) {
+  switch (action.type) {
+    case 'RUN_POST_PANEL_PROCESSING_ERRORED':
+      return action.hasErrored;
+    default:
+      return state;
+  }
+}
+export function runPostPanelProcessingSuccess(state = {}, action) {
+  switch (action.type) {
+    case 'RUN_POST_PANEL_PROCESSING_SUCCESS':
       return action.results;
     default:
       return state;
