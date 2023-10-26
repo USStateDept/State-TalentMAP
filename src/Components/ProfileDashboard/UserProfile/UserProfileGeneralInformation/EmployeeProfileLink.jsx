e116349d3a7a5333c16f59cfff9c5e2827a86024
elizabeth <elizabeth.jimenez.cs@gmail.com>
2023-10-26 17:30:32 -0400

    reports called on action

--- src/Components/ProfileDashboard/UserProfile/UserProfileGeneralInformation/EmployeeProfileLink.jsx
+++ src/Components/ProfileDashboard/UserProfile/UserProfileGeneralInformation/EmployeeProfileLink.jsx
@@ -1,63 +1,91 @@
 import swal from '@sweetalert/with-react';
 import PropTypes from 'prop-types';
 import FA from 'react-fontawesome';
+import shortid from 'shortid';
+import { useDispatch } from 'react-redux';
 import { USER_PROFILE } from 'Constants/PropTypes';
 import InteractiveElement from 'Components/InteractiveElement';
-import { downloadPdfStream, isOnProxy } from 'utilities';
-import Alert from '../../../Alert';
+import { downloadPdfStream } from 'utilities';
+import { toastError, toastInfo, toastSuccess } from 'actions/toast';
+import api from '../../../../api';
 import InformationDataPoint from '../../InformationDataPoint';
 import EmployeeProfileModal from './EmployeeProfileModal';
 
 const EmployeeProfileLink = ({ userProfile, showEmployeeProfileLinks }) => {
+  const dispatch = useDispatch();
+  const hruId = userProfile?.user_info?.hru_id;
+
+
   const downloadEmployeeProfile = () => {
-    downloadPdfStream(userProfile.redactedReport.data);
+    const id = shortid.generate();
+    dispatch(toastInfo('Please wait while we process your request.', 'Loading...', id));
+
+    api().get(`/fsbid/employee/${hruId}/employee_profile_report/?redacted_report=true`,
+      {
+        responseType: 'arraybuffer',
+      },
+    ).then(response => {
+      downloadPdfStream(response.data);
+      dispatch(toastSuccess('Employee profile successfully downloaded.', 'Success', id, true));
+    }).catch(() => {
+      dispatch(toastError('We were unable to process your Employee Profile download. Please try again later.', 'An error has occurred', id, true));
+    });
   };
 
-  // not showing on GOBrowser until we fix
-  const displayLinks = !isOnProxy() && showEmployeeProfileLinks;
-  const redactedBlob = new Blob([userProfile?.redactedReport?.data], { type: 'application/pdf' });
-  const unredactedBlob = new Blob([userProfile?.unredactedReport?.data], { type: 'application/pdf' });
-  const bloburl = window.URL.createObjectURL(unredactedBlob);
-
-  const openPdf = () => swal({
+  const openPdf = (url) => swal({
     title: 'Employee Profile Report:',
     button: false,
     className: 'modal-1300',
     content: (
       <EmployeeProfileModal
-        url={bloburl}
+        url={url}
       />
     ),
   });
+
+  const viewEmployeeProfile = () => {
+    const id = shortid.generate();
+
+    dispatch(toastInfo('Please wait while we process your request.', 'Loading...', id));
+    api().get(`/fsbid/employee/${hruId}/employee_profile_report/?redacted_report=false`,
+      {
+        responseType: 'arraybuffer',
+      },
+    ).then(response => {
+      dispatch(toastSuccess('Viewing Unredacted Profile.', 'Success', id, true));
+
+      const unredactedBlob = new Blob([response?.data], { type: 'application/pdf' });
+      const bloburl = window.URL.createObjectURL(unredactedBlob);
+
+      openPdf(bloburl);
+    }).catch(() => {
+      dispatch(toastError('We were unable to process your Employee Profile download. Please try again later.', 'An error has occurred', id, true));
+    });
+  };
 
   return (
     <InformationDataPoint
       content={
         <div>
           {
-            displayLinks && !unredactedBlob?.size && !redactedBlob?.size &&
-            <Alert type="error" title="Error grabbing Profile PDF" messages={[{ body: 'Please try again.' }]} tinyAlert />
-          }
-          {
-            displayLinks && !!unredactedBlob?.size &&
-            <InteractiveElement
-              onClick={openPdf}
-              type="a"
-              title="View Unredacted Employee Profile PDF"
-            >
-              Employee Profile
-            </InteractiveElement>
-          }
-          {
-            displayLinks && !!redactedBlob.size &&
-            <InteractiveElement
-              onClick={downloadEmployeeProfile}
-              type="a"
-              title="Download Redacted Employee Profile PDF"
-              className="ml-10"
-            >
-              <FA name="download" />
-            </InteractiveElement>
+            showEmployeeProfileLinks &&
+            <>
+              <InteractiveElement
+                onClick={viewEmployeeProfile}
+                type="a"
+                title="View Unredacted Employee Profile PDF"
+              >
+                Employee Profile
+              </InteractiveElement>
+              <InteractiveElement
+                onClick={downloadEmployeeProfile}
+                type="a"
+                title="Download Redacted Employee Profile PDF"
+                className="ml-10"
+              >
+                <FA name="download" />
+              </InteractiveElement>
+            </>
           }
         </div>
       }
