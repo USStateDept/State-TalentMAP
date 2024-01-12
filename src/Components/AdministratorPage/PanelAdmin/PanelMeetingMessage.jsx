e476f94f6c1535789a147863486c5d469fe63717
Scott Burdick <scott.m.burdick@gmail.com>
2024-01-12 14:33:05 -0500

    Update to show two link choices

--- src/Components/AdministratorPage/PanelAdmin/PanelMeetingMessage.jsx
+++ src/Components/AdministratorPage/PanelAdmin/PanelMeetingMessage.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const PanelMeetingMessage = ({ id }) => (
-  <span>Panel ({id}) has been successfully added to Panel Meetings. <span><Link to={`/profile/administrator/panel/${id}/`}>Go to panel</Link>.</span></span>
+  <span>Panel ({id}) has been successfully added to Panel Meetings. <span><Link to={`/profile/administrator/panel/${id}/`}>Go to edit panel</Link>, or <Link to={`/profile/ao/panelmeetingagendas/${id}/`}>Go to panel details</Link>.</span></span>
 );
 
 PanelMeetingMessage.propTypes = {
