c158b10f0ccfb9281bae9f50435537128770d332
pveland621 <31417027+pveland621@users.noreply.github.com>
2024-01-31 14:06:45 -0500

    adding prop to differentiate pages

--- src/Components/ResultsPillContainer/ResultsPillContainer.jsx
+++ src/Components/ResultsPillContainer/ResultsPillContainer.jsx
@@ -4,30 +4,40 @@ import InteractiveElement from 'Components/InteractiveElement';
 import { PILL_ITEM_ARRAY } from '../../Constants/PropTypes';
 import PillList from '../PillList/PillList';
 
-const ResultsPillContainer = ({ items, onPillClick }) => {
+const ResultsPillContainer = ({ items, onPillClick, isClientsPage }) => {
   const [showFilters, setShowFilters] = useState(false);
 
   return (
-    <div className={`collapsible-container ${showFilters ? 'collapsible-container-expanded' : ''}`}>
-      <InteractiveElement title="Show Filters" onClick={() => setShowFilters(!showFilters)}>
-        <div className={`collapsible-title ${showFilters ? 'collapsible-title-expanded' : ''}`}>
-          {`Click to ${showFilters ? 'Hide' : 'Show'} Filter Selections`}
+    isClientsPage ?
+      <div className={`collapsible-container ${showFilters ? 'collapsible-container-expanded' : ''}`}>
+        <InteractiveElement title="Show Filters" onClick={() => setShowFilters(!showFilters)}>
+          <div className={`collapsible-title ${showFilters ? 'collapsible-title-expanded' : ''}`}>
+            {`Click to ${showFilters ? 'Hide' : 'Show'} Filter Selections`}
+          </div>
+        </InteractiveElement>
+        <div className={`collapsible-section ${showFilters ? 'showCollapse' : 'hideCollapse'}`}>
+          <div className="label">Your Selections:</div>
+          <PillList
+            items={items}
+            onPillClick={(param, value, remove) => onPillClick(param, value, remove)}
+          />
         </div>
-      </InteractiveElement>
-      <div className={`collapsible-section ${showFilters ? 'showCollapse' : 'hideCollapse'}`}>
-        <div className="label">Your Selections:</div>
+      </div>
+      :
+      <div className="usa-grid-full">
+        <div className="selections-container">Your Selections:</div>
         <PillList
           items={items}
           onPillClick={(param, value, remove) => onPillClick(param, value, remove)}
         />
       </div>
-    </div>
   );
 };
 
 ResultsPillContainer.propTypes = {
   items: PILL_ITEM_ARRAY.isRequired,
   onPillClick: PropTypes.func.isRequired,
+  isClientsPage: PropTypes.bool.isRequired,
 };
 
 ResultsPillContainer.defaultProps = {
