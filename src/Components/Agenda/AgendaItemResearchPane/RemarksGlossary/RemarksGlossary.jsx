8d452b172f0069d8c23ea51f1e5ef8a7106d3f4c
elizabeth <elizabeth.jimenez.cs@gmail.com>
2024-02-21 16:43:48 -0500

    if  back to empty

--- src/Components/Agenda/AgendaItemResearchPane/RemarksGlossary/RemarksGlossary.jsx
+++ src/Components/Agenda/AgendaItemResearchPane/RemarksGlossary/RemarksGlossary.jsx
@@ -85,7 +85,7 @@ const RemarksGlossary = ({ isReadOnly, remarks, remarkCategories,
     const rInserts = r?.remark_inserts || [];
     const someAreEmpty = rInserts.some((a) => {
       const remarkInsertValue = getTextInputValue(a?.rirmrkseqnum, a?.riseqnum);
-      return remarkInsertValue[0] === '{';
+      return (remarkInsertValue[0] === '{' || remarkInsertValue === '');
     });
 
     const selected = find(userSelections, { seq_num: r.seq_num });
