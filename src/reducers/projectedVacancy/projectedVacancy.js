0f4d7c1492a3b591328e71696ba96b7dfc21a773
Jeanie Lam <jlam@metaphaseconsulting.com>
2024-02-21 09:16:24 -0500

    Added new endpoints to retrieve missing card data

--- src/reducers/projectedVacancy/projectedVacancy.js
+++ src/reducers/projectedVacancy/projectedVacancy.js
@@ -63,27 +63,27 @@ export function projectedVacancyFilters(state = {}, action) {
   }
 }
 
-// ================ GET LANGUAGE OFFSET DATA ================
+// ================ GET LANGUAGE OFFSET OPTIONS ================
 
-export function projectedVacancyLanguageOffsetsErrored(state = false, action) {
+export function projectedVacancyLanguageOffsetOptionsErrored(state = false, action) {
   switch (action.type) {
-    case 'PROJECTED_VACANCY_LANGUAGE_OFFSETS_ERRORED':
+    case 'PROJECTED_VACANCY_LANGUAGE_OFFSET_OPTIONS_ERRORED':
       return action.hasErrored;
     default:
       return state;
   }
 }
-export function projectedVacancyLanguageOffsetsLoading(state = false, action) {
+export function projectedVacancyLanguageOffsetOptionsLoading(state = false, action) {
   switch (action.type) {
-    case 'PROJECTED_VACANCY_LANGUAGE_OFFSETS_LOADING':
+    case 'PROJECTED_VACANCY_LANGUAGE_OFFSET_OPTIONS_LOADING':
       return action.isLoading;
     default:
       return state;
   }
 }
-export function projectedVacancyLanguageOffsets(state = {}, action) {
+export function projectedVacancyLanguageOffsetOptions(state = {}, action) {
   switch (action.type) {
-    case 'PROJECTED_VACANCY_LANGUAGE_OFFSETS_SUCCESS':
+    case 'PROJECTED_VACANCY_LANGUAGE_OFFSET_OPTIONS_SUCCESS':
       return action.results;
     default:
       return state;
@@ -165,6 +165,60 @@ export function projectedVacancyEditCapsuleDescLoading(state = false, action) {
 export function projectedVacancyEditCapsuleDesc(state = {}, action) {
   switch (action.type) {
     case 'PROJECTED_VACANCY_EDIT_CAPSULE_DESC_SUCCESS':
+      return action.results;
+    default:
+      return state;
+  }
+}
+
+// ================ GET METADATA ================
+
+export function projectedVacancyMetadataErrored(state = false, action) {
+  switch (action.type) {
+    case 'PROJECTED_VACANCY_METADATA_ERRORED':
+      return action.hasErrored;
+    default:
+      return state;
+  }
+}
+export function projectedVacancyMetadataLoading(state = false, action) {
+  switch (action.type) {
+    case 'PROJECTED_VACANCY_METADATA_LOADING':
+      return action.isLoading;
+    default:
+      return state;
+  }
+}
+export function projectedVacancyMetadata(state = {}, action) {
+  switch (action.type) {
+    case 'PROJECTED_VACANCY_METADATA_SUCCESS':
+      return action.results;
+    default:
+      return state;
+  }
+}
+
+// ================ GET LANGUAGE OFFSETS ================
+
+export function projectedVacancyLanguageOffsetsErrored(state = false, action) {
+  switch (action.type) {
+    case 'PROJECTED_VACANCY_LANGUAGE_OFFSETS_ERRORED':
+      return action.hasErrored;
+    default:
+      return state;
+  }
+}
+export function projectedVacancyLanguageOffsetsLoading(state = false, action) {
+  switch (action.type) {
+    case 'PROJECTED_VACANCY_LANGUAGE_OFFSETS_LOADING':
+      return action.isLoading;
+    default:
+      return state;
+  }
+}
+export function projectedVacancyLanguageOffsets(state = {}, action) {
+  switch (action.type) {
+    case 'PROJECTED_VACANCY_LANGUAGE_OFFSETS_SUCCESS':
       return action.results;
     default:
       return state;
